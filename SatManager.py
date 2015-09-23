845eb813cc034cb6050f8a93bfd8ece96be50e6f
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2015-09-23 14:18:47 +0200

    adding Sat Interpolator
    implementing sat interpolator, takes in account the threshold (in
    meters)  used for filter data according to bathymetry.
       interpOnV1(CHL_16,bathy_threshold = 0)
    
    Correction on dumpV4file() : the matrix is now sliced appropriately.
    
    readfromfile() can now read both 3d and 2d matrices

--- SatManager.py
+++ SatManager.py
@@ -14,7 +14,10 @@ def readfromfile(filename):
     returns CHL
     ''' 
     ncIN = NC.netcdf_file(filename,'r')
-    CHL_IN = ncIN.variables['CHL'].data[0,:,:].copy()
+    varObj = ncIN.variables['CHL']
+    ndims = len(varObj.shape)
+    if ndims==2: CHL_IN=varObj.data.copy()
+    if ndims==3: CHL_IN=varObj.data[0,:,:].copy()    
     ncIN.close()
     return CHL_IN
 
@@ -56,7 +59,9 @@ def dumpV4file(filename,CHL):
     ncOUT.createDimension('lat',V4.jpj)
     
     ncvar = ncOUT.createVariable('CHL', 'f', ('lat','lon'))
-    ncvar[:] = CHL
+    chl = np.ones(V4.jpj, V4.jpi) * fillValue
+    chl[1:,:] = CHL[0:-1:,11:]
+    ncvar[:] = chl
     setattr(ncvar, 'missing_value', fillValue)
     setattr(ncvar, '_FillValue', fillValue)
     ncvar = ncOUT.createVariable('depth','f',('depth',))
@@ -69,16 +74,85 @@ def dumpV4file(filename,CHL):
     ncvar[:] = V4.lon
     ncOUT.close()
     
+
+def interpOnV1(CHL_16,bathy_threshold = 0):
+    '''
+    Performs interpolation on V1 grid using the following algorithm
+    For a given pixel of V1 mesh, we select the four pixels of the OrigSat mesh 
+    corresponding exactly to it.
+    Then, we look at the bathymetry in these pixels, and filter pixels having bathymetry greater than
+    bathy_threshold. 
+    Returned value is the average of the filtered pixels.
+    Condition to return values are:
+     - at least 3 pixels having bathymetry < bathy_threshold
+     - 2 pixels if they are on a diagonal
     
+    '''
+    V1 = masks.V1mesh
+    Bathyfile="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/MODIS/Bathy_MODIS.nc"
+    ncIN = NC.netcdf_file(Bathyfile,'r')
+    BATHY=ncIN.variables['bathy'].data
+    ncIN.close()
+    
+    PREVLON = np.array( [getnextIndex(NativeMesh.lon, V1.lon[i]) for i in range(V1.jpi)], np.int32) -1
+    PREVLAT = np.array( [getnextIndex(NativeMesh.lat, V1.lat[i]) for i in range(V1.jpj)], np.int32) -1
+
+    CHL_8 = np.ones((V1.jpj,V1.jpi),np.float32) * fillValue
+    
+    for i in range(V1.jpi):
+        ji = PREVLON[i]
+        for j in range(V1.jpj):
+            jj = PREVLAT[j] 
+            
+            riq =CHL_16[jj:jj+2,ji:ji+2]
+            bat = BATHY[jj:jj+2,ji:ji+2]
+            
+            batgoods = bat < bathy_threshold
+            condition = False
+            
+            if batgoods.sum() > 3:
+                condition = True               
+            if batgoods.sum() == 2:
+                diag1 = batgoods.diagonal()
+                diag2 = [batgoods[0,1], batgoods[1,0]]
+                if (diag1.all()) | (diag2.all()) :
+                    condition=True
+           
+            if condition:
+                selected = riq[batgoods]
+                CHL_8[j,i] = selected[selected>fillValue].mean()
+    return CHL_8    
        
-def dumpV1file(filename,M):
-    a=0
+def dumpV1file(filename,CHL):
+ 
+    V1 = masks.V1mesh
+    ncOUT  = NC.netcdf_file(filename,'w')
+    ncOUT.createDimension('time', 1)
+    ncOUT.createDimension('depth',1)
+    ncOUT.createDimension('lon',V1.jpi)
+    ncOUT.createDimension('lat',V1.jpj)
+    
+    ncvar = ncOUT.createVariable('CHL', 'f', ('lat','lon'))
+    ncvar[:] = CHL
+    setattr(ncvar, 'missing_value', fillValue)
+    setattr(ncvar, '_FillValue', fillValue)
+    ncvar = ncOUT.createVariable('depth','f',('depth',))
+    ncvar[:] = 1.47210180759
+    ncvar = ncOUT.createVariable('time','f',('time',))
+    ncvar[:] = 1.0
+    ncvar = ncOUT.createVariable('lat','f',('lat',))
+    ncvar[:] = V1.lat
+    ncvar = ncOUT.createVariable('lon','f',('lon',))
+    ncvar[:] = V1.lon
+    ncOUT.close()                  
+
     
 def logAverager(M):
     '''
     Inner matrix M has dimensions (nFrames, jpj, jpi )
     Performs average passing through natural logarithm
     Mean  = exp(ln(values).mean() ) 
+    At the moment works only for files on native mesh (253,733)
      
     '''
     nFrames,jpj,jpi = M.shape
@@ -95,5 +169,13 @@ def logAverager(M):
                 LOGmean = np.log(l[goodValues]).sum() / count
                 CHL_OUT[j,i] = np.exp(LOGmean)
     return CHL_OUT
+
+def getnextIndex(array,value):
+    for i,val in enumerate(array):
+        if val>value:
+            break
+    return i
+
+
     
     

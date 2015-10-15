856d997e102b7fc3d6297128a6debfc00261e410
Gianfranco Gallizia <skyglobe83@gmail.com>
2015-10-15 14:54:37 +0200

    Added helpers to commons

---
+++ commons/helpers.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+"""Helper functions"""
+
+def is_number(val):
+    """Tells if a value is a number type
+
+    Args:
+        - *val*: the value to test
+
+    Returns:
+        - True if val is a number type.
+        - False if val is not a number type.
+    """
+    return isinstance(val, (int, long, float, complex, np.float32))
+

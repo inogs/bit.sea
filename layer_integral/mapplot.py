0ce98b41ad4aeb10e2938c2b5e940dce7d76f75f
Gianfranco Gallizia <skyglobe83@gmail.com>
2015-10-19 12:53:18 +0200

    Refactored mapplot procedure to use a more OO approach

--- layer_integral/mapplot.py
+++ layer_integral/mapplot.py
@@ -11,18 +11,19 @@ def mapplot(map_dict, mask=None, min_ticks=4, max_ticks=8, coastline=False):
         - *min_ticks* (optional): Number of ticks to set in the shorter axis.
         - *max_ticks* (optional): Number of ticks to set in the longer axis.
     """
+    fig , ax = pl.subplots()
     if (not mask is None) and coastline:
         coast_m = np.array(mask.mask[0,:,:], dtype=np.float32)
         coast_m[coast_m != 0] = np.nan
-        pl.imshow(coast_m, cmap='hot')
-        pl.gca().invert_yaxis()
-        pl.hold(True)
+        ax.imshow(coast_m, cmap='hot')
+        ax.invert_yaxis()
+        fig.hold(True)
     clim = map_dict['clim']
     title = "%s %s %s" % (map_dict['date'], map_dict['varname'], map_dict['layer'].__repr__())
-    pl.imshow(map_dict['data'])
-    pl.clim(clim[0], clim[1])
-    pl.colorbar()
-    pl.gca().invert_yaxis()
+    im = ax.imshow(map_dict['data'])
+    im.set_clim(clim[0], clim[1])
+    fig.colorbar(im)
+    ax.invert_yaxis()
     if not mask is None:
         shape = map_dict['data'].shape
         #Decide who gets the most ticks
@@ -40,14 +41,16 @@ def mapplot(map_dict, mask=None, min_ticks=4, max_ticks=8, coastline=False):
         x_labels = list()
         for x in x_points:
             x_labels.append(mask.convert_x_y_to_lon_lat(int(x), 0)[0])
-        pl.xticks(x_points, x_labels)
+        ax.set_xticks(x_points)
+        ax.set_xticklabels(x_labels)
         #Set Y axis ticks
         y_points = np.linspace(0,shape[0]-1,y_ticks).tolist()
         y_labels = list()
         for y in y_points:
             y_labels.append(mask.convert_x_y_to_lon_lat(0, int(y))[1])
-        pl.yticks(y_points, y_labels)
-    pl.title(title)
-    pl.grid()
-    pl.show()
+        ax.set_yticks(y_points)
+        ax.set_yticklabels(y_labels)
+    fig.suptitle(title)
+    ax.grid()
+    fig.show()
 

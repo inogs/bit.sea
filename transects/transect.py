f5d872f88ccb5c4e15f8172836f7d86f4c9eb8ce
Gianfranco Gallizia <skyglobe83@gmail.com>
2015-10-20 15:59:24 +0200

    Added get_transect_data method to Transect

--- transects/transect.py
+++ transects/transect.py
@@ -1,10 +1,13 @@
 # Copyright (c) 2015 eXact Lab srl
 # Author: Gianfranco Gallizia <gianfranco.gallizia@exact-lab.it>
+import numpy as np
 from xml.dom import minidom
 from ast import literal_eval
 from commons.segment import Segment
 from commons.helpers import is_number
 from commons.xml import *
+from commons.mask import Mask
+from commons.dataextractor import DataExtractor
 
 class Transect(object):
     """Stores a multiple segment transect definition.
@@ -25,6 +28,8 @@ class Transect(object):
         if not isinstance(segmentlist, (list, tuple)) or ((len(segmentlist) > 0) and not isinstance(segmentlist[0], (Segment,))):
             raise ValueError("segmentlist must be a list of Segments")
         self.__segmentlist = segmentlist
+        #Variable data cache
+        self.__datacache = { 'filename':None, 'data':None }
 
     @property
     def varname(self):
@@ -37,6 +42,42 @@ class Transect(object):
     @property
     def segmentlist(self):
         return self.__segmentlist
+
+    def get_transect_data(self, segment_index, mask, datafilepath, fill_value=np.nan, timestep=0):
+        """Extracts transect data from a NetCDF file.
+
+        Args:
+            - *segment_index*: the index of the segment to retrieve.
+            - *mask*: a commons.Mask object.
+            - *datafilepath*: path to the NetCDF data file.
+            - *fill_value* (optional): value to use when there's no data
+              available (default: np.nan).
+            - *timestep* (optional): the time index (default: 0).
+        Returns: a NumPy array that contains the requested data.
+        """
+        #Input validation
+        if not is_number(segment_index):
+            raise ValueError("'%s' is not a number." % (segment_index,))
+        seg = self.__segmentlist[segment_index]
+        if not isinstance(mask, (Mask,)):
+            raise ValueError("mask must be a Mask object.")
+        datafilepath = str(datafilepath)
+        #Retrieve indices
+        x_min, y_min = mask.convert_lon_lat_to_indices(seg.lon_min, seg.lat_min)
+        x_max, y_max = mask.convert_lon_lat_to_indices(seg.lon_max, seg.lat_max)
+        #Check if we don't have cached data
+        if (self.__datacache['filename'] is None) or (self.__datacache['filename'] != datafilepath):
+            #Read data from the NetCDF file
+            de = DataExtractor(self.__varname, datafilepath, mask, fill_value)
+            self.__datacache['filename'] = datafilepath
+            self.__datacache['data'] = de.filled_values
+        if x_min == x_max:
+            data = np.array(self.__datacache['data'][timestep,:,y_min:y_max,x_min], dtype=float)
+        elif y_min == y_max:
+            data = np.array(self.__datacache['data'][timestep,:,y_min,x_min:x_max], dtype=float)
+        else:
+            raise ValueError("Invalid segment: %s" % (seg,))
+        return data
 
     @staticmethod
     def get_list_from_XML_file(plotlistfile):

8e1c5b842e41bfd080adb8b1674af9fe0a87f323
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2015-11-06 14:21:56 +0100

    renaming Float/getModelData.py -> profiler.py (in root dir)
    profiler launches the job profiler only if it is called as main

--- Float/shared_data.py
+++
@@ -1,5 +0,0 @@
-INPUTDIR="/pico/scratch/userexternal/gbolzon0/PROFILATORE/AVE/"
-BASEDIR='/pico/scratch/userexternal/gbolzon0/PROFILATORE/'
-
-DATESTART = '20150901-00:00:00'
-DATE__END = '20150917-00:00:00'
---
+++ profiler.py
@@ -0,0 +1,20 @@
+
+
+from commons.time_interval import TimeInterval
+
+INPUTDIR="/pico/scratch/userexternal/gbolzon0/PROFILATORE/AVE/"
+BASEDIR='/pico/scratch/userexternal/gbolzon0/PROFILATORE/'
+
+DATESTART = '20150901-00:00:00'
+DATE__END = '20150917-00:00:00'
+
+T_INT = TimeInterval(DATESTART,DATE__END, '%Y%m%d-%H:%M:%S')
+
+if __name__ == '__main__':
+    from instruments.matchup_manager import Matchup_Manager
+
+    M = Matchup_Manager(T_INT,INPUTDIR,BASEDIR)
+    M.writefiles_for_profiling('./jobProfiler.sh')
+    M.dumpModelProfiles('./jobProfiler.sh')
+
+

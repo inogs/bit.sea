f2e33717205dec3347df0d5dec34e34ce80d39ed
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2015-11-18 10:46:54 +0100

    Bug Fix : FloatProfilesMatchup class has to be defined after the FloatMatchup class (from which inherits)
    ProfileMatchup has now the localizationArrays() method to return Lon,Lat,time
    ProfilesMatchup has its method export()

--- matchup/matchup.py
+++ matchup/matchup.py
@@ -4,8 +4,82 @@ from commons.layer import Layer
 from statistics import matchup
 
 
+
+
+class ProfilesMatchup(matchup):
+    def __init__(self, Model=None, Ref=None, Depth=None, Lon=None, Lat=None, Time=None, Qc=None):
+        if Model is None:
+            self.Model = np.array([],np.float32)
+            self.Ref   = np.array([],np.float32)
+            self.Depth = np.array([],np.float32)
+            self.Lon   = np.array([],np.float32)
+            self.Lat   = np.array([],np.float32)
+            self.Lengths = []
+        else:
+            self.Model = Model
+            self.Ref   = Ref
+            self.Depth = Depth
+            self.Lon   = Lon
+            self.Lat   = Lat
+            self.Time  = Time
+            self.Qc    = Qc
+            self.Lengths = [len(Model)]
+
+    def subset(self,layer):
+        if self.number() ==0:
+            return self
+        ii = (self.Depth <= layer.bottom) & (self.Depth >= layer.top)
+        return ProfilesMatchup(self.Model[ii], self.Ref[ii], self.Depth[ii], self.Lon[ii], self.Lat[ii], self.Time[ii], self.Qc[ii])
+
+
+    def extend(self,fm):
+        lenfm = fm.Model.size
+        fmLon,fmLat,fmTime = fm.localizationArrays()
+
+        self.Model = np.concatenate((self.Model, fm.Model))
+        self.Ref   = np.concatenate((self.Ref,   fm.Ref))
+        self.Depth = np.concatenate((self.Depth, fm.Depth))
+        self.Lon   = np.concatenate((self.Lon,   fmLon))
+        self.Lat   = np.concatenate((self.Lat,   fmLat))
+        self.Time  = np.concatenate((self.Time,  fmTime))
+        self.Qc    = np.concatenate((self.Qc,    fm.Qc ))
+
+        self.Lengths.extend([ lenfm ])
+
+    def export(self,directory,prefix):
+
+        self.Model.tofile(directory + prefix + "model.txt",sep="\n",format="%10.5f")
+        self.Ref.tofile(  directory + prefix + "ref.txt"  ,sep="\n",format="%10.5f")
+        self.Lon.tofile(  directory + prefix + "lon.txt"  ,sep="\n",format="%10.5f")
+        self.Lat.tofile(  directory + prefix + "lat.txt"  ,sep="\n",format="%10.5f")
+        self.Depth.tofile(directory + prefix + "depth.txt",sep="\n",format="%10.5f")
+        self.Time.tofile( directory + prefix + "time.txt" ,sep="\n",format="%10.5f")
+        self.Qc.tofile(   directory + prefix + "Qc.txt"   ,sep="\n",format="%10.5f")
+
+
+    def plot(self):
+        '''
+        Red lines are reference (biofloat)
+        Blue lines are model
+        '''
+
+        pl.figure()
+        StartInd=0
+        for length in self.Lengths:
+            End_Ind = StartInd + length
+            model = self.Model[StartInd:End_Ind]
+            ref   = self.Ref[StartInd:End_Ind]
+            pres  = self.Depth[StartInd:End_Ind]
+
+            pl.plot(model,pres,'b', ref,pres,'r')
+            StartInd = End_Ind
+        pl.gca().invert_yaxis()
+        pl.show(block=False)
+
+
+
 class FloatProfilesMatchup(ProfilesMatchup):
-    def __init__(self, Model=None, Ref=None, Depth=None, Lon=None, Lat=None, time=None, qc=None, name=None, cycle=None):        
+    def __init__(self, Model=None, Ref=None, Depth=None, Lon=None, Lat=None, time=None, qc=None, name=None, cycle=None):
         if Model is None:
             self.Model = np.array([],np.float32)
             self.Ref   = np.array([],np.float32)
@@ -31,20 +105,20 @@ class FloatProfilesMatchup(ProfilesMatchup):
 
 
     def extend(self,fm):
+        lenfm = fm.Model.size
+        fmLon,fmLat,fmTime = fm.localizationArrays()
+
         self.Model = np.concatenate((self.Model, fm.Model))
         self.Ref   = np.concatenate((self.Ref,   fm.Ref))
         self.Depth = np.concatenate((self.Depth, fm.Depth))
-        lenfm = fm.Model.size
-        self.Lon   = np.concatenate((self.Lon,   np.ones(lenfm,)*fm.instrument.lon))
-        self.Lat   = np.concatenate((self.Lat,   np.ones(lenfm,)*fm.instrument.lat))
-        t = fm.instrument.time
-        instrumenttime = t.toordinal()+366 + float(t.hour)/24 + float(t.minute)/(24.*60)
-
-        self.Time  = np.concatenate((self.Time,  np.ones(lenfm,)*instrumenttime))
+        self.Lon   = np.concatenate((self.Lon,   fmLon))
+        self.Lat   = np.concatenate((self.Lat,   fmLat))
+        self.Time  = np.concatenate((self.Time,  fmTime))
         self.Qc    = np.concatenate((self.Qc,    fm.Qc ))
+
         self.name  = np.concatenate((self.name,  np.ones(lenfm,)*int(fm.instrument.name()) ))
         self.cycle = np.concatenate((self.cycle, np.ones(lenfm,)*fm.instrument._my_float.cycle))
-        self.Lengths.extend([ len(fm.Model)])
+        self.Lengths.extend([ lenfm ])
 
     def export(self,directory,prefix):
 
@@ -57,57 +131,6 @@ class FloatProfilesMatchup(ProfilesMatchup):
         self.Qc.tofile(   directory + prefix + "Qc.txt"   ,sep="\n",format="%10.5f")
         self.name.tofile( directory + prefix + "name.txt" ,sep="\n",format="%d")
         self.cycle.tofile(directory + prefix + "cycle.txt",sep="\n",format="%d")
-
-
-class ProfilesMatchup(matchup):
-    def __init__(self, Model=None, Ref=None, Depth=None, Lon=None, Lat=None):        
-        if Model is None:
-            self.Model = np.array([],np.float32)
-            self.Ref   = np.array([],np.float32)
-            self.Depth = np.array([],np.float32)
-            self.Lon   = np.array([],np.float32)
-            self.Lat   = np.array([],np.float32)
-            self.Lengths = []
-        else:
-            self.Model = Model
-            self.Ref   = Ref
-            self.Depth = Depth
-            self.Lon   = Lon
-            self.Lat   = Lat
-            self.Lengths = [len(Model)]
-        
-    def subset(self,layer):
-        if self.number() ==0:
-            return self
-        ii = (self.Depth <= layer.bottom) & (self.Depth >= layer.top)        
-        return ProfilesMatchup(self.Model[ii], self.Ref[ii], self.Depth[ii], self.Lon[ii], self.Lat[ii])
-
-    
-    def extend(self,fm):
-        self.Model = np.concatenate((self.Model, fm.Model))
-        self.Ref   = np.concatenate((self.Ref,   fm.Ref))
-        self.Depth = np.concatenate((self.Depth, fm.Depth))
-        self.Lon   = np.concatenate((self.Lon,   fm.Lon))
-        self.Lat   = np.concatenate((self.Lat,   fm.Lat))
-        self.Lengths.extend([ len(fm.Model)])
-    def plot(self):
-        '''
-        Red lines are reference (biofloat)
-        Blue lines are model
-        '''
-
-        pl.figure()
-        StartInd=0
-        for length in self.Lengths:
-            End_Ind = StartInd + length
-            model = self.Model[StartInd:End_Ind]
-            ref   = self.Ref[StartInd:End_Ind]
-            pres  = self.Depth[StartInd:End_Ind]
-
-            pl.plot(model,pres,'b', ref,pres,'r')
-            StartInd = End_Ind
-        pl.gca().invert_yaxis()
-        pl.show(block=False)
 
 
 class ProfileMatchup():
@@ -124,8 +147,19 @@ class ProfileMatchup():
         self.Depth = Depth
         self.Qc    = Qc
         self.instrument = profileObj
-        self.Lon   = np.ones_like(Model)*profileObj.lon
-        self.Lat   = np.ones_like(Model)*profileObj.lat
+
+
+    def localizationArrays(self):
+        '''Returns arrays Lon,Lat,Time as repetition of instrument fields
+        Time is expressed as matlab time
+        '''
+        n = len(self.Model)
+        Lon   = np.ones(n,)*self.instrument.lon
+        Lat   = np.ones(n,)*self.instrument.lat
+        t = self.instrument.time
+        instrumenttime = t.toordinal()+366 + float(t.hour)/24 + float(t.minute)/(24.*60)
+        Time  = np.ones(n,)*instrumenttime
+        return Lon,Lat,Time
 
     def plot(self):
         '''
@@ -140,8 +174,7 @@ class ProfileMatchup():
             
 
 if __name__ == '__main__':
-    import numpy as np
     Depth = np.arange(10.)/10
-    a=ProfilesMatchup(np.arange(10),np.arange(10)+1,Depth,Depth,Depth  )
+    a=ProfilesMatchup(np.arange(10),np.arange(10)+1,Depth,Depth,Depth,Depth,Depth  )
     L=Layer(0.3, 0.8)
     b = a.subset(L)

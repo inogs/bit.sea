dffea493f660b1d69ba05e2b3b0b142ad5dab73f
Gianfranco Gallizia <skyglobe83@gmail.com>
2016-01-27 09:11:45 +0100

    Added rundays parameter to get_runs method.

--- commons/timeseries.py
+++ commons/timeseries.py
@@ -32,17 +32,28 @@ class TimeSeries(object):
             warn("%s doesn't exists" % archive_dir)
             self._archive_dir = archive_dir
 
-    def get_runs(self):
+    def get_runs(self, rundays=[2,5]):
         """
+        Args:
+            - *rundays* (optional): a list of integers mapping the weekdays in
+              which the chain runs (default: [2,5]).
+              1 is Monday
+              2 is Tuesday
+              3 is Wednesday
+              4 is Thursday
+              5 is Friday
+              6 is Saturday
+              7 is Sunday
+
         Returns: a list of tuples (datetime, path) within the time_interval for
         which there's a run.
         """
         output = list()
         t = self._time_interval.start_time
         while t <= self._time_interval.end_time:
-            if (t.isoweekday() == 2) or (t.isoweekday() == 5):
-                test_path = path.join(self._archive_dir, t.strftime('%Y%m%d'), self._prefix_dir)
-                if path.exists(test_path) and path.isdir(test_path):
-                    output.append((t, test_path))
+            if t.isoweekday() in rundays:
+                run_path = path.join(self._archive_dir, t.strftime('%Y%m%d'), self._prefix_dir)
+                if path.exists(run_path) and path.isdir(run_path):
+                    output.append((t, run_path))
             t += timedelta(1)
         return output

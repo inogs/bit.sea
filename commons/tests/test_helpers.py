09dc3ea87f1142dfe3a46d7feafc85a7d56110ce
Gianfranco Gallizia <skyglobe83@gmail.com>
2016-01-28 15:55:53 +0100

    Added get_date_string helper with tests.

--- commons/tests/test_helpers.py
+++ commons/tests/test_helpers.py
@@ -22,3 +22,24 @@ def test_is_number():
     assert not is_number(None)
     #String
     assert not is_number("0")
+
+def test_get_date_string():
+    l, s = get_date_string("20160101")
+    assert (l == "2016-01-01") and (s == "20160101")
+
+def test_get_date_string_empty_string():
+    l, s = get_date_string("")
+    assert (l == "") and (l == s)
+
+@raises(TypeError)
+def test_get_date_string_invalid_input():
+    #List of one string
+        _ , _ = get_date_string(['20160101'])
+    #Tuple with one string
+        _ , _ = get_date_string(('20160101',))
+    #Number
+        _ , _ = get_date_string(20160101)
+    #None
+        _ , _ = get_date_string(None)
+    #Empty list
+        _ , _ = get_date_string([])

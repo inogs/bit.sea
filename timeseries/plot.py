72db60386bfdc2224f23f4137a3436ee54795a11
Gianfranco Gallizia <skyglobe83@gmail.com>
2016-02-03 20:09:37 +0100

    Changed depths argument meaning and added docstrings to plot_Hovmoeller_diagram method.

--- timeseries/plot.py
+++ timeseries/plot.py
@@ -75,8 +75,45 @@ def plot_from_files(file_list, varname, subbasin, coast=CoastEnum.open_sea, stat
     return fig,ax
 
 def plot_Hovmoeller_diagram(file_list, varname, subbasin, coast=CoastEnum.open_sea, stat=StatEnum.mean, depths=72, fig=None, ax=None):
+    """
+    Plots a time series Hovmoeller diagram.
+
+    Args:
+        - *file_list*: a list of path strings for the files in the time series.
+        - *varname*: name of the variable to plot.
+        - *subbasin*: an element from SubBasinEnum.
+        - *coast* (optional): an element from CoastEnum (default:
+          CoastEnum.open_sea).
+        - *stat* (optional): an element from StatEnum (default: StatEnum.mean).
+        - *depths* (optional): integer OR list OR Numpy array of depth values
+          (default: 72).  If you pass a single integer it will be interpreted
+          as the lenght of the depths array if you pass a list or Numpy array
+          it will be used to set the labels and its lenght will be assumed as
+          the lenght of the depths array.
+        - *fig* (optional): an instance of matplotlib figure. A new one will be
+          created if it is set to None (default: None).
+        - *ax* (optional): an instance of matplotlib axes. A new one will be
+          created if it is set to None (default: None).
+
+    Returns: a matplotlib figure and axes object.
+    """
     if (fig is None) or (ax is None):
         fig , ax = plt.subplots()
+    dlabels = None
+    if isinstance(depths, (int, long)):
+        pass
+    elif isinstance(depths, (list, tuple)):
+        dlabels = np.array(depths)
+        depths = len(dlabels)
+    elif isinstance(depths, np.ndarray):
+        #If 1D array
+        if len(depths.shape) == 1:
+            dlabels = np.copy(depths)
+            depths = dlabels.shape[0]
+        else:
+            raise ValueError("Invalid depths argument")
+    else:
+        raise ValueError("Invalid depths argument")
     plotmat = np.zeros([depths, len(file_list)])
     label_list = list()
     #For each file
@@ -90,21 +127,30 @@ def plot_Hovmoeller_diagram(file_list, varname, subbasin, coast=CoastEnum.open_s
         #Open it with netCDF4
         dset = netCDF4.Dataset(f)
         #Copy the data in the plot matrix
-        plotmat[:,i] = dset[varname][subbasin, coast, :, stat]
+        plotmat[:,i] = dset[varname][subbasin, coast, 0:depths, stat]
         #Close the file
         dset.close()
     #Plot the matrix
     ax.imshow(plotmat)
     #Set labels
+    ax.set_xticks(range(len(label_list)))
     ax.set_xticklabels(label_list, rotation='vertical')
-    #Invert Y axis
-    ax.invert_yaxis()
+    if not (dlabels is None):
+        ticks = np.array(np.round(np.linspace(0, depths-1, num=8)), dtype=int)
+        print ticks
+        #ticks = [x * n for x in range(n)]
+        labs = dlabels[ticks]
+        print "Ticks: %s\nLabels: %s" % (str(ticks), str(labs))
+        ax.set_yticks(ticks)
+        ax.set_yticklabels(labs)
     return fig,ax
 
 if __name__ == "__main__":
     from glob import glob
     fl = sorted(glob('timeseries/*nc'))
-    fig,ax = plot_from_files(fl, 'O2o', SubBasinEnum.med)
-    plt.show()
-    fig,ax = plot_Hovmoeller_diagram(fl, 'O2o', SubBasinEnum.med)
+    #fig,ax = plot_from_files(fl, 'O2o', SubBasinEnum.med)
+    #plt.show()
+    #fig,ax = plot_Hovmoeller_diagram(fl, 'O2o', SubBasinEnum.med)
+    depths = [x * (5000 / 72) for x in range(72)]
+    fig,ax = plot_Hovmoeller_diagram(fl, 'O2o', SubBasinEnum.med, depths=depths)
     plt.show()

59f4e33cbd19a69c75aa62edf4071cf0d96a22c5
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2016-02-24 15:13:04 +0100

    Just adding files for Reanalysis

---
+++ Sat/RA/aveSatRA.py
@@ -0,0 +1,43 @@
+from commons.Timelist import TimeList
+from commons.time_interval import TimeInterval
+from commons import IOnames
+import numpy as np
+import os
+import SatManager as Sat
+
+CHECKDIR ="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/CCI/NEW_20161702/DAILY/CHECKED/"
+WEEKLYDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/CCI/NEW_20161702/WEEKLY_V4/"
+
+reset = False
+
+Timestart="19990102"
+Time__end="20150101"
+IonamesFile = '../postproc/IOnames_sat.cci.xml'
+TI = TimeInterval(Timestart,Time__end,"%Y%m%d")
+TLCheck = TimeList.fromfilenames(TI, CHECKDIR,"*.nc",IonamesFile)
+IOname = IOnames.IOnames(IonamesFile)
+
+WEEK_reqs=TLCheck.getWeeklyList(2)
+
+jpi = Sat.NativeMesh.jpi
+jpj = Sat.NativeMesh.jpj
+
+
+for req in WEEK_reqs:
+    outfile = req.string + IOname.Output.suffix + ".nc"
+    outpathfile = WEEKLYDIR + outfile
+    conditionToSkip = (os.path.exists(outpathfile)) and (not reset)
+
+    if conditionToSkip: continue
+
+    print outfile
+    ii, w = TLCheck.select(req)
+    nFiles = len(ii)
+    M = np.zeros((nFiles,jpj,jpi),np.float32)
+    for iFrame, j in enumerate(ii):
+        inputfile = TLCheck.filelist[j]
+        CHL = Sat.readfromfile(inputfile)
+        M[iFrame,:,:] = CHL
+    CHL_OUT = Sat.logAverager(M)
+    Sat.dumpV4file(outpathfile, CHL_OUT)
+

1ced91a381815aeb7a595cedf7e60ea6001bd78b
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2016-03-09 10:31:07 +0100

    Adding file

---
+++ validation/deliverables/quid.sh
@@ -0,0 +1,23 @@
+# Descriptor of CMEMS-Med-QUID-006-008-V2-V1.0.docx
+
+
+# SECTION 4?:
+ScMYvalidation_plan.py # writes export_data_ScMYValidation_plan.pkl
+
+# figure 4.2
+  plot_timeseries.py     # reads export_data_ScMYValidation_plan.pkl, dumps
+
+
+# figure 4.3
+  plot_timeseries_RMS.py     # reads export_data_ScMYValidation_plan.pkl
+
+# table 4.1
+   plot_timeseries_RMS.py
+
+
+
+MYValidation_plan_statics.py # writes export_data_ScMYValidation_plan_statics.pkl
+# table 4.3
+reader_statics.py# phosphate
+# table 4.4
+reader_statics.py# Nitrate

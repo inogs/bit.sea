62f78a527ceee78d80b7cc3d983d8eead4523ff2
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2016-03-16 12:21:59 +0100

    adding 2 variables on VarDescriptorB station names can have more than 20 chars  --> update on maskload

--- postproc/maskload.py
+++ postproc/maskload.py
@@ -94,8 +94,8 @@ SUBlist.append('med')
 
 def read_Positions_for_Pointprofiles(filename):
 
-    dtIN  = np.dtype([('Name','S20'), ('Lon',np.float32), ('Lat',np.float32)])
-    dtOUT = np.dtype([('Name','S20'), ('Lon',np.float32), ('Lat',np.float32), ('i',np.int), ('j',np.int)])  
+    dtIN  = np.dtype([('Name','S40'), ('Lon',np.float32), ('Lat',np.float32)])
+    dtOUT = np.dtype([('Name','S40'), ('Lon',np.float32), ('Lat',np.float32), ('i',np.int), ('j',np.int)])  
     MeasPoints=np.loadtxt(filename, dtype=dtIN, skiprows=1,ndmin=1,delimiter='\t')
     
     nMeas=MeasPoints.size

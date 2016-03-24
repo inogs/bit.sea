c0d3be4dfe328b21cefdd2347106af6f4dc87c24
GianFranco Marras <a07ogs00@node013.pico.cineca.it>
2016-03-24 17:24:50 +0100

    preparation of data with varnames for ogstm.xx

--- Sat/aveSat16.py
+++ Sat/aveSat16.py
@@ -33,11 +33,15 @@ for req in WEEK_reqs:
     print outfile
     ii, w = TLCheck.select(req)
     nFiles = len(ii)
+    if nFiles < 3 : 
+        print req
+        print "less than 3 files"
+        continue
     M = np.zeros((nFiles,jpj,jpi),np.float32)
     for iFrame, j in enumerate(ii):
         inputfile = TLCheck.filelist[j]
         CHL = Sat.readfromfile(inputfile)
         M[iFrame,:,:] = CHL
     CHL_OUT = Sat.logAverager(M)
-    Sat.dumpfile(outpathfile, CHL_OUT)
+    Sat.dumpV4file(outpathfile, CHL_OUT)
 

24e77314a6f6c868a14c2377728545062d24e530
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2016-04-05 11:43:53 +0200

    Bug fix The problem is to find the path dir of a module

--- layer_integral/coastline.py
+++ layer_integral/coastline.py
@@ -1,7 +1,9 @@
 import numpy as np
+import os
 
 def get():
-    coastline=np.load('Coastline.npy')
+    localdir=os.path.dirname(os.path.abspath(__file__))
+    coastline=np.load(localdir + '/Coastline.npy')
     c_lon=coastline['Lon']
     c_lat=coastline['Lat']
     return c_lon, c_lat

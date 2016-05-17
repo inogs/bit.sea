e7847d805c420f29ae6f59616de696cc627231cc
Giorgio Bolzon <gbolzon@inogs.it>
2016-05-17 11:04:35 +0200

    1) Layer object can be defined also for a single depth    e.g. l=Layer(20,20)    is admitted 2) Adding a new method MapBuilder.get_layer_integral() 3) Adaptation of validation/deliverables scripts to the use of that method.

--- commons/layer.py
+++ commons/layer.py
@@ -4,22 +4,30 @@ class Layer(object):
     def __init__(self,top, bottom):
         t = float(top)
         b = float(bottom)
-        if t < b:
+        if t <= b:
             self.__top = t
             self.__bottom = b
         else:
             raise ValueError("top must be above of bottom")
 
     def __repr__(self):
+        if self.top ==self.bottom:
+            return "Layer %g m" %self.__top
         return "Layer %g-%g m" %(self.__top, self.__bottom)
 
     def __str__(self):
+        if self.top ==self.bottom:
+            return "Layer %g m" %self.__top
         return "layer%g-%g" %(self.__top, self.__bottom)
 
     def string(self):
+        if self.top ==self.bottom:
+            return "%g m" %self.__top
         return "%g-%gm" %(self.__top, self.__bottom)
 
     def longname(self):
+        if self.top ==self.bottom:
+            return "%04g m" %self.__top
         return "%04g-%04gm" %(self.__top, self.__bottom)
 
     @property

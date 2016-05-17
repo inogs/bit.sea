d081219d0d5ab8d92e536f9aa5f8f20b57bc2115
Anna Teruzzi <ateruzzi@node013.pico.cineca.it>
2016-05-17 13:57:00 +0200

    Created multirun dir

---
+++ validation/multirun/ScMYvalidation_plan.py
@@ -0,0 +1,153 @@
+import argparse
+def argument():
+    parser = argparse.ArgumentParser(description = '''
+    Calculates Chl statistics on matchups of Model and Sat.
+    Main result are
+    - BGC_CLASS4_CHL_RMS_SURF_BASIN
+    - BGC_CLASS4_CHL_BIAS_SURF_BASIN
+    of deliverable CMEMS-Med-biogeochemistry-ScCP-1.0.pdf
+
+    Other similar results are also calculated and dumped in
+    the a pickle file provided as argument.
+    ''',
+    formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(   '--satdir', '-s',
+                                type = str,
+                                required =False,
+                                default = "/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/CCI/MONTHLY_V4/",
+                                help = ''' Input satellite dir'''
+                                )
+
+    parser.add_argument(   '--moddir', '-m',
+                                type = str,
+                                required =False,
+                                default = "/pico/scratch/userexternal/gbolzon0/RA_CARBO/RA_02/wrkdir/POSTPROC/output/AVE_FREQ_2/TMP/",
+                                help = ''' Input model dir'''
+                                )
+
+    parser.add_argument(   '--outfile', '-o',
+                                type = str,
+                                required = True,
+                                default = 'export_data_ScMYValidation_plan.pkl',
+                                help = 'Output pickle file')
+
+
+    return parser.parse_args()
+
+
+args = argument()
+
+
+
+from commons.Timelist import TimeList
+from commons.time_interval import TimeInterval
+import commons.IOnames as IOnames
+import numpy as np
+import Sat.SatManager as Sat
+import matchup.matchup as matchup
+from commons.dataextractor import DataExtractor
+from layer_integral.mapbuilder import MapBuilder
+from commons.mask import Mask
+from commons.submask import SubMask
+from basins import OGS
+from commons.layer import Layer
+import pickle
+
+def weighted_mean(Conc, Weight):
+
+    Weight_sum      = Weight.sum()
+    Mass            = (Conc * Weight).sum()
+    Weighted_Mean   = Mass/Weight_sum
+    return Weighted_Mean
+
+TheMask=Mask('/pico/home/usera07ogs/a07ogs00/OPA/V4/etc/static-data/MED1672_cut/MASK/meshmask.nc')
+MODEL_DIR= args.moddir
+REF_DIR  = args.satdir
+outfile  = args.outfile
+
+
+Timestart="20130101"
+Time__end="20140101"
+TI    = TimeInterval(Timestart,Time__end,"%Y%m%d")
+
+sat_TL   = TimeList.fromfilenames(TI, REF_DIR  ,"*.nc", prefix="", dateformat="%Y%m%d")
+model_TL = TimeList.fromfilenames(TI, MODEL_DIR,"*.nc")
+
+IOname = IOnames.IOnames('IOnames_sat.xml')
+
+nFrames = model_TL.nTimes
+nSUB = len(OGS.P.basin_list)
+
+jpk,jpj,jpi =TheMask.shape
+dtype = [(sub.name, np.bool) for sub in OGS.P]
+SUB = np.zeros((jpj,jpi),dtype=dtype)
+for sub in OGS.P:
+    sbmask         = SubMask(sub,maskobject=TheMask).mask
+    SUB[sub.name]  = sbmask[0,:,:]
+
+mask200_2D = TheMask.mask_at_level(200.0)
+
+BGC_CLASS4_CHL_RMS_SURF_BASIN      = np.zeros((nFrames,nSUB),np.float32)
+BGC_CLASS4_CHL_BIAS_SURF_BASIN     = np.zeros((nFrames,nSUB),np.float32)
+MODEL_MEAN                         = np.zeros((nFrames,nSUB),np.float32)
+SAT___MEAN                         = np.zeros((nFrames,nSUB),np.float32)
+BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG  = np.zeros((nFrames,nSUB),np.float32)
+BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG = np.zeros((nFrames,nSUB),np.float32)
+
+# This is the surface layer choosen to match satellite chl data
+surf_layer = Layer(0,10)
+
+for itime, modeltime in enumerate(model_TL.Timelist):
+    print modeltime
+    CoupledList = sat_TL.couple_with([modeltime])
+    sattime = CoupledList[0][0]
+    satfile = REF_DIR + sattime.strftime(IOname.Input.dateformat) + IOname.Output.suffix + ".nc"
+    modfile = model_TL.filelist[itime]
+
+    De         = DataExtractor(TheMask,filename=modfile, varname='P_i')
+    Model      = MapBuilder.get_layer_average(De, surf_layer)
+    #ncIN = NC.netcdf_file(modfile,'r')
+    #Model = ncIN.variables['P_i'].data[0,0,:,:].copy()#.astype(np.float64)
+    #Model = ncIN.variables['lchlm'].data.copy()
+    #ncIN.close()
+
+    Sat16 = Sat.readfromfile(satfile,var='lchlm') #.astype(np.float64)
+
+
+    cloudsLand = (np.isnan(Sat16)) | (Sat16 > 1.e19) | (Sat16<0)
+    modelLand  = np.isnan(Model) #lands are nan
+    nodata     = cloudsLand | modelLand
+    selection = ~nodata & mask200_2D
+    M = matchup.matchup(Model[selection], Sat16[selection])
+
+    for isub, sub in enumerate(OGS.P):
+        selection = SUB[sub.name] & (~nodata) & mask200_2D
+        M = matchup.matchup(Model[selection], Sat16[selection])
+        BGC_CLASS4_CHL_RMS_SURF_BASIN[itime,isub]  = M.RMSE()
+        BGC_CLASS4_CHL_BIAS_SURF_BASIN[itime,isub] = M.bias()
+        weight = TheMask.area[selection]
+        MODEL_MEAN[itime,isub] = weighted_mean( M.Model,weight)
+        SAT___MEAN[itime,isub] = weighted_mean( M.Ref,  weight)
+
+        Mlog = matchup.matchup(np.log10(Model[selection]), np.log10(Sat16[selection])) #add matchup based on logarithm
+        BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG[itime,isub]  = Mlog.RMSE()
+        BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG[itime,isub] = Mlog.bias()
+
+BGC_CLASS4_CHL_EAN_RMS_SURF_BASIN  = BGC_CLASS4_CHL_RMS_SURF_BASIN.mean(axis=0)
+BGC_CLASS4_CHL_EAN_BIAS_SURF_BASIN = BGC_CLASS4_CHL_BIAS_SURF_BASIN.mean(axis=0)
+
+
+LIST   =[i for i in range(7)]
+
+LIST[0]=model_TL.Timelist
+LIST[1]=BGC_CLASS4_CHL_RMS_SURF_BASIN
+LIST[2]=BGC_CLASS4_CHL_BIAS_SURF_BASIN
+LIST[3]=MODEL_MEAN
+LIST[4]=SAT___MEAN
+LIST[5]=BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG
+LIST[6]=BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG
+
+fid = open(outfile,'wb')
+pickle.dump(LIST, fid)
+fid.close()
---
+++ validation/multirun/plot_timeseries_RMS.py
@@ -0,0 +1,149 @@
+# OUTPUTS
+# images for QUID
+# CMEMS-Med-QUID-006-008-V2-V1.0.docx
+# Figure IV.3
+
+
+import pickle
+import pylab as pl
+import matplotlib.dates as mdates
+import sys
+import numpy as np
+import argparse
+
+def argument():
+    parser = argparse.ArgumentParser(description = '''
+    plot somethings
+    ''',
+    formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument(   '--outdir', '-o',
+                            type = str,
+                            required =True,
+                            default = "./",
+                            help = ''' Output image dir'''
+                            )
+
+    parser.add_argument(   '--inputfile', '-i',
+                            type = str,
+                            required = True,
+                            default = 'export_data_ScMYValidation_plan.pkl',
+                            help = 'Input pickle file')
+
+
+    return parser.parse_args()
+
+args = argument()
+
+
+
+fid = open(args.inputfile)
+LIST = pickle.load(fid)
+fid.close()
+TIMES                          = LIST[0]
+BGC_CLASS4_CHL_RMS_SURF_BASIN  = LIST[1]
+BGC_CLASS4_CHL_BIAS_SURF_BASIN = LIST[2]
+MODEL_MEAN                     = LIST[3]
+SAT___MEAN                     = LIST[4]
+BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG = LIST[5]
+BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG= LIST[6]
+
+#surf_layer = Layer(0,10)
+
+from basins import OGS
+
+nSUB = len(OGS.P.basin_list)
+
+for isub,sub in enumerate(OGS.P):
+    print sub.name
+    fig, ax = pl.subplots()
+    ax.plot(TIMES,BGC_CLASS4_CHL_RMS_SURF_BASIN[:,isub],'-k',label='RMS')
+    ax.hold(True)
+    ax.plot(TIMES,BGC_CLASS4_CHL_BIAS_SURF_BASIN[:,isub],'-b',label='Bias')
+    ax.set_ylabel(sub.name.upper() + ' - CHL [mg/m$^3$]').set_fontsize(14)
+    ax.legend(loc="best",labelspacing=0, handletextpad=0,borderpad=0.1)
+    leg = pl.gca().get_legend()
+    ltext  = leg.get_texts()
+    pl.setp(ltext,fontsize=12)
+    pl.rc('xtick', labelsize=12)
+    pl.rc('ytick', labelsize=12)
+    pl.ylim(-0.3, 0.3)
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%Y"))
+    ax.grid(True)
+    xlabels = ax.get_xticklabels()
+    pl.setp(xlabels, rotation=30)
+#    #ax.tick_params(direction='left', pad=2)
+    #fig.show()
+    outfilename=args.outdir+"/"+'chl-RMS-BIAS_' + sub.name + ".png"
+    pl.savefig(outfilename)
+    #sys.exit()
+
+start_year=1999
+end___year=2014
+nyr=end___year-start_year+1
+RMS__sum=np.zeros((nyr,11),np.float32)
+RMS__win=np.zeros((nyr,11),np.float32)
+BIAS_sum=np.zeros((nyr,11),np.float32)
+BIAS_win=np.zeros((nyr,11),np.float32)
+RMSL_sum=np.zeros((nyr,11),np.float32)
+RMSL_win=np.zeros((nyr,11),np.float32)
+BIASLsum=np.zeros((nyr,11),np.float32)
+BIASLwin=np.zeros((nyr,11),np.float32)
+#
+# winter = JFMA
+# summer = JJAS
+#
+for i in range(start_year,end___year+1):
+    print i
+    n=i-start_year
+    w1=n*12
+    w2=w1+4
+    s1=w1+5
+    s2=s1+4
+    print w1,w2,s1,s2
+    RMS__win[n,:]=LIST[1][w1:w2,:].mean(axis=0)
+    RMS__sum[n,:]=LIST[1][s1:s2,:].mean(axis=0)
+    BIAS_win[n,:]=LIST[2][w1:w2,:].mean(axis=0)
+    BIAS_sum[n,:]=LIST[2][s1:s2,:].mean(axis=0)
+    RMSL_win[n,:]=LIST[5][w1:w2,:].mean(axis=0)
+    RMSL_sum[n,:]=LIST[5][s1:s2,:].mean(axis=0)
+    BIASLwin[n,:]=LIST[6][w1:w2,:].mean(axis=0)
+    BIASLsum[n,:]=LIST[6][s1:s2,:].mean(axis=0)
+
+mat = np.zeros((8,nSUB))
+
+mat[0,:] = RMS__win[:,:].mean(axis=0)
+mat[1,:] = RMS__sum[:,:].mean(axis=0)
+mat[2,:] = BIAS_win[:,:].mean(axis=0)
+mat[3,:] = BIAS_sum[:,:].mean(axis=0)
+mat[4,:] = RMSL_win[:,:].mean(axis=0)
+mat[5,:] = RMSL_sum[:,:].mean(axis=0)
+mat[6,:] = BIASLwin[:,:].mean(axis=0)
+mat[7,:] = BIASLsum[:,:].mean(axis=0)
+
+mat = mat.T
+print '-----------------------------------------'
+#TABELLA QUID IV.1
+lines=[]
+myformat = '%s ' + '%5.3g '*8 +"\n";
+for isub,sub in enumerate(OGS.P):
+    line = myformat %( sub.name, mat[isub,0], mat[isub,1], mat[isub,2],
+                       mat[isub,3], mat[isub,4], mat[isub,5],
+                       mat[isub,6],mat[isub,7])
+
+    lines.append(line)
+
+outfiletable = args.outdir+"/"+"table.dat"
+file = open(outfiletable,"w")
+file.writelines(lines)
+file.close()
+# RMS__win[:,:].mean(axis=0)
+# print 'RMS_sum : ',RMS__sum[:,:].mean(axis=0)
+# print 'BIAS_win: ',BIAS_win[:,:].mean(axis=0)
+# print 'BIAS_sum: ',BIAS_sum[:,:].mean(axis=0)
+# print 'RMSLwin : ',RMSL_win[:,:].mean(axis=0)
+# print 'RMSLsum : ',RMSL_sum[:,:].mean(axis=0)
+# print 'BIASLwin: ',BIASLwin[:,:].mean(axis=0)
+# print 'BIASLsum: ',BIASLsum[:,:].mean(axis=0)

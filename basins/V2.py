0820cf9b83d65e6a87fb89f7c6653d6f47a5f477
Gianpiero Cossarini <gcossari@node013.pico.cineca.it>
2016-06-24 15:59:33 +0200

    addition of the longname of subbasin in V2.py upgrade of the plots for online validation of chl sup

--- basins/V2.py
+++ basins/V2.py
@@ -7,60 +7,60 @@ alb = SimplePolygonalBasin('alb', alb, 'Alboran Sea')
 
 swm1 = Polygon([-1.0, 5.00,  5.00,-1.00 ],
                [32.0,32.00, 39.50,39.50 ])
-swm1 = SimplePolygonalBasin('swm1', swm1)
+swm1 = SimplePolygonalBasin('swm1', swm1,'South Western Mediterranean west')
 
 swm2 = Polygon([5, 9.25,  9.25, 5 ],
                [32.0,32.00, 39.50,39.5 ])
-swm2 = SimplePolygonalBasin('swm2', swm2)
+swm2 = SimplePolygonalBasin('swm2', swm2,'South Western Mediterranean east')
 
 nwm = Polygon([-1.0,9.25, 9.25,-1],
         [39.5,39.5, 46,46])
-nwm = SimplePolygonalBasin('nwm', nwm)
+nwm = SimplePolygonalBasin('nwm', nwm,'North Western Mediterranean ')
 
 tyr1 = Polygon([09.25,15.00,10,09.25],
                [41.25,41.25,46,46.00])
-tyr1 = SimplePolygonalBasin('tyr1', tyr1)
+tyr1 = SimplePolygonalBasin('tyr1', tyr1,'Northern Tyrrhenian')
 
 
 tyr2 = Polygon([ 9.25, 15.00, 15.00, 15.60,  16.10, 16.50, 15.00, 9.25],
                [36.75, 36.75, 38.00, 38.20,  38.20, 39.50, 41.25, 41.25] )
-tyr2 = SimplePolygonalBasin('tyr2', tyr2)
+tyr2 = SimplePolygonalBasin('tyr2', tyr2,'Southern Tyrrhenian')
 
 ion1 = Polygon([ 9.25, 15.00, 15.00, 10.70,  9.25],
                [32.00, 32.00, 36.75, 36.75, 35.00])
-ion1 = SimplePolygonalBasin('ion1', ion1)
+ion1 = SimplePolygonalBasin('ion1', ion1,'Western Ionian')
 
 ion2 = Polygon([15.00, 21.85, 21.85, 15.00],
                [30.00, 30.00, 36.75, 36.75])
-ion2 = SimplePolygonalBasin('ion2', ion2)
+ion2 = SimplePolygonalBasin('ion2', ion2,'Eastern Ionian')
 
 ion3 = Polygon([15.00, 21.85, 21.85, 18.50, 17.00, 16.10, 16.50, 16.10, 15.60, 15.00],
                [36.75, 36.75, 40.00, 40.00, 41.00, 40.00, 39.50, 38.20, 38.20, 38.00])
-ion3 = SimplePolygonalBasin('ion3', ion3)
+ion3 = SimplePolygonalBasin('ion3', ion3,'Northern Ionian')
 
 adr1 = Polygon([10,13.0,20.0, 15],
                [46,42.5,42.5, 46])
-adr1 = SimplePolygonalBasin('adr1', adr1)
+adr1 = SimplePolygonalBasin('adr1', adr1,'Northern Adriatic')
 
 adr2 = Polygon([ 14.0,20.0,20,18.5,18.0,16.6,13.0],
                [ 42.5,42.5,40,40.0,40.5,41.0,42.5])
-adr2 = SimplePolygonalBasin('adr2', adr2)
+adr2 = SimplePolygonalBasin('adr2', adr2,'Southern Adriatic')
 
 lev1 = Polygon([21.85, 26.25, 26.25, 24.90, 24.00, 21.85 ],
                [30.00, 30.00, 35.10, 35.10, 35.30, 35.30 ]) 
-lev1 = SimplePolygonalBasin('lev1', lev1)
+lev1 = SimplePolygonalBasin('lev1', lev1,'Western Levantine')
 
 lev2 = Polygon([26.25, 33.00, 33.00, 28.00, 28.00, 26.30, 26.25 ],
                [33.60, 33.60, 38.00, 38.00, 35.30, 35.30, 35.28 ]) 
-lev2 = SimplePolygonalBasin('lev2', lev2)
+lev2 = SimplePolygonalBasin('lev2', lev2,'Northern Levantine')
 
 lev3 = Polygon([26.25, 26.25, 33.00, 33.00],
                [30.00, 33.60, 33.60, 30.00]) 
-lev3 = SimplePolygonalBasin('lev3', lev3)
+lev3 = SimplePolygonalBasin('lev3', lev3,'Southern Levantine')
 
 lev4 = Polygon([33.00, 37.00, 37.00, 33.00],
                [30.00, 30.00, 38.00, 38.00]) 
-lev4 = SimplePolygonalBasin('lev4', lev4)
+lev4 = SimplePolygonalBasin('lev4', lev4,'Eastern Levantine')
 
 aeg = Polygon([21.85, 24.00, 24.90, 26.25, 26.25, 26.30, 28.00, 28.00, 21.85],
         [35.30, 35.30, 35.10, 35.10, 35.28, 35.30, 35.30, 42.00, 42.00])
@@ -69,7 +69,7 @@ aeg = SimplePolygonalBasin('aeg', aeg, 'Aegean Sea')
 
 atl  = Polygon([ -5.5,-5.5,-9.0,-9.0], 
                [ 32.0,40.0,40.0,32.0]) 
-atl  = SimplePolygonalBasin('atl', atl)
+atl  = SimplePolygonalBasin('atl', atl,'Atlantic buffer')
 
 med = ComposedBasin('med',[alb ,swm1,swm2,nwm,tyr1,tyr2,adr1,adr2,aeg,ion1,ion2,ion3,lev1,lev2,lev3,lev4],'Mediterranean Sea')
 
--- validation/online/sat_plotter.py
+++ validation/online/sat_plotter.py
@@ -6,7 +6,6 @@ from basins import V2 as OGS
 
 TI_V1 = TimeInterval('20150407','20160607','%Y%m%d')
 TI_V2 = TimeInterval('20160412','20160621','%Y%m%d')
-ARCHIVE_DIR_WRONG="/gpfs/work/IscrC_MYMEDBIO/COPERNICUS/V2"
 ARCHIVE_DIR      ="/gpfs/work/IscrC_MYMEDBIO/COPERNICUS/V2-dev"
 ARCHIVE_V1       ="/gpfs/work/IscrC_MYMEDBIO/COPERNICUS/V4"
 OUTFIG_DIR       ="/pico/home/userexternal/gcossari/COPERNICUS/CATENA/FIG_VALIDATION_ONLINE"
@@ -15,31 +14,23 @@ F0v1    = timelistcontainer(TI_V1,ARCHIVE_V1,'f0')
 F1v1    = timelistcontainer(TI_V1,ARCHIVE_V1,'f1')
 F2v1    = timelistcontainer(TI_V1,ARCHIVE_V1,'f2')
 
-F0v2wrong = timelistcontainer(TI_V2,ARCHIVE_DIR_WRONG,'f0')
-F1v2wrong = timelistcontainer(TI_V2,ARCHIVE_DIR_WRONG,'f1')
-F2v2wrong = timelistcontainer(TI_V2,ARCHIVE_DIR_WRONG,'f2')
-
 F0v2 = timelistcontainer(TI_V2,ARCHIVE_DIR      ,'f0')
 F1v2 = timelistcontainer(TI_V2,ARCHIVE_DIR      ,'f1')
 F2v2 = timelistcontainer(TI_V2,ARCHIVE_DIR      ,'f2')
 
 coast='open_sea'
-for sub in [OGS.alb]:
+#for sub in [OGS.alb]:
+for sub in OGS.P:
     print sub.name
     fig, (ax1, ax2, ax3) = pl.subplots(3, sharex=True, figsize=(15,10)) 
     # BIAS
-    # f0  v2 wrong
+    # f0  v1 
     times, f0v1 = F0v1.plotdata(F0v1.bias, sub.name, coast)
-    ax1.plot(times,f0v1,'b', label='V1 chain' )
-    
-    times, f0_w= F0v2wrong.plotdata(F0v2wrong.bias, sub.name,coast)
-    ax1.plot(times,f0_w,'-r',label='Wr: $1^{st}$ day of forecast (misfit DA d+7)')
-    # f1  v2 wrong 
-    times, f1_w= F1v2wrong.plotdata(F1v2wrong.bias, sub.name,coast)
-    ax1.plot(times,f1_w,'--r',label='Wr: $2^{nd}$ day of forecast')
-    # f2  v2 wrong 
-    times, f2_w= F2v2wrong.plotdata(F2v2wrong.bias, sub.name,coast)
-    ax1.plot(times,f2_w,':r',label='Wr: $3^{rd}$ day of forecast')
+    ax1.plot(times,f0v1,'-b')
+    times, f1v1 = F1v1.plotdata(F1v1.bias, sub.name, coast)
+    ax1.plot(times,f1v1,'--b')
+    times, f2v1 = F2v1.plotdata(F2v1.bias, sub.name, coast)
+    ax1.plot(times,f2v1,':b')
     # f0  v2  
     times, f0= F0v2.plotdata(F0v2.bias, sub.name,coast)
     ax1.plot(times,f0,'-k',label='$1^{st}$ day of forecast (misfit DA d+7)')
@@ -49,25 +40,23 @@ for sub in [OGS.alb]:
     # f2  v2  
     times, f2= F2v2.plotdata(F2v2.bias, sub.name,coast)
     ax1.plot(times,f2,':k',label='$3^{rd}$ day of forecast')
-    ax1.set_title(sub.extended_name,fontsize=14)
+    ax1.set_title(sub.extended_name + " (" + coast + ")",fontsize=14)
     ax1.set_ylabel('BIAS $mg/m^3$', fontsize=14)
-    ax1.legend(bbox_to_anchor=(1.05,1.15), fontsize = 12)
+    ax1.legend(bbox_to_anchor=(.31,1.15), fontsize = 12)
     ax1.xaxis.set_major_locator(mdates.MonthLocator())
     ax1.xaxis.set_major_formatter(mdates.DateFormatter("%m-%Y"))
-        
+    ax1.grid(True)    
     #RMS
-    # f0  v2 wrong 
-    times, f0_w= F0v2wrong.plotdata(F0v2wrong.rmse, sub.name,coast)
-    ax2.plot(times,f0_w,'-r')
-    # f1  v2 wrong 
-    times, f1_w= F1v2wrong.plotdata(F1v2wrong.rmse, sub.name,coast)
-    ax2.plot(times,f1_w,'--r') 
-    # f2  v2 wrong 
-    times, f2_w= F2v2wrong.plotdata(F2v2wrong.rmse, sub.name,coast)
-    ax2.plot(times,f2_w,':r')
+
+    times, f0v1 = F0v1.plotdata(F0v1.rmse, sub.name, coast)
+    ax2.plot(times,f0v1,'-b',label='V1')
+    times, f1v1 = F1v1.plotdata(F1v1.rmse, sub.name, coast)
+    ax2.plot(times,f1v1,'--b')
+    times, f2v1 = F2v1.plotdata(F2v1.rmse, sub.name, coast)
+    ax2.plot(times,f2v1,':b')
     # f0  v2  
     times, f0= F0v2.plotdata(F0v2.rmse, sub.name,coast)
-    ax2.plot(times,f0,'-k') 
+    ax2.plot(times,f0,'-k',label='V2') 
     # f1  v2 
     times, f1= F1v2.plotdata(F1v2.rmse, sub.name,coast)
     ax2.plot(times,f1,'--k')
@@ -75,19 +64,18 @@ for sub in [OGS.alb]:
     times, f2= F2v2.plotdata(F2v2.rmse, sub.name,coast)
     ax2.plot(times,f2,':k')
     ax2.set_ylabel('RMS $mg/m^3$',fontsize=14)
+    ax2.legend(bbox_to_anchor=(.15,1.05), fontsize = 14)
     ax2.xaxis.set_major_locator(mdates.MonthLocator())
     ax2.xaxis.set_major_formatter(mdates.DateFormatter("%m-%Y"))
+    ax2.grid(True)
 
     # n. of valid points
-    # f0  v2 wrong 
-    times, f0_w= F0v2wrong.plotdata(F0v2wrong.number, sub.name,coast)
-    ax3.plot(times,f0_w,'-r')
-    # f1  v2 wrong 
-    times, f1_w= F1v2wrong.plotdata(F1v2wrong.number, sub.name,coast)
-    ax3.plot(times,f1_w,'--r')
-    # f2  v2 wrong 
-    times, f2_w= F2v2wrong.plotdata(F2v2wrong.number, sub.name,coast)
-    ax3.plot(times,f2_w,':r')
+    times, f0v1 = F0v1.plotdata(F0v1.number, sub.name, coast)
+    ax3.plot(times,f0v1,'-b')
+    times, f1v1 = F1v1.plotdata(F1v1.number, sub.name, coast)
+    ax3.plot(times,f1v1,'--b')
+    times, f2v1 = F2v1.plotdata(F2v1.number, sub.name, coast)
+    ax3.plot(times,f2v1,':b')
     # f0  v2  
     times, f0= F0v2.plotdata(F0v2.number, sub.name,coast)
     ax3.plot(times,f0,'-k')
@@ -105,7 +93,7 @@ for sub in [OGS.alb]:
     pl.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
 
     pl.show(block=False)
-    nomefile=OUTFIG_DIR + "/Fig_NRTvalidation_sat_" + sub.name + ".png"
+    nomefile=OUTFIG_DIR + "/NRTvalidation_chlsup_" + sub.name + "_" + coast + ".png"
     fig.savefig(nomefile)
     pl.close(fig)
 

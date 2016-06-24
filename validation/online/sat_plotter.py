c37a39da6b32851a2f7c5e031a04cbf5e377064c
Giorgio Bolzon <gbolzon@inogs.it>
2016-06-24 11:40:03 +0200

    Adding data from chain V1

--- validation/online/sat_plotter.py
+++ validation/online/sat_plotter.py
@@ -4,42 +4,50 @@ import pylab as pl
 import matplotlib.dates as mdates
 from basins import V2 as OGS
 
-TI = TimeInterval('20160401','20160601','%Y%m%d')
+TI_V1 = TimeInterval('20150407','20160607','%Y%m%d')
+TI_V2 = TimeInterval('20160412','20160621','%Y%m%d')
 ARCHIVE_DIR_WRONG="/gpfs/work/IscrC_MYMEDBIO/COPERNICUS/V2"
 ARCHIVE_DIR      ="/gpfs/work/IscrC_MYMEDBIO/COPERNICUS/V2-dev"
+ARCHIVE_V1       ="/gpfs/work/IscrC_MYMEDBIO/COPERNICUS/V4"
 OUTFIG_DIR       ="/pico/home/userexternal/gcossari/COPERNICUS/CATENA/FIG_VALIDATION_ONLINE"
 
-F0v2wrong = timelistcontainer(TI,ARCHIVE_DIR_WRONG,'f0')
-F1v2wrong = timelistcontainer(TI,ARCHIVE_DIR_WRONG,'f1')
-F2v2wrong = timelistcontainer(TI,ARCHIVE_DIR_WRONG,'f2')
+F0v1    = timelistcontainer(TI_V1,ARCHIVE_V1,'f0')
+F1v1    = timelistcontainer(TI_V1,ARCHIVE_V1,'f1')
+F2v1    = timelistcontainer(TI_V1,ARCHIVE_V1,'f2')
 
+F0v2wrong = timelistcontainer(TI_V2,ARCHIVE_DIR_WRONG,'f0')
+F1v2wrong = timelistcontainer(TI_V2,ARCHIVE_DIR_WRONG,'f1')
+F2v2wrong = timelistcontainer(TI_V2,ARCHIVE_DIR_WRONG,'f2')
 
+F0v2 = timelistcontainer(TI_V2,ARCHIVE_DIR      ,'f0')
+F1v2 = timelistcontainer(TI_V2,ARCHIVE_DIR      ,'f1')
+F2v2 = timelistcontainer(TI_V2,ARCHIVE_DIR      ,'f2')
 
-F0v2 = timelistcontainer(TI,ARCHIVE_DIR      ,'f0')
-F1v2 = timelistcontainer(TI,ARCHIVE_DIR      ,'f1')
-F2v2 = timelistcontainer(TI,ARCHIVE_DIR      ,'f2')
-
+coast='open_sea'
 for sub in [OGS.alb]:
     print sub.name
     fig, (ax1, ax2, ax3) = pl.subplots(3, sharex=True, figsize=(15,10)) 
     # BIAS
-    # f0  v2 wrong 
-    times, f0_w= F0v2wrong.plotdata(F0v2wrong.bias, sub.name,'open_sea')
+    # f0  v2 wrong
+    times, f0v1 = F0v1.plotdata(F0v1.bias, sub.name, coast)
+    ax1.plot(times,f0v1,'b', label='V1 chain' )
+    
+    times, f0_w= F0v2wrong.plotdata(F0v2wrong.bias, sub.name,coast)
     ax1.plot(times,f0_w,'-r',label='Wr: $1^{st}$ day of forecast (misfit DA d+7)')
     # f1  v2 wrong 
-    times, f1_w= F1v2wrong.plotdata(F1v2wrong.bias, sub.name,'open_sea')
+    times, f1_w= F1v2wrong.plotdata(F1v2wrong.bias, sub.name,coast)
     ax1.plot(times,f1_w,'--r',label='Wr: $2^{nd}$ day of forecast')
     # f2  v2 wrong 
-    times, f2_w= F2v2wrong.plotdata(F2v2wrong.bias, sub.name,'open_sea')
+    times, f2_w= F2v2wrong.plotdata(F2v2wrong.bias, sub.name,coast)
     ax1.plot(times,f2_w,':r',label='Wr: $3^{rd}$ day of forecast')
     # f0  v2  
-    times, f0= F0v2.plotdata(F0v2.bias, sub.name,'open_sea')
+    times, f0= F0v2.plotdata(F0v2.bias, sub.name,coast)
     ax1.plot(times,f0,'-k',label='$1^{st}$ day of forecast (misfit DA d+7)')
     # f1  v2 
-    times, f1= F1v2.plotdata(F1v2.bias, sub.name,'open_sea')
+    times, f1= F1v2.plotdata(F1v2.bias, sub.name,coast)
     ax1.plot(times,f1,'--k',label='$2^{nd}$ day of forecast')
     # f2  v2  
-    times, f2= F2v2.plotdata(F2v2.bias, sub.name,'open_sea')
+    times, f2= F2v2.plotdata(F2v2.bias, sub.name,coast)
     ax1.plot(times,f2,':k',label='$3^{rd}$ day of forecast')
     ax1.set_title(sub.extended_name,fontsize=14)
     ax1.set_ylabel('BIAS $mg/m^3$', fontsize=14)
@@ -49,22 +57,22 @@ for sub in [OGS.alb]:
         
     #RMS
     # f0  v2 wrong 
-    times, f0_w= F0v2wrong.plotdata(F0v2wrong.rmse, sub.name,'open_sea')
+    times, f0_w= F0v2wrong.plotdata(F0v2wrong.rmse, sub.name,coast)
     ax2.plot(times,f0_w,'-r')
     # f1  v2 wrong 
-    times, f1_w= F1v2wrong.plotdata(F1v2wrong.rmse, sub.name,'open_sea')
+    times, f1_w= F1v2wrong.plotdata(F1v2wrong.rmse, sub.name,coast)
     ax2.plot(times,f1_w,'--r') 
     # f2  v2 wrong 
-    times, f2_w= F2v2wrong.plotdata(F2v2wrong.rmse, sub.name,'open_sea')
+    times, f2_w= F2v2wrong.plotdata(F2v2wrong.rmse, sub.name,coast)
     ax2.plot(times,f2_w,':r')
     # f0  v2  
-    times, f0= F0v2.plotdata(F0v2.rmse, sub.name,'open_sea')
+    times, f0= F0v2.plotdata(F0v2.rmse, sub.name,coast)
     ax2.plot(times,f0,'-k') 
     # f1  v2 
-    times, f1= F1v2.plotdata(F1v2.rmse, sub.name,'open_sea')
+    times, f1= F1v2.plotdata(F1v2.rmse, sub.name,coast)
     ax2.plot(times,f1,'--k')
     # f2  v2  
-    times, f2= F2v2.plotdata(F2v2.rmse, sub.name,'open_sea')
+    times, f2= F2v2.plotdata(F2v2.rmse, sub.name,coast)
     ax2.plot(times,f2,':k')
     ax2.set_ylabel('RMS $mg/m^3$',fontsize=14)
     ax2.xaxis.set_major_locator(mdates.MonthLocator())
@@ -72,22 +80,22 @@ for sub in [OGS.alb]:
 
     # n. of valid points
     # f0  v2 wrong 
-    times, f0_w= F0v2wrong.plotdata(F0v2wrong.number, sub.name,'open_sea')
+    times, f0_w= F0v2wrong.plotdata(F0v2wrong.number, sub.name,coast)
     ax3.plot(times,f0_w,'-r')
     # f1  v2 wrong 
-    times, f1_w= F1v2wrong.plotdata(F1v2wrong.number, sub.name,'open_sea')
+    times, f1_w= F1v2wrong.plotdata(F1v2wrong.number, sub.name,coast)
     ax3.plot(times,f1_w,'--r')
     # f2  v2 wrong 
-    times, f2_w= F2v2wrong.plotdata(F2v2wrong.number, sub.name,'open_sea')
+    times, f2_w= F2v2wrong.plotdata(F2v2wrong.number, sub.name,coast)
     ax3.plot(times,f2_w,':r')
     # f0  v2  
-    times, f0= F0v2.plotdata(F0v2.number, sub.name,'open_sea')
+    times, f0= F0v2.plotdata(F0v2.number, sub.name,coast)
     ax3.plot(times,f0,'-k')
     # f1  v2 
-    times, f1= F1v2.plotdata(F1v2.number, sub.name,'open_sea')
+    times, f1= F1v2.plotdata(F1v2.number, sub.name,coast)
     ax3.plot(times,f1,'--k')
     # f2  v2  
-    times, f2= F2v2.plotdata(F2v2.number, sub.name,'open_sea')
+    times, f2= F2v2.plotdata(F2v2.number, sub.name,coast)
     ax3.plot(times,f2,':k')
     ax3.set_ylabel('# of points',fontsize=14)
     ax3.xaxis.set_major_locator(mdates.MonthLocator())

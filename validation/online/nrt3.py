7a07b265b076ba73cc6a74dea6266e39021b37bc
Giorgio Bolzon <gbolzon@inogs.it>
2016-07-12 11:25:53 +0200

    Adding the development of what to define bias and rmse for every subbasin and layer minor bug fix

--- validation/online/nrt3.py
+++ validation/online/nrt3.py
@@ -1,10 +1,12 @@
 from commons import timerequestors
 from basins import V2 as OGS
 from instruments import bio_float
+from instruments.var_conversions import FLOATVARS
 from instruments.matchup_manager import Matchup_Manager
 from commons.mask import Mask
 from commons.layer import Layer
 import numpy as np
+from matchup.statistics import matchup
 
 
 R=timerequestors.Weekly_req(2016,6,28)
@@ -13,7 +15,7 @@ BASEDIR="/gpfs/work/IscrC_MYMEDBIO/COPERNICUS/bit.sea/validation/online/NRT3/"
 INPUTDIR="/pico/home/usera07ogs/a07ogs00/OPA/V2C/wrkdir/2/POSTPROC/AVE_FREQ_1/online_validation/PREVIOUS/TMP/"
 
 LAYERLIST=[Layer(0,10), Layer(10,30), Layer(30,60), Layer(60,100), Layer(100,150), Layer(150,300), Layer(300,600), Layer(600,1000)]
-VARLIST = ['P_i','N3n','O2o']
+VARLIST = ['P_l','N3n','O2o']
 nSub   = len(OGS.NRT3.basin_list)
 nDepth = len(LAYERLIST)
 nVar   = len(VARLIST)
@@ -30,16 +32,27 @@ M = Matchup_Manager(TI,INPUTDIR,BASEDIR)
 
 for ivar, var in enumerate(VARLIST):
     for isub, sub in enumerate(OGS.NRT3):
-        Profilelist = bio_float.FloatSelector(var, TI, sub)
+        Profilelist = bio_float.FloatSelector(FLOATVARS[var], TI, sub)
         nProfiles = len(Profilelist)
         Matchup_object_list=[]
         for ip in range(nProfiles):
-            matchup =  M.getMatchups([Profilelist[ip]], TheMask.zlevels, var, read_adjusted=True)
-            Matchup_object_list.append(matchup) 
-        
+            floatmatchup =  M.getMatchups([Profilelist[ip]], TheMask.zlevels, var, read_adjusted=True)
+            Matchup_object_list.append(floatmatchup)
+
         for ilayer, layer in enumerate(LAYERLIST):
-            for matchup in Matchup_object_list:
-                m_layer = matchup.subset(layer)
-                
-            BIAS[ivar,isub,ilayer] = ''
+            Matchup_object_list_layer=[]
+            MODEL = []
+            REF   = []
+            for floatmatchup in Matchup_object_list:
+                m_layer = floatmatchup.subset(layer)
+                if m_layer.number() > 0:
+                    Matchup_object_list_layer.append(m_layer)
+                    REF.append(m_layer.Ref.mean())
+                    MODEL.append(m_layer.Model.mean())
+
+            NPOINTS[ivar,isub,ilayer] = len(MODEL)
+            if len(MODEL) > 0:
+                M_LAYER = matchup(np.array(MODEL), np.array(REF))
+                BIAS[ivar,isub,ilayer] = M_LAYER.bias()
+                RMSE[ivar,isub,ilayer] = M_LAYER.RMSE()
         

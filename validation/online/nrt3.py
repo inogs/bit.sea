8f08e93226b27da3931dd9b7d984aa02434d7e18
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2016-07-12 16:13:17 +0200

    adding use of argparse adding .sh file caller

--- validation/online/nrt3.py
+++ validation/online/nrt3.py
@@ -1,3 +1,45 @@
+import argparse
+
+def argument():
+    parser = argparse.ArgumentParser(description = '''
+    Generates float validation data on a single week, from one single chain run.
+    Week is centered on tuesday.
+    Produces a single file, containing bias, rmse and number of measurments for each subbasin and layer
+    for chlorophyll, nitrate and oxygen.
+    In this approach we define the measurement as mean on layer of the float profile values.
+    ''', formatter_class=argparse.RawTextHelpFormatter)
+
+
+    parser.add_argument(   '--date','-d',
+                                type = str,
+                                required = True,
+                                help = 'Date in yyyymmdd format, corresponding to the tuesday = rundate - 7 days')
+
+    parser.add_argument(   '--basedir','-b',
+                                type = str,
+                                required = True,
+                                help = 'dir containing PROFILES/')
+    parser.add_argument(   '--inputdir','-i',
+                                type = str,
+                                required = True,
+                                help = 'dir containg big ave files')
+
+    parser.add_argument(   '--maskfile', '-m',
+                                type = str,
+                                default = "/pico/home/usera07ogs/a07ogs00/OPA/V4/etc/static-data/MED1672_cut/MASK/meshmask.nc",
+                                required = False,
+                                help = ''' Path of maskfile''')
+
+    parser.add_argument(   '--outfile', '-o',
+                                type = str,
+                                default = None,
+                                required = True,
+                                help = "")
+
+    return parser.parse_args()
+
+args = argument()
+
 from commons import timerequestors
 from basins import V2 as OGS
 from instruments import bio_float
@@ -7,52 +49,82 @@ from commons.mask import Mask
 from commons.layer import Layer
 import numpy as np
 from matchup.statistics import matchup
+import datetime
+import scipy.io.netcdf as NC
+from commons.utils import addsep
 
-
-R=timerequestors.Weekly_req(2016,6,28)
-TheMask = Mask('/pico/home/usera07ogs/a07ogs00/OPA/V4/etc/static-data/MED1672_cut/MASK/meshmask.nc')
-BASEDIR="/gpfs/work/IscrC_MYMEDBIO/COPERNICUS/bit.sea/validation/online/NRT3/"
-INPUTDIR="/pico/home/usera07ogs/a07ogs00/OPA/V2C/wrkdir/2/POSTPROC/AVE_FREQ_1/online_validation/PREVIOUS/TMP/"
+TheMask  = Mask(args.maskfile)
+BASEDIR =  addsep(args.basedir)
+INPUTDIR = addsep(args.inputdir)
+outfile  = args.outfile
+datestr  = args.date
+d = datetime.datetime.strptime(datestr,'%Y%m%d')
+R=timerequestors.Weekly_req(d.year,d.month,d.day)
 
 LAYERLIST=[Layer(0,10), Layer(10,30), Layer(30,60), Layer(60,100), Layer(100,150), Layer(150,300), Layer(300,600), Layer(600,1000)]
 VARLIST = ['P_l','N3n','O2o']
+read_adjusted = [True,False,False]
 nSub   = len(OGS.NRT3.basin_list)
 nDepth = len(LAYERLIST)
 nVar   = len(VARLIST)
 
 BIAS    = np.zeros((nVar,nSub,nDepth), np.float32)
 RMSE    = np.zeros((nVar,nSub,nDepth), np.float32)
-NPOINTS = np.zeros((nVar,nSub,nDepth), np.float32)
+NPOINTS = np.zeros((nVar,nSub,nDepth), np.int32)
 
 TI =R.time_interval
 M = Matchup_Manager(TI,INPUTDIR,BASEDIR)
 
 
-
-
 for ivar, var in enumerate(VARLIST):
+    print var
     for isub, sub in enumerate(OGS.NRT3):
         Profilelist = bio_float.FloatSelector(FLOATVARS[var], TI, sub)
         nProfiles = len(Profilelist)
+        print sub.name, nProfiles
         Matchup_object_list=[]
         for ip in range(nProfiles):
-            floatmatchup =  M.getMatchups([Profilelist[ip]], TheMask.zlevels, var, read_adjusted=True)
+            floatmatchup =  M.getMatchups([Profilelist[ip]], TheMask.zlevels, var, read_adjusted=read_adjusted[ivar])
             Matchup_object_list.append(floatmatchup)
 
         for ilayer, layer in enumerate(LAYERLIST):
-            Matchup_object_list_layer=[]
-            MODEL = []
-            REF   = []
+            MODEL_LAYER_MEAN = [] # one value for each suitable profile in (subbasin, layer)
+            REF_LAYER_MEAN   = []
             for floatmatchup in Matchup_object_list:
                 m_layer = floatmatchup.subset(layer)
+                #print ilayer, m_layer.number()
                 if m_layer.number() > 0:
-                    Matchup_object_list_layer.append(m_layer)
-                    REF.append(m_layer.Ref.mean())
-                    MODEL.append(m_layer.Model.mean())
+                    REF_LAYER_MEAN.append(m_layer.Ref.mean())
+                    MODEL_LAYER_MEAN.append(m_layer.Model.mean())
 
-            NPOINTS[ivar,isub,ilayer] = len(MODEL)
-            if len(MODEL) > 0:
-                M_LAYER = matchup(np.array(MODEL), np.array(REF))
+            NPOINTS[ivar,isub,ilayer] = len(MODEL_LAYER_MEAN)
+            if len(MODEL_LAYER_MEAN) > 0:
+                M_LAYER = matchup(np.array(MODEL_LAYER_MEAN), np.array(REF_LAYER_MEAN))
                 BIAS[ivar,isub,ilayer] = M_LAYER.bias()
                 RMSE[ivar,isub,ilayer] = M_LAYER.RMSE()
-        
+
+
+
+ncOUT = NC.netcdf_file(outfile,'w')
+
+ncOUT.createDimension('var', nVar)
+ncOUT.createDimension('sub', nSub)
+ncOUT.createDimension('depth',nDepth)
+s=''
+for var in VARLIST: s= s+var + ","
+setattr(ncOUT, 'varlist',s[:-1])
+s='';
+for sub in OGS.NRT3: s =s+sub.name + ","
+setattr(ncOUT,'sublist',s[:-1])
+s='';
+for layer in LAYERLIST: s =s+layer.string() + ","
+setattr(ncOUT,'layerlist',s[:-1])
+
+ncvar=ncOUT.createVariable('bias', 'f', ('var','sub','depth'))
+ncvar[:] = BIAS
+ncvar=ncOUT.createVariable('rmse', 'f', ('var','sub','depth'))
+ncvar[:] = RMSE
+ncvar=ncOUT.createVariable('npoints', 'i', ('var','sub','depth'))
+ncvar[:] = NPOINTS
+
+ncOUT.close()
---
+++ validation/online/nrt3.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+MASKFILE=/pico/home/usera07ogs/a07ogs00/OPA/V4/etc/static-data/MED1672_cut/MASK/meshmask.nc
+LOC=/pico/home/usera07ogs/a07ogs00/OPA/V2C-dev/wrkdir/2/POSTPROC/AVE_FREQ_1/online_validation
+INPUTDIR=${LOC}/PREVIOUS/TMP/
+PREV_DIR=${LOC}/PREVIOUS/PROFILATORE/PROFILES
+ACTU_DIR=${LOC}/ACTUAL/PROFILATORE/PROFILES
+
+BASEDIR=NRT3
+PROFILES_DIR=${BASEDIR}/PROFILES
+mkdir -p $PROFILES_DIR
+rm -f $PROFILES_DIR/*nc
+cp $PREV_DIR/*nc $PROFILES_DIR
+cp $ACTU_DIR/*nc $PROFILES_DIR #overwriting old one
+
+python nrt3.py -o pippo.nc -b $BASEDIR -m $MASKFILE -i $INPUTDIR -d 20160628
+

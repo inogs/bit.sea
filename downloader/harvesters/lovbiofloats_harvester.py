236f865b3974f7698e9a663c46ebad5451e81118
Giorgio Bolzon <gbolzon@inogs.it>
2016-09-20 09:35:07 +0200

    Working state for http dowloader from LOV

--- downloader/harvesters/lovbiofloats_harvester.py
+++ downloader/harvesters/lovbiofloats_harvester.py
@@ -3,23 +3,104 @@
 from __future__ import print_function
 
 import sys
-
+import os
 import urllib2
 from os.path import join, exists, realpath, dirname
-from os import listdir, remove
+from os import remove, rename
 from xml.dom.minidom import parseString
 
 import xml.etree.ElementTree as xml_tree
 
 from harvester_interface import HarvesterInterface
-from utilities.ftp_utilities import list_files, download_file
 from utilities.files_and_dirs import ensure_dir
 from utilities.date_and_time import now_as_string
 from vlfr import username, password
+import numpy as np
 
 http_url = "http://www.oao.obs-vlfr.fr/BD_FLOAT/NETCDF/"
-
 relative_path = "FLOAT_LOVBIO"
+
+manager = urllib2.HTTPPasswordMgrWithDefaultRealm()        
+manager.add_password(None, http_url, username, password)
+
+auth = urllib2.HTTPBasicAuthHandler(manager)
+opener = urllib2.build_opener(auth)
+urllib2.install_opener(opener)
+
+
+class ErrorMessage(object):
+    """A class to handle error messages"""
+    def __init__(self):
+        # self.message contains the error
+        self.message = None
+def download_file(url, f, path, log, perms=None,
+                  skip_if_exists=True, skip_is_strange = False):
+    """
+    Download a file from an ftp remote archive.
+    
+    Args:
+        - *connection*: An ftp connection, created with the ftplib library
+        - *f*: The name of the file as a string
+        - *path*: The directory where the file will be saved
+        - *log*: A Log object to report the operations to the user
+        - *perms* (optional): A dictionary of permissions like the one the
+          list_file function returns. If this dictionary is not none, the
+          file will be downloaded only if the readable flag for others is
+          present.
+        - *skip_if_exists*: A boolean value that says what to do if the file
+          is already present. Default is True.
+        - *skip_is_strange*: This means that the fact that the file is already
+          present shall be logged with high severity. If skip_if_exists is
+          False it has no effect. Default is False.
+    
+    Returns:
+        *Success*: A boolean value that reports if the downloade
+        was executed or if it was skipped for some reasons
+    
+    Raises:
+        *DownloadError* if something went wrong during the download
+    """
+    outfile = join(path, f)
+    if skip_if_exists:
+        # Check if the file already exists
+        if exists(outfile):
+            if skip_is_strange:
+                log.info('Skipping file ' + f + ' because'
+                         ' it was already downloaded!')
+            else:
+                log.debug('Skipping file ' + f + ' because '
+                          'it was already downloaded')
+            return False
+
+    # Check if the file is readable
+    if perms is not None:
+        if 'read' not in perms[f]['others']:
+            log.info('The file ' + f + ' is not '
+                     'readable. Check the '
+                     'permissions')
+            return False
+ 
+    # Download the file
+    log.debug('Downloading ' + f + '...')
+    
+    # I will first save the partial file with a temp name. When the
+    # transfers end, I will rename it
+
+    temp_name = join(path, 'incomplete_download.tmp')
+    response = urllib2.urlopen(url)
+    F = open(temp_name,'wb')
+    F.write(response.read())
+    F.close()
+
+    # Move the temp file in the new downloaded file
+    rename(temp_name, outfile)
+    
+    log.debug('File ' + f + ' downloaded!')
+    return True
+
+
+
+
 wmo_file = realpath(dirname(realpath(__file__)) + "/../harvesters_info/wmo.txt")
 xml_path = realpath(dirname(realpath(__file__)) + '/../harvesters_xml')
 
@@ -55,17 +136,11 @@ class LovBioFloatsHarvester(HarvesterInterface):
 
         # Read the wmo file line by line (exclude the first one because
         # it does not contain data)
-        with open(wmo_file, 'r') as wmo_info:
-            wmo_list = [l.split() for l in wmo_info.readlines()[1:]]
-        # Put its content in a dictionary
-        wmo_status = dict()
-        for l in wmo_list:
-            name = l[1]
-            status = l[-1]
-            wmo_status[name] = status
-
-        active_floats = [f for f in wmo_status if wmo_status[f]=='A']
-        dead_floats = [f for f in wmo_status if wmo_status[f]=='D']
+        FLOAT_dtype=[('id',np.int),('wmo',np.int),('id_type',np.int),('type','S20'),('nome_fs','S20'),('status','S1')]
+        A=np.loadtxt(wmo_file,dtype=FLOAT_dtype,skiprows=1,delimiter="\t")
+        lines_active_floats=np.where(A['status']=='A')
+        lines_dead__floats =np.where(A['status']=='D')
+        
 
         # Now we need the xml file that keeps what we did on the
         # last updates
@@ -90,74 +165,63 @@ class LovBioFloatsHarvester(HarvesterInterface):
         ensure_dir(path, log, expected=True)
 
         # Open the connection with the remote archive
-        manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        
-        manager.add_password(None, http_url, username, password)
 
-        auth = urllib2.HTTPBasicAuthHandler(manager)
-        opener = urllib2.build_opener(auth)
-        urllib2.install_opener(opener)
         
-        
-
         # Enter in the directory tree
         
 
         # Download data for every active float
-        for f in active_floats:
+        for l in lines_active_floats:
             # Update the xml with the current status of the float
+            f = A[l]['wmo']
             f_in_xml = root.findall('wmo_' + str(f))
             if len(f_in_xml) == 0:
                 f_node = xml_tree.SubElement(root, 'wmo_' + str(f))
-                f_node.set('status', 'A')
             else:
                 f_node = [fn for fn in f_in_xml if fn.tag=='wmo_'+str(f)][0]
-                f_node.set('status', 'A')
+            f_node.set('status', 'A')
 
             try:
-                connection.cwd(f)
+                floatname = A[l]['nome_fs']
+                urlfilelist = http_url + floatname +  "/liste_all"
+                print(urlfilelist)
+                response = urllib2.urlopen(urlfilelist)
             except:
                 log.info('No directory associated with file ' + str(f) +
                          '. This file will be skipped!')
                 continue
-
-            _, float_dirs, _ = list_files(connection)
+            
+            
+            remotepathlist = response.read().rsplit("\n")[:-1]
+            filelist=[os.path.basename(f) for f in remotepathlist]
             # Now I look for the profiles dir. This is the folder
             # where all the data are stored
-            if 'profiles' in float_dirs:
-                connection.cwd('profiles')
-                float_files, _, perms = list_files(connection)
-                min_len = len(f) + 2
-                to_be_downloaded = [ff for ff in float_files
-                                    if len(ff)>min_len
-                                    and ff[:min_len]=='MR'+f]
-                if len(to_be_downloaded) > 0:
-                    download_for_f = []
-                    # Copy all file in a local dir with the same name
-                    # skipping the one that we already have
-                    float_local_dir = join(path, f)
-                    ensure_dir(float_local_dir, log, expected = False)
-                    for ff in to_be_downloaded:
-                        d = download_file(connection, ff, float_local_dir,
-                                          log, perms, True)
-                        # If the file was downloaded without any problem,
-                        # add it to the list of downloaded files
-                        if d:
-                            downloaded.append(ff)
-                            download_for_f.append(ff)
-                    if len(download_for_f) == 0:
-                        log.info('No updates found for float ' + str(f))                    
-                else:
-                    log.info('No updates found for float ' + str(f))
-                connection.cwd('..')
+
+
+            if len(filelist) > 0:
+                download_for_f = []
+                # Copy all file in a local dir with the same name
+                # skipping the one that we already have
+                float_local_dir = join(path, f)
+                ensure_dir(float_local_dir, log, expected = False)
+                for ff in filelist:
+                    url = http_url + floatname + "/" + ff
+                    d = download_file(url, ff, float_local_dir,
+                                      log, None, True)
+                    # If the file was downloaded without any problem,
+                    # add it to the list of downloaded files
+                    if d:
+                        downloaded.append(ff)
+                        download_for_f.append(ff)
+                if len(download_for_f) == 0:
+                    log.info('No updates found for float ' + str(f))                    
             else:
-                log.info('Float ' + f + ' does not contain a profile '
-                         'dir inside its directory. No data will be '
-                         'downloaded for this float')
-            connection.cwd('..')
+                log.info('No updates found for float ' + str(f))
 
 
-        for f in dead_floats:
+
+        for l in lines_dead__floats:
+            f = A[l]['wmo']
             to_be_updated = False
             # Update the xml with the current status of the float
             # Check if it must be updated
@@ -167,14 +231,13 @@ class LovBioFloatsHarvester(HarvesterInterface):
                 # and it will be updated
                 to_be_updated = True
                 f_node = xml_tree.SubElement(root, 'wmo_' + str(f))
-                f_node.set('status', 'D')
             else:
                 f_node = [fn for fn in f_in_xml if fn.tag=='wmo_'+str(f)][0]
                 # If I already know this float, but the last time it
                 # was not dead, update it
                 if f_node.get('status') != 'D':
                     to_be_updated = True
-                f_node.set('status', 'D')
+            f_node.set('status', 'D')
             
             if not to_be_updated:
                 log.debug("Wmo " + str(f) + " is dead and will not be updated")
@@ -185,41 +248,38 @@ class LovBioFloatsHarvester(HarvesterInterface):
 
             if to_be_updated:
                 try:
-                    connection.cwd(f)
+                    floatname = A[l]['nome_fs']
+                    urlfilelist = http_url + floatname +  "/liste_all"
+                    response = urllib2.urlopen(urlfilelist)
                 except:
                     log.info('No directory associated with file ' + str(f) +
                              '. This file will be skipped!')
                     continue
 
-                _, float_dirs, _ = list_files(connection)
+                remotepathlist = response.read().rsplit("\n")[:-1]
+                filelist=[os.path.basename(f) for f in remotepathlist]
                 # Now I look for the profiles dir. This is the folder
                 # where all the data are stored
-                if 'profiles' in float_dirs:
-                    connection.cwd('profiles')
-                    float_files, _, perms = list_files(connection)
-                    min_len = len(f) + 2
-                    to_be_downloaded = [ff for ff in float_files
-                                        if len(ff)>min_len
-                                        and ff[:min_len]=='MR'+f]
-                    if len(to_be_downloaded) > 0:
-                        # Copy all file in a local dir with the same name
-                        # skipping the one that we already have
-                        float_local_dir = join(path, f)
-                        ensure_dir(float_local_dir, log, expected = False)
-                        for ff in to_be_downloaded:
-                            d = download_file(connection, ff, float_local_dir,
-                                              log, perms, True)
-                            # If the file was downloaded without any problem,
-                            # add it to the list of downloaded files
-                            if d:
-                                downloaded.append(ff)
-                    connection.cwd('..')
+                if len(filelist) > 0:
+                    download_for_f = []
+                    # Copy all file in a local dir with the same name
+                    # skipping the one that we already have
+                    float_local_dir = join(path, f)
+                    ensure_dir(float_local_dir, log, expected = False)
+                    for ff in filelist:
+                        url = http_url + floatname + "/" + ff
+                        d = download_file(url, ff, float_local_dir,
+                                          log, None, True)
+                        # If the file was downloaded without any problem,
+                        # add it to the list of downloaded files
+                        if d:
+                            downloaded.append(ff)
+                    if len(download_for_f) == 0:
+                        log.info('No updates found for float ' + str(f))                    
                 else:
                     log.info('Float ' + f + ' does not contain a profile '
                              'dir inside its directory. No data will be '
                              'downloaded for this float')
-                connection.cwd('..')
-        connection.quit()
 
         # Save the XML file
         root.set('Updated', now_as_string())
@@ -253,18 +313,14 @@ class LovBioFloatsHarvester(HarvesterInterface):
         """
         # In the following list I will store the name of the
         # files that will be downloaded or updated
+        print("REBUILD")
         downloaded = []
 
         # Read the wmo file line by line (exclude the first one because
         # it does not contain data)
-        with open(wmo_file, 'r') as wmo_info:
-            wmo_list = [l.split() for l in wmo_info.readlines()[1:]]
-        # Put its content in a dictionary
-        wmo_status = dict()
-        for l in wmo_list:
-            name = l[1]
-            status = l[-1]
-            wmo_status[name] = status
+        FLOAT_dtype=[('id',np.int),('wmo','S20'),('id_type',np.int),('type','S20'),('nome_fs','S20'),('status','S1')]
+        A=np.loadtxt(wmo_file,dtype=FLOAT_dtype,skiprows=1,delimiter="\t")
+
 
         # Delete, if present, the XML files with all the floats
         xml_file = join(xml_path, self.__class__.__name__ + '.xml')
@@ -281,66 +337,62 @@ class LovBioFloatsHarvester(HarvesterInterface):
         ensure_dir(path, log, expected=False)
 
         # Open the connection with the remote archive
-        connection = FTP(ftp_url)
-        connection.login()
-
-        # Enter in the directory tree
-        connection.cwd('ifremer/argo/dac/coriolis')
 
         # Download data for every active float
-        for f in wmo_status:
-
+        for l in range(len(A)):
+            f = A[l]['wmo']
+            if A[l]['wmo'] == A[l]['nome_fs']: continue
+            if A[l]['nome_fs']=='NULL ': continue
             # Update the xml with the current status of the float
             f_in_xml = root.findall('wmo_' + str(f))
             if len(f_in_xml) == 0:
                 f_node = xml_tree.SubElement(root, 'wmo_' + str(f))
-                f_node.set('status', wmo_status[f])
+
             else:
                 f_node = [fn for fn in f_in_xml if fn.tag=='wmo_'+str(f)][0]
-                f_node.set('status', wmo_status[f])
+            f_node.set('status', A[l]['status'])
 
             try:
-                connection.cwd(f)
+                floatname = A[l]['nome_fs']
+                urlfilelist = http_url + floatname +  "/liste_all"
+                print(urlfilelist)
+                response = urllib2.urlopen(urlfilelist)
             except:
-                log.info('No directory associated with file ' + str(f) +
+                log.info('Cannot download file ' + urlfilelist +
                          '. This file will be skipped!')
                 continue
 
-            _, float_dirs, _ = list_files(connection)
+            remotepathlist = response.read().rsplit("\n")[:-1]
+            filelist=[os.path.basename(fn) for fn in remotepathlist]
             # Now I look for the profiles dir. This is the folder
             # where all the data are stored
-            if 'profiles' in float_dirs:
-                connection.cwd('profiles')
-                float_files, _, perms = list_files(connection)
-                min_len = len(f) + 2
-                to_be_downloaded = [ff for ff in float_files
-                                    if len(ff)>min_len
-                                    and ff[:min_len]=='MR'+f]
-                if len(to_be_downloaded) > 0:
-                    download_for_f = []
-                    # Copy all file in a local dir with the same name
-                    float_local_dir = join(path, f)
-                    ensure_dir(float_local_dir, log, expected = False)
-                    for ff in to_be_downloaded:
-                        d = download_file(connection, ff, float_local_dir,
-                                          log, perms, skip_if_present)
-                        # If the file was downloaded without any problem,
-                        # add it to the list of downloaded files
-                        if d:
-                            downloaded.append(ff)
-                            download_for_f.append(ff)
-                    if len(download_for_f) == 0:
-                        log.info('No updates found for float ' + str(f))                    
-                else:
-                    log.info('No updates found for float ' + str(f))
-                connection.cwd('..')
-            else:
-                log.info('Float ' + f + ' does not contain a profile '
-                         'dir inside its directory. No data will be '
-                         'downloaded for this float')
-            connection.cwd('..')
+            if len(filelist) > 0:
+                download_for_f = []
+                # Copy all file in a local dir with the same name
+                # skipping the one that we already have
+                print("BBBBB")
+                print(path)
+                print(f)                
+                float_local_dir = join(path, f)
 
-        connection.quit()
+                print(float_local_dir)
+                ensure_dir(float_local_dir, log, expected = False)
+                for ff in filelist:
+                    url = http_url + floatname + "/" + ff
+                    d = download_file(url, ff, float_local_dir,
+                                      log, None, True)
+                    # If the file was downloaded without any problem,
+                    # add it to the list of downloaded files
+                    if d:
+                        downloaded.append(ff)
+                        download_for_f.append(ff)
+                if len(download_for_f) == 0:
+                    log.info('No updates found for float ' + str(f))                    
+            else:
+                log.info('No updates found for float ' + str(f))
+
+
+
         
         # Save the XML file
         xml_as_string = xml_tree.tostring(root)

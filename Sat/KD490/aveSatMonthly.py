c9e04f88d09a3ba497bf6dabe5c2316ad6720a03
Giorgio Bolzon <gbolzon@inogs.it>
2016-11-30 09:59:35 +0100

    Adatpting aveSatMonthly.py and adding check_climatology

--- Sat/KD490/aveSatMonthly.py
+++ Sat/KD490/aveSatMonthly.py
@@ -1,30 +1,29 @@
 from commons.Timelist import TimeList
 from commons.time_interval import TimeInterval
-from commons import IOnames
 import numpy as np
 import os
 import Sat.SatManager as Sat
 
-CHECKDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/CCI/DAILY/CHECKED/"
-MONTHLYDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/CCI/MONTHLY_V4/"
+CHECKDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/KD490/DAILY/CHECKED/"
+MONTHLYDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/KD490/MONTHLY/ORIGMESH/"
 
 reset = False
 
-Timestart="19990102"
-Time__end="20140101"
+Timestart="19990101"
+Time__end="20160101"
 TI = TimeInterval(Timestart,Time__end,"%Y%m%d")
 TLCheck = TimeList.fromfilenames(TI, CHECKDIR,"*.nc",prefix='',dateformat='%Y%m%d')
-IonamesFile = '../postproc/IOnames_sat.xml'
-IOname = IOnames.IOnames(IonamesFile)
-
+#IonamesFile = '../../postproc/IOnames_sat.xml'
+#IOname = IOnames.IOnames(IonamesFile)
+suffix = os.path.basename(TLCheck.filelist[0])[8:]
 MONTHLY_reqs=TLCheck.getMonthlist()
 
-jpi = Sat.NativeMesh.jpi
-jpj = Sat.NativeMesh.jpj
+jpi = Sat.masks.KD490mesh.jpi
+jpj = Sat.masks.KD490mesh.jpj
 
 
 for req in MONTHLY_reqs:
-    outfile = req.string + IOname.Output.suffix + ".nc"
+    outfile = req.string + suffix
     outpathfile = MONTHLYDIR + outfile
     conditionToSkip = (os.path.exists(outpathfile)) and (not reset)
 
@@ -36,8 +35,7 @@ for req in MONTHLY_reqs:
     M = np.zeros((nFiles,jpj,jpi),np.float32)
     for iFrame, j in enumerate(ii):
         inputfile = TLCheck.filelist[j]
-        CHL = Sat.readfromfile(inputfile)
+        CHL = Sat.readfromfile(inputfile,'KD490')
         M[iFrame,:,:] = CHL
-    CHL_OUT = Sat.logAverager(M)
-    Sat.dumpV4file(outpathfile, CHL_OUT)
-
+    Kext_OUT = Sat.averager(M)
+    Sat.dump_KD490_nativefile(outpathfile, Kext_OUT)

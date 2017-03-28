f1a3cad252395ee1874281b40df21e7b28c76a0a
Giorgio Bolzon <gbolzon@inogs.it>
2017-03-28 10:29:32 +0200

    Parallelization

--- Sat/CHL/aveSat10days.py
+++ Sat/CHL/aveSat10days.py
@@ -4,6 +4,16 @@ from commons import IOnames
 import numpy as np
 import os
 import SatManager as Sat
+try:
+    from mpi4py import MPI
+    comm  = MPI.COMM_WORLD
+    rank  = comm.Get_rank()
+    nranks =comm.size
+    isParallel = True
+except:
+    rank   = 0
+    nranks = 1
+    isParallel = False
 
 CHECKDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/CCI_1km/DAILY/CHECKED/"
 OUTDIR="/pico/home/userexternal/pdicerbo/WorkDir/AveSat24/Checked_10Days_Sat1km/"
@@ -24,7 +34,7 @@ jpj = Sat.NativeMesh.jpj
 
 counter = 0
 
-for req in TenDaysReqs:
+for req in TenDaysReqs[rank::nranks]:
     counter = counter + 1
     
     outfile = req.string + IOname.Output.suffix + ".nc"

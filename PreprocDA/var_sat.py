3da84d382a86e8e445d052733ed58af5e0519cbb
pdicerbo <pierluigi.dicerbo@gmail.com>
2017-04-04 17:57:22 +0200

    adding var_sat.py source file

---
+++ PreprocDA/var_sat.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def var_sat_CCI_10gg(limstd, dayF, MyMesh, INDIR, OUTDIR):
+  """
+  Computes the Monthly surface variance of the 10-days averages
+  The inputs file are within INDIR directory and the outputs will be placed
+  within OUTDIR
+
+  The mesh must be consistent with the data contained in INDIR
+  """
+
+
+if __name__ == "__main__":
+
+    from postproc.masks import Mesh24
+    print "pippo"
+

70dd6581e40b35b82fabe525c6c5e7d615300265
pdicerbo <pierluigi.dicerbo@gmail.com>
2017-04-06 11:38:47 +0200

    variance computation almost done

--- PreprocDA/var_sat.py
+++ PreprocDA/var_sat.py
@@ -1,6 +1,7 @@
 import numpy as np
 from commons.Timelist import TimeList
 from commons.time_interval import TimeInterval
+from commons.timerequestors import Clim_month
 import SatManager as Sat
 
 def var_sat_CCI_10gg(limstd, dayF, MyMesh, INDIR, OUTDIR):
@@ -17,31 +18,37 @@ def var_sat_CCI_10gg(limstd, dayF, MyMesh, INDIR, OUTDIR):
 
   TI = TimeInterval(Timestart,Time__end,"%Y%m%d")
   TLCheck = TimeList.fromfilenames(TI, INDIR,"*.nc",prefix='',dateformat='%Y%m%d')
-  MONTHLY_reqs=TLCheck.getMonthlist()
+  MONTHLY_reqs=[Clim_month(i) for i in range(1,13)]
 
   print "number of requests: ", len(MONTHLY_reqs), " inputFreq: ", TLCheck.inputFrequency
 
   jpi = MyMesh.jpi
   jpj = MyMesh.jpj
 
-  Chl = np.zeros((jpi,jpj,12), dtype=float)
-  ChlSquare = np.zeros((jpi,jpj,12), dtype=float)
+  Chl = np.zeros((12,jpj,jpi), dtype=float)
+  ChlSquare = np.zeros((12,jpj,jpi), dtype=float)
+  Var2D = np.zeros((12,jpj,jpi), dtype=float)
 
   for req in MONTHLY_reqs:
     ii, w = TLCheck.select(req)
+    print ii, "\n"
+  
+    nFiles = len(ii)
+    M  = np.zeros((nFiles,jpj,jpi),np.float32)
+    M2 = np.zeros((nFiles,jpj,jpi),np.float32)
     
-    # print req," ii = ", ii, " w = ", w
-    if req.year == 2015 and req.month == 11:
+    for iFrame, j in enumerate(ii):
+        inputfile = TLCheck.filelist[j]
+        CHL = Sat.readfromfile(inputfile,'CHL')
+        M[iFrame,:,:]  = CHL
+        M2[iFrame,:,:] = CHL*CHL
+    
+    MonthIndex = req.month-1
+    Chl[MonthIndex,:,:] = Sat.WeightedAverager(M,w)
+    ChlSquare[MonthIndex,:,:] = Sat.WeightedAverager(M2,w)
 
-      nFiles = len(ii)
-      M = np.zeros((nFiles,jpj,jpi),np.float32)
-      print nFiles, "\t", ii
-      for iFrame, j in enumerate(ii):
-          inputfile = TLCheck.filelist[j]
-          # print inputfile
-          CHL = Sat.readfromfile(inputfile,'CHL')
-          # print "MYSHAPE = ", CHL.shape, M.shape
-          M[iFrame,:,:] = CHL
+  for ii in range(0,12):
+    Var2D = ChlSquare(ii,0:,0:) - Chl(ii,0:,0:)
 
   print "\n\tDone :)\n"
 

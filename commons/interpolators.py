921432090028fbec39da383831c30b27ec4d8cc8
Giorgio Bolzon <gbolzon@inogs.it>
2017-06-09 11:18:07 +0200

    Adding shift() and SeaOverLand() functions

--- commons/interpolators.py
+++ commons/interpolators.py
@@ -1,6 +1,44 @@
 from scipy.interpolate import griddata
 import numpy as np
+def shift(M2d,pos, verso):
+    out = np.ones_like(M2d)*np.nan
 
+    if (verso=='u'): out[:-pos,:] = M2d [pos:,:]
+    if (verso=='d'): out[ pos:,:] = M2d[:-pos,:]
+    if (verso=='l'): out[:,:-pos] = M2d [:,pos:]
+    if (verso=='r'): out[:,pos:]  = M2d[:,:-pos]
+
+    if (verso=="ul") : out[:-pos,:-pos] =  M2d [pos:,pos:]
+    if (verso=="ur") : out[:-pos,pos: ] =  M2d [pos:,:-pos]
+    if (verso=="dl") : out[ pos:,pos:]  =  M2d [:-pos,pos:]
+    if (verso=="dr") : out[pos:,pos: ]  = M2d [:-pos,:-pos]
+    return out
+
+
+
+def SeaOverLand(M2d,n):
+    jpj,jpi = M2d.shape
+    a = np.zeros((8*n, jpj,jpi),np.float32)
+    for i in range(n):
+        a[i,:,:] = shift(M2d,i+1,'u')
+    for i in range(n):
+        a[i+n,:,:] = shift(M2d,i+1,'d')
+    for i in range(n):
+        a[i+2*n,:,:] = shift(M2d,i+1,'r')
+    for i in range(n):
+        a[i+3*n,:,:] = shift(M2d,i+1,'l')
+    for i in range(n):
+        a[i+4*n,:,:] = shift(M2d,i+1,'ur')
+    for i in range(n):
+        a[i+5*n,:,:] = shift(M2d,i+1,'dl')
+    for i in range(n):
+        a[i+6*n,:,:] = shift(M2d,i+1,'ul')
+    for i in range(n):
+        a[i+7*n,:,:] = shift(M2d,i+1,'dr')
+    result = np.nanmean (a,axis=0)
+    ii=~np.isnan(M2d)
+    result[ii] = M2d[ii]
+    return result
 
 def surf_interp_2d(Mask1, Mask2, Map2d):
     '''

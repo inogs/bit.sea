32b1cac06cf70d9118b23e3cd2077ef74eb19bac
Laura Feudale <lfeudale@node165.galileo.cineca.it>
2019-01-14 10:48:27 +0100

    Adding second definitio of Nitracline dNit/dz and adding filters for N3n and P_l

--- validation/deliverables/SingleFloat_vs_Model_Stat_Timeseries.py
+++ validation/deliverables/SingleFloat_vs_Model_Stat_Timeseries.py
@@ -9,7 +9,7 @@ def argument():
 
     parser.add_argument(   '--maskfile', '-m',
                                 type = str,
-                                default = "/pico/home/usera07ogs/a07ogs00/OPA/V2C/etc/static-data/MED1672_cut/MASK/meshmask.nc",
+                                default = "/marconi_scratch/userexternal/lfeudale/Maskfiles/meshmask.nc",
                                 required = False,
                                 help = ''' Path of maskfile''')
 
@@ -40,10 +40,11 @@ TheMask=Mask(args.maskfile, loadtmask=False)
 layer=Layer(0,200)
 
 VARLIST = ['P_l','N3n','O2o']
+#VARLIST = ['N3n']
 Adj = [True,True,False]
 nVar = len(VARLIST)
 
-METRICS = ['Int_0-200','Corr','DCM','z_01','Nit_1','SurfVal']
+METRICS = ['Int_0-200','Corr','DCM','z_01','Nit_1','SurfVal','dNit_dz']
 nStat = len(METRICS)
 
 M = Matchup_Manager(ALL_PROFILES,TL,BASEDIR)
@@ -52,17 +53,25 @@ MED_PROFILES = bio_float.FloatSelector(None,TL.timeinterval,OGS.med)
 wmo_list=bio_float.get_wmo_list(MED_PROFILES)
 
 iz200 = TheMask.getDepthIndex(200)+1 # Max Index for depth 200m
+iz10 = TheMask.getDepthIndex(10.8)
+iz10 = 9
+iz10 = TheMask.getDepthIndex(50)
+
 
 for wmo in wmo_list:
+#  if (wmo=="6901771"):
+    print wmo_list
     OUTFILE = OUTDIR + wmo + ".nc"
     print OUTFILE
     list_float_track=bio_float.filter_by_wmo(ALL_PROFILES,wmo)
     nTime = len(list_float_track)
     A_float = np.zeros((nVar, nTime, nStat), np.float32 ) * np.nan
     A_model = np.zeros((nVar, nTime, nStat), np.float32 ) * np.nan
-
+#    import sys
+#    sys.exit()
 
     for ivar, var_mod in enumerate(VARLIST):
+#     if ( ivar == 1 ):
         var = LOVFLOATVARS[var_mod]
         adj=Adj[ivar]
         for itime in range(nTime):
@@ -71,35 +80,63 @@ for wmo in wmo_list:
             Pres,Profile,Qc=p.read(var,read_adjusted=adj)
             if len(Pres) < 10 : continue
             GM = M.getMatchups([p], TheMask.zlevels, var_mod, read_adjusted=adj, interpolation_on_Float=False)
+            # Filter out the N3n spike in surface:
+            if (var_mod == "N3n"):
+#                for k, kk in enumerate(GM.Ref): 
+#                    if (kk<=0.011): 
+#                       GM.Ref[k]=np.nan
+                if (GM.Ref[0]>0.44): GM.Ref[:]=np.nan
+#                if (GM.Ref[0]
+            if (var_mod == "P_l"):
+                if (GM.Ref[0]>0.45): GM.Ref[:]=np.nan
+
             gm200 = GM.subset(layer)
             nLevels = gm200.number()
             izmax = min(nLevels,iz200)
  
-            A_float[ivar,itime,0] =  np.sum(gm200.Ref  *TheMask.dz[:izmax])/TheMask.dz[:izmax].sum() # Integral
-            A_model[ivar,itime,0] =  np.sum(gm200.Model*TheMask.dz[:izmax])/TheMask.dz[:izmax].sum() # Integral
+            A_float[ivar,itime,0] =  np.nansum(gm200.Ref  *TheMask.dz[:izmax])/TheMask.dz[:izmax].sum() # Integral
+            A_model[ivar,itime,0] =  np.nansum(gm200.Model*TheMask.dz[:izmax])/TheMask.dz[:izmax].sum() # Integral
 
             A_float[ivar,itime,1] = gm200.correlation() # Correlation
             A_model[ivar,itime,1] = gm200.correlation() # Correlation
 
 	    A_float[ivar,itime,5] = gm200.Ref[0] # Surf Value
             A_model[ivar,itime,5] = gm200.Model[0] # Surf Value
+            if (var_mod == "N3n"):
+                A_float[ivar,itime,5] = gm200.Ref[iz10] # Value at 10m
+                A_model[ivar,itime,5] = gm200.Model[iz10] # Value at 10m
 
+            print "------------"
+            print iz10
+            print gm200.Ref[0]
+            print gm200.Ref[10]
+            print gm200.Ref[iz10]
+            print "------------"
+            
 
             if (VARLIST[ivar] == "P_l"):
+              if ( ( p.time.month >= 4. )  & ( p.time.month <= 10 )):
+#                print "DCM " + str(p.time.month)
                 A_float[ivar,itime,2] = find_DCM(gm200.Ref  ,gm200.Depth)[1] # DCM
                 A_model[ivar,itime,2] = find_DCM(gm200.Model,gm200.Depth)[1] # DCM
                 DCM2_float = find_DCM2(gm200.Ref  ,gm200.Depth)[1] # DCM
                 DCM2_model = find_DCM2(gm200.Model,gm200.Depth)[1] # DCM
 #               print  "FLOAT MAX: " , A_float[ivar,itime,2]
 #		print  "MODELMAX: " , A_model[ivar,itime,2]
-
+              if ((( p.time.month >=1 ) & ( p.time.month <= 3 )) | ( p.time.month == 1 )):
+#                print "MLB " + str(p.time.month)
                 A_float[ivar,itime,3] = find_MLD(gm200.Ref  ,gm200.Depth) # MLD
                 A_model[ivar,itime,3] = find_MLD(gm200.Model,gm200.Depth) # MLD
 
             if (VARLIST[ivar] == "N3n"):
                 A_float[ivar,itime,4] = find_NITRICL(gm200.Ref  ,gm200.Depth) # Nitricline
                 A_model[ivar,itime,4] = find_NITRICL(gm200.Model,gm200.Depth) # Nitricline
-#	    import sys
-#	    sys.exit()
+
+                A_float[ivar,itime,6] = find_NITRICL_dz_max(gm200.Ref  ,gm200.Depth) # dNit/dz
+                A_model[ivar,itime,6] = find_NITRICL_dz_max(gm200.Model,gm200.Depth) # Nitricline
+#                print "NITRICL"
+
 
     dumpfile(OUTFILE,A_float,A_model,VARLIST,METRICS)
+    import sys
+#    sys.exit()
--- validation/deliverables/SingleFloat_vs_Model_Stat_Timeseries_plotter.py
+++ validation/deliverables/SingleFloat_vs_Model_Stat_Timeseries_plotter.py
@@ -9,7 +9,7 @@ def argument():
 
     parser.add_argument(   '--maskfile', '-m',
                                 type = str,
-                                default = "/marconi_scratch/userexternal/lfeudale/Maskfiles/meshmask.nc",
+                                default = "/gpfs/scratch/userexternal/lfeudale/Maskfiles/meshmask.nc",
                                 required = False,
                                 help = ''' Path of maskfile''')
     parser.add_argument(   '--inputdir', '-i',
@@ -46,19 +46,20 @@ def fig_setup(wmo,Lon,Lat):
     from layer_integral import coastline
 
     fig = plt.figure()
-    ax0 = plt.subplot2grid((4, 3), (0, 0), colspan=2)
-    ax1 = plt.subplot2grid((4, 3), (0, 2))
-    ax2 = plt.subplot2grid((4, 3), (1, 0), colspan=3)
-    ax3 = plt.subplot2grid((4, 3), (2, 0), colspan=3)
-    ax4 = plt.subplot2grid((4, 3), (3, 0), colspan=3)
-    axs = [ax0, ax1, ax2, ax3, ax4]
-    for ax in [ax2, ax3, ax4]:
+    ax0 = plt.subplot2grid((5, 3), (0, 0), colspan=2)
+    ax1 = plt.subplot2grid((5, 3), (0, 2))
+    ax2 = plt.subplot2grid((5, 3), (2, 0), colspan=3)
+    ax3 = plt.subplot2grid((5, 3), (3, 0), colspan=3)
+    ax4 = plt.subplot2grid((5, 3), (4, 0), colspan=3)
+    ax5 = plt.subplot2grid((5, 3), (1, 0), colspan=3)
+    axs = [ax0, ax1, ax2, ax3, ax4, ax5]
+    for ax in [ax2, ax3, ax4, ax5]:
 	ax.xaxis.grid(True)
         #ax.set_xlim([datetime(2016,1,1),datetime(2018,1,1)])
         ax.set_xlim([datetime(int(DATESTART[0:4]),int(DATESTART[4:6]),int(DATESTART[6:9])),datetime(int(DATE__END[0:4]),int(DATE__END[4:6]),int(DATE__END[6:9]))+timedelta(days=1)])
 
     fig.set_size_inches(10,15)
-    fig.set_dpi(150)
+    fig.set_dpi(300)
     c_lon,c_lat=coastline.get()
 
 #    list_float_track=bio_float.filter_by_wmo(Profilelist_1,wmo_list[j])
@@ -71,9 +72,14 @@ def fig_setup(wmo,Lon,Lat):
 #    print Lon[ind_max_sup],Lat[ind_max_sup]
 #    ax0.plot(Lon[ind_max_sup],Lat[ind_max_sup],'g.')
 #    ax0.plot(Lon[0],Lat[0],'bx')
-    ax0.set_xlim([-10,36])
-    ax0.set_ylabel("LAT",color = 'k', fontsize = 15)
-    ax0.set_xlabel("LON",color = 'k', fontsize = 15)
+    ax0.set_xlim([-5.5,36])
+#    ax0.set_ylabel("LAT",color = 'k', fontsize = 15)
+#    ax0.set_xlabel("LON",color = 'k', fontsize = 15)
+    xlabels = ax0.get_xticklabels()
+    ylabels = ax0.get_yticklabels()
+    plt.setp(xlabels, fontsize=15)
+    plt.setp(ylabels, fontsize=15)
+
 
     extent=4
     ax1.plot(c_lon,c_lat,'k')
@@ -81,6 +87,11 @@ def fig_setup(wmo,Lon,Lat):
     ax1.plot(Lon[0],Lat[0],'bo')
     ax1.set_xlim([Lon.min() -extent/2, Lon.max() +extent/2])
     ax1.set_ylim([Lat.min() -extent/2, Lat.max() +extent/2])
+    xlabels1 = ax1.get_xticklabels()
+    ylabels1 = ax1.get_yticklabels()
+    plt.setp(xlabels1, fontsize=15)
+    plt.setp(ylabels1, fontsize=15)
+
 
     return fig, axs
 
@@ -92,7 +103,7 @@ OUTDIR = addsep(args.outdir)
 VARLIST = ['P_l','N3n','O2o']
 VARLIST_NAME = ['Chlorophyll','Nitrate','Oxygen']
 nVar = len(VARLIST)
-METRICS = ['Int_0-200','Corr','DCM','z_01','Nit_1']
+METRICS = ['Int_0-200','Corr','DCM','z_01','Nit_1','SurfVal','dNit_dz']
 nStat = len(METRICS)
 
 M = Matchup_Manager(ALL_PROFILES,TL,BASEDIR)
@@ -115,6 +126,8 @@ for wmo in wmo_list:
         Lat[ip] = p.lat
     times = [p.time for p in wmo_track_list]
     for ivar, var in enumerate(VARLIST):
+#     if ( ivar==1 ):
+        print var
         OUTFILE = OUTDIR + var + "_" + wmo + ".png"
         print OUTFILE
         fig, axes = fig_setup(wmo,Lon,Lat)
@@ -123,35 +136,44 @@ for wmo in wmo_list:
 	model, ref =A.plotdata(var,'Int_0-200')
         surf_model, surf_ref = A.plotdata(var,'SurfVal')
 	if (~np.isnan(model).all() == True) or (~np.isnan(ref).all() == True): 
-            axes[2].plot(times,  ref,'r',label='REF INTEG')
+            axes[2].plot(times,  ref,'.b',label='REF INTEG')
             axes[2].plot(times,model,'b',label='MOD INTEG')
 	    
-	    axes[2].plot(times,  surf_ref,'--r',label='REF SURF')
-            axes[2].plot(times,  surf_model,'--b',label='MOD SURF')
-	    if (var == "P_l"):
-	        axes[2].set_ylabel('Chlorophyll \n $[mg{\  } m^{-3}]$',fontsize=15)
+	    axes[5].plot(times,  surf_ref,'.b',label='REF SURF')
+            axes[5].plot(times,  surf_model,'b',label='MOD SURF')
+	    if ( var == "P_l" ):
+	        axes[2].set_ylabel('INT \n $[mg{\  } m^{-3}]$',fontsize=15)
+                axes[5].set_ylabel('SURF \n $[mg{\  } m^{-3}]$',fontsize=15)
                 axes[4].set_ylim(40,200)
-	    if (var == "O2o"):
-                axes[2].set_ylabel('Oxygen 0-200m \n $[mmol{\  } m^{-3}]$',fontsize=15)
-            if (var == "N3n"):
-                axes[2].set_ylabel('Nitrate 0-200m \n $[mmol{\  } m^{-3}]$',fontsize=15)
+	    if ( var == "O2o" ):
+                axes[2].set_ylabel('INT \n $[mmol{\  } m^{-3}]$',fontsize=15)
+                axes[5].set_ylabel('SURF \n $[mmol{\  } m^{-3}]$',fontsize=15)
+            if ( var == "N3n" ):
+                axes[2].set_ylabel('INT \n $[mmol{\  } m^{-3}]$',fontsize=15)
 #		axes[2].set_ylabel('INTEG 0-200m \n $[mmol{\  } m^{-3}]$',fontsize=15)
-                axes[2].set_ylim(0,8)
-                axes[4].set_ylim(0,200)
-	    legend = axes[2].legend(loc='upper left', shadow=True, fontsize=12)
+#                axes[2].set_ylim(0,8)
+                axes[2].set_ylim(0,4)
+#                axes[4].set_ylim(40,200)
+                axes[5].set_ylabel('SURF \n $[mmol{\  } m^{-3}]$',fontsize=15)
+                axes[5].set_ylabel('at 50m depth \n $[mmol{\  } m^{-3}]$',fontsize=15)
+#	    legend = axes[2].legend(loc='upper left', shadow=True, fontsize=12)
  	    model_corr, ref_corr =A.plotdata(var,'Corr')
 	    times_r = times
 	    for icr , cr in enumerate(ref_corr): 
-		if (cr <= 0):
+#                print cr
+#		if (cr <= 0):
+                if ( ( ~np.isnan(cr) <= 0 ) | np.isnan(cr) ):
 		    ref_corr[icr] = np.nan
 #                    times_r.remove(times[icr])
 
-	    axes[3].plot(times,ref_corr,'k')
+	    axes[3].plot(times,ref_corr,'b')
 #           axes[3].plot(times_r,ref_corr[ref_corr>0],'k')
 	    axes[3].set_ylabel('CORR',fontsize=15)
+#            axes[5].set_ylabel('SURF',fontsize=15)
 
 	    axes[2].set_xticklabels([])
 	    axes[3].set_xticklabels([])
+            axes[5].set_xticklabels([])
 
             axes[3].set_ylim(0,1)
 
@@ -162,21 +184,21 @@ for wmo in wmo_list:
 #	       model_mld = np.nan
 
 	    if (~np.isnan(model_dcm).all() == True) or (~np.isnan(ref_dcm).all() == True):
-                axes[4].plot(times,  ref_dcm,'r',label='DCM REF')
+                axes[4].plot(times,  ref_dcm,'.b',label='DCM REF')
                 axes[4].plot(times,model_dcm,'b',label='DCM MOD')
-                axes[4].plot(times, ref_mld,'--r',label='MLB REF')
-                axes[4].plot(times,model_mld,'--b',label='MLB MOD')
+                axes[4].plot(times, ref_mld,'.r',label='MWB REF')
+                axes[4].plot(times,model_mld,'r',label='MWB MOD')
 #		axes[4].plot(times,np.ones_like(times)* np.nanmean(ref_dcm),'r',linewidth=3) 
 #		axes[4].plot(times,np.ones_like(times)* np.nanmean(model_dcm),'b',linewidth=3) #marker='.')
 
 	        axes[4].invert_yaxis()
-	        axes[4].set_ylabel('DCM/MLB $[m]$',fontsize=15)
+	        axes[4].set_ylabel('DCM/MWB $[m]$',fontsize=15)
 #		axes[4].xaxis_date()
 #		axes[4].xaxis.set_major_locator(mdates.MonthLocator())
 		axes[4].xaxis.set_major_formatter(mdates.DateFormatter("%d-%m-%Y"))
 
 		xlabels = axes[4].get_xticklabels()
-		plt.setp(xlabels, rotation=30)
+		plt.setp(xlabels, rotation=30,fontsize=15)
 
 
 #	    if (~np.isnan(model_mld).all() == True) or (~np.isnan(ref_mld).all() == True):
@@ -185,28 +207,40 @@ for wmo in wmo_list:
 #                axes_4b.plot(times,model_mld,'--b',label='MOD')
 #	        axes_4b.invert_yaxis()
 #	        axes_4b.set_ylabel('MLD $[m]$ - -',fontsize=15)
-            legend = axes[4].legend(loc='lower left', shadow=True, fontsize=12)
+#            legend = axes[4].legend(loc='lower left', shadow=True, fontsize=12)
 
         if (var == "N3n"):
             model_nit, ref_nit =A.plotdata(var,'Nit_1')
+            model_nit2, ref_nit2 = A.plotdata(var,'dNit_dz')
 	    if (~np.isnan(ref_nit).all() == True) or (~np.isnan(model_nit).all() == True):
-            	axes[4].plot(times,  ref_nit,'r',label='REF')
+            	axes[4].plot(times,  ref_nit,'.b',label='REF')
             	axes[4].plot(times,model_nit,'b',label='MOD')
             	axes[4].invert_yaxis()
-	    	axes[4].set_ylabel('NITRICL $[m]$',fontsize=15)
+	    	axes[4].set_ylabel('NITRCL 1/2 $[m]$',fontsize=15)
+#                print ref_nit2
+                axes[4].plot(times, ref_nit2,'.r',label='dNit REF')
+                axes[4].plot(times,model_nit2,'r',label='dNit MOD') 
             else: 
 		axes[4].plot(times,  np.ones_like(times))
             axes[4].xaxis.set_major_formatter(mdates.DateFormatter("%d-%m-%Y"))
             xlabels = axes[4].get_xticklabels()
             plt.setp(xlabels, rotation=30)
-	    legend = axes[4].legend(loc='upper left', shadow=True, fontsize=12)
+#	    legend = axes[4].legend(loc='upper left', shadow=True, fontsize=12)
 
         if (var == "O2o"):
 	    axes[4].plot(times,  np.ones_like(times))
             axes[4].xaxis.set_major_formatter(mdates.DateFormatter("%d-%m-%Y"))
             xlabels = axes[4].get_xticklabels()
-            plt.setp(xlabels, rotation=30)
+            plt.setp(xlabels, rotation=30,fontsize=15)
+
+        label_s = 15
+        axes[2].tick_params(axis = 'both', which = 'major', labelsize = label_s)
+        axes[3].tick_params(axis = 'both', which = 'major', labelsize = label_s)
+        axes[4].tick_params(axis = 'both', which = 'major', labelsize = label_s)
+        axes[5].tick_params(axis = 'both', which = 'major', labelsize = label_s)
+
 
         fig.savefig(OUTFILE)
 	plt.close(fig)
     import sys
+#    sys.exit()

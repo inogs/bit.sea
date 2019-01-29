c171ce6a2fec94dd9824ff2f9cfd4b77cc7aab72
Laura Feudale <lfeudale@node165.galileo.cineca.it>
2019-01-29 13:50:52 +0100

    Update of the qualification_quid with change of Figure number output and additional calls of python scripts for the pCO2 analysis (vs SOCAT and REAN data).

--- validation/deliverables/qualification_quid_assm_v7.sh
+++ validation/deliverables/qualification_quid_assm_v7.sh
@@ -2,36 +2,38 @@
 
 # QUID REANALYSIS
 # SECTION 4:
-export MASKFILE=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v19_3/wrkdir/MODEL/meshmask.nc
+export MASKFILE=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/MODEL/meshmask.nc
 
-         INPUTDIR=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v20_1/wrkdir/MODEL/AVE_FREQ_2
-   INPUT_AGGR_DIR=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v20_1/wrkdir/POSTPROC/output/AVE_FREQ_2/TMP
-STAT_PROFILES_DIR=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v20_1/wrkdir/POSTPROC/output/AVE_FREQ_2/STAT_PROFILES
+         INPUTDIR=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/MODEL/AVE_FREQ_2/ #_compressed/
+   INPUT_AGGR_DIR=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/POSTPROC/output/AVE_FREQ_2/TMP/
+STAT_PROFILES_DIR=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/POSTPROC/output/AVE_FREQ_2/STAT_PROFILES
 SAT_MONTHLY_DIR=/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/MULTISENSOR_1km/MONTHLY_24/
 
 OUTDIR=spaghettiplots
 mkdir -p $OUTDIR
 python plot_layer_timeseries_on_profiles.py -i $STAT_PROFILES_DIR -m $MASKFILE -o $OUTDIR
 
-mkdir -p Fig4.1/ Fig4.7 Fig4.9 Fig4.10 Fig4.17 Fig4.18
+mkdir -p Fig4.1/ Fig4.7 Fig4.9 Fig4.10 Fig4.18 Fig4.19 Fig4.7bis
 
 
-COMMONS_PARAMS="-m $MASKFILE -o LayerMaps/  -l Plotlist_bio.xml -s 20150101 -e 20170101"
+COMMONS_PARAMS="-m $MASKFILE -o LayerMaps/  -l Plotlist_bio.xml -s 20170101 -e 20180101"
 
 mkdir -p LayerMaps
 python averager_and_plot_map.py -i $INPUT_AGGR_DIR  -v P_l  -t mean $COMMONS_PARAMS      # Fig4.1 CHL-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN
 python averager_and_plot_map.py -i $INPUTDIR        -v N3n  -t mean $COMMONS_PARAMS      # Fig4.10 NIT-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN
 python averager_and_plot_map.py -i $INPUTDIR        -v N1p  -t mean $COMMONS_PARAMS      # Fig4.9  PHOS-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN
-python averager_and_plot_map.py -i $INPUTDIR        -v ppn  -t integral $COMMONS_PARAMS  # Fig4.7 NPP-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN per lo 0-200m
+INPUTDIR_PPN=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/POSTPROC/output/AVE_FREQ_2/TMP/
+python averager_and_plot_map_ppn.py -i $INPUTDIR_PPN        -v ppn  -t integral $COMMONS_PARAMS  # Fig4.7 NPP-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN per lo 0-200m
+python averager_and_plot_map_ppn.py -i $INPUTDIR_PPN        -v ppn  -t integral -m $MASKFILE -o Fig4.7bis -l Plotlist_bio_ppn20m.xml -s 20170101 -e 20180101
 
-python averager_and_plot_map.py -i $INPUTDIR        -v Ac   -t mean $COMMONS_PARAMS   # Ac-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN  --> not requested in the ScQP
+python averager_and_plot_map.py -i $INPUTDIR        -v ALK   -t mean $COMMONS_PARAMS   # Ac-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN  --> not requested in the ScQP
 python averager_and_plot_map.py -i $INPUTDIR        -v DIC  -t mean $COMMONS_PARAMS   # DIC-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN --> not requested in the ScQP
 cp LayerMaps/*P_l* Fig4.1/
 cp LayerMaps/*N3n* Fig4.10/
 cp LayerMaps/*N1p* Fig4.9/
 cp LayerMaps/*ppn* Fig4.7/
-cp LayerMaps/*Ac* Fig4.17/
-cp LayerMaps/*DIC* Fig4.18/
+cp LayerMaps/*ALK* Fig4.18/
+cp LayerMaps/*DIC* Fig4.19/
 
 # python averager_and_plot_map.py -i $INPUTDIR        -v O2o  -t mean $COMMONS_PARAMS  # DO-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN --> Not showed for lack of ref
 # python averager_and_plot_map.py -i $INPUT_AGGR_DIR  -v P_c  -t mean $COMMONS_PARAMS  # PHYC-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN --> Not showed for lack of ref
@@ -41,25 +43,27 @@ python sat_ave_and_plot.py      -i $SAT_MONTHLY_DIR -m $MASKFILE  -o Fig4.1/
 
 mkdir -p Fig4.2 Fig4.3/offshore Fig4.3/coast table4.1 table4.2
 SAT_WEEKLY_DIR=/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/MULTISENSOR_1km/WEEKLY_24_Friday
-INPUT_AGGR_DIR=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v20_1/wrkdir/POSTPROC/output/AVE_FREQ_2/TMP
 ###STAT_PROFILES_DIR1=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v19_3/wrkdir/POSTPROC/output/AVE_FREQ_1/STAT_PROFILES/
 OUTDIR=TMP_week/
-python ScMYvalidation_plan.py -s $SAT_WEEKLY_DIR -i $INPUT_AGGR_DIR -m $MASKFILE -c open_sea   -o export_data_ScMYValidation_plan_open_sea.pkl
-python ScMYvalidation_plan.py -s $SAT_WEEKLY_DIR -i $INPUT_AGGR_DIR -m $MASKFILE -c coast      -o export_data_ScMYValidation_plan_coast.pkl
+#python ScMYvalidation_plan.py -s $SAT_WEEKLY_DIR -i $INPUT_AGGR_DIR -m $MASKFILE -c open_sea   -o export_data_ScMYValidation_plan_open_sea.pkl
+#python ScMYvalidation_plan.py -s $SAT_WEEKLY_DIR -i $INPUT_AGGR_DIR -m $MASKFILE -c coast      -o export_data_ScMYValidation_plan_coast.pkl
+python ScMYvalidation_plan_STD_CORR.py -s $SAT_WEEKLY_DIR -i $INPUT_AGGR_DIR -m $MASKFILE -c open_sea   -o export_data_ScMYValidation_plan_open_sea_STD_CORR.pkl
+python ScMYvalidation_plan_STD_CORR.py -s $SAT_WEEKLY_DIR -i $INPUT_AGGR_DIR -m $MASKFILE -c coast      -o export_data_ScMYValidation_plan_coast_STD_CORR.pkl
+
 #python plot_timeseries_2014_2016.py -i $STAT_PROFILES_DIR1 -m $MASKFILE -o export_data_ScMYValidation_plan_open_sea.pkl -c export_data_ScMYValidation_plan_coast.pkl -O ./Fig4.2/
-python plot_timeseries.py -o export_data_ScMYValidation_plan_open_sea.pkl -c export_data_ScMYValidation_plan_coast.pkl -O ./Fig4.2/
+python plot_timeseries_STD.py -o export_data_ScMYValidation_plan_open_sea_STD_CORR.pkl -c export_data_ScMYValidation_plan_coast_STD_CORR.pkl -O ./Fig4.2/
 
 # CHL-SURF-W-CLASS4-SIMG-BIAS-BASIN
 # CHL-SURF-W-CLASS4-SIMG-RMSD-BASIN
-python plot_timeseries_RMS.py -i export_data_ScMYValidation_plan_open_sea.pkl -o Fig4.3/offshore  # table4.1
-python plot_timeseries_RMS.py -i export_data_ScMYValidation_plan_coast.pkl    -o Fig4.3/coast     # table4.2
+python plot_timeseries_RMS_CORR.py -i export_data_ScMYValidation_plan_open_sea_STD_CORR.pkl -o Fig4.3/offshore  # table4.1
+python plot_timeseries_RMS_CORR.py -i export_data_ScMYValidation_plan_coast_STD_CORR.pkl    -o Fig4.3/coast     # table4.2
 cp Fig4.3/offshore/table4.1.dat table4.1
 cp Fig4.3/coast/table4.1.dat    table4.2/table4.2.dat
 
 mkdir -p Fig4.8
-###INTEGRALS_PPN=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v19_3/wrkdir/POSTPROC/output/AVE_FREQ_2/PPN/INTEGRALS/PPN/ # girato un aveScan ridotto solo per loro
-INTEGRALS_PPN=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v20_1/wrkdir/POSTPROC/output/MONTHLY/PPN/INTEGRALS/PPN/ # girato un aveScan ridotto solo per loro
+INTEGRALS_PPN=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/POSTPROC/output/MONTHLY/PPN/INTEGRALS/PPN/
 python read_ppn_from_avescan_do_plot.py -c open_sea   -i $INTEGRALS_PPN -o Fig4.8
+
 
 # Figure carbonatiche 1x1
 # ALK-LAYER-Y-CLASS4-CLIM-RMSD
@@ -71,20 +75,23 @@ python read_ppn_from_avescan_do_plot.py -c open_sea   -i $INTEGRALS_PPN -o Fig4.
 # python ricostruzione_Integrals.py -i /pico/scratch/userexternal/gbolzon0/eas_v12/eas_v19_3/wrkdir/POSTPROC/output/AVE_FREQ_2/1x1/INTEGRALS/ -o 1x1/
 # ----------------------------
 
+
 #### PH-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN in Fig4.20
+# OLD FIGURE:
 mkdir -p Fig4.20/Feb Fig4.20/May Fig4.20/Aug Fig4.20/Nov Fig4.21/Feb Fig4.21/May  Fig4.21/Aug  Fig4.21/Nov
-INPUTDIR_MON=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v20_1/wrkdir/POSTPROC/output/MONTHLY/AVE/
+INPUTDIR_MON=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/MODEL/AVE_FREQ_2/
 COMMONS_PARAMS="-i $INPUTDIR_MON -m $MASKFILE -l Plotlist_bio.xml  -t mean "
-python averager_and_plot_map.py $COMMONS_PARAMS  -v pH  -s 20160201 -e 20160301 -o Fig4.20/Feb
-python averager_and_plot_map.py $COMMONS_PARAMS  -v pH  -s 20160501 -e 20160601 -o Fig4.20/May
-python averager_and_plot_map.py $COMMONS_PARAMS  -v pH  -s 20160801 -e 20160901 -o Fig4.20/Aug
-python averager_and_plot_map.py $COMMONS_PARAMS  -v pH  -s 20161101 -e 20161201 -o Fig4.20/Nov
+python averager_and_plot_map.py $COMMONS_PARAMS  -v pH  -s 20170201 -e 20170301 -o Fig4.20/Feb
+python averager_and_plot_map.py $COMMONS_PARAMS  -v pH  -s 20170501 -e 20170601 -o Fig4.20/May
+python averager_and_plot_map.py $COMMONS_PARAMS  -v pH  -s 20170801 -e 20170901 -o Fig4.20/Aug
+python averager_and_plot_map.py $COMMONS_PARAMS  -v pH  -s 20171101 -e 20171201 -o Fig4.20/Nov
 
 # PCO-LAYER-Y-CLASS1-[CLIM/LIT]-MEAN in Fig4.21
-python averager_and_plot_map.py $COMMONS_PARAMS  -v pCO2  -s 20160201 -e 20160301 -o Fig4.21/Feb
-python averager_and_plot_map.py $COMMONS_PARAMS  -v pCO2  -s 20160501 -e 20160601 -o Fig4.21/May
-python averager_and_plot_map.py $COMMONS_PARAMS  -v pCO2  -s 20160801 -e 20160901 -o Fig4.21/Aug
-python averager_and_plot_map.py $COMMONS_PARAMS  -v pCO2  -s 20161101 -e 20161201 -o Fig4.21/Nov
+# OLD FIGURE:
+python averager_and_plot_map.py $COMMONS_PARAMS  -v pCO2  -s 20170201 -e 20170301 -o Fig4.21/Feb
+python averager_and_plot_map.py $COMMONS_PARAMS  -v pCO2  -s 20170501 -e 20170601 -o Fig4.21/May
+python averager_and_plot_map.py $COMMONS_PARAMS  -v pCO2  -s 20170801 -e 20170901 -o Fig4.21/Aug
+python averager_and_plot_map.py $COMMONS_PARAMS  -v pCO2  -s 20171101 -e 20171201 -o Fig4.21/Nov
 
 # BIOFLOATS SECTION: Hovmoeller plots, wmo trajectories and statistics per basin
 # It is included the 2014 also.
@@ -99,7 +106,7 @@ mv $OUTDIR/*O2o*.png Fig4.15
 NCDIR=tmp_nc
 OUTDIR=Fig4.4b
 python SingleFloat_vs_Model_Stat_Timeseries.py -m $MASKFILE -o $NCDIR
-python SingleFloat_vs_Model_Stat_Timeseries_plotter.py -i $NCDIR -o $OUTDIR
+python SingleFloat_vs_Model_Stat_Timeseries_plotter.py -i $NCDIR -o $OUTDIR -m $MASKFILE
 mv $OUTDIR/N3n*.png Fig4.12
 mv $OUTDIR/O2o*.png Fig4.15
 
@@ -144,31 +151,72 @@ cp $OUTFIGDIR/*O2o* Fig4.16
 # Fig. 4.19:
 # ALK-LAYER-Y-CLASS4-CLIM-BIAS/RMSD/CORR calculated on 8 layers
 # DIC-LAYER-Y-CLASS4-CLIM-BIAS/RMSD/CORR calculated on 8 layers
+ 
+# Fig. 4.17:
+# PH-PROF-Y-CLASS4-[CLIM/LIT]-MEAN
+# pCO-PROF-Y-CLASS4-[CLIM/LIT]-MEAN
 
-STAT_PROFILES_MONTHLY_DIR=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v20_1/wrkdir/POSTPROC/output/MONTHLY/STAT_PROFILES/
-# Figures 4.11 and 4.19
-mkdir -p sim_vs_clim_profiles/ Fig4.11 Fig4.19
-python simulation_vs_clim.py -i $STAT_PROFILES_MONTHLY_DIR -o sim_vs_clim_profiles/ -s 20150101 -e 20170101 -m $MASKFILE
+#STAT_PROFILES_MONTHLY_DIR=/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v20_1/wrkdir/POSTPROC/output/MONTHLY/STAT_PROFILES/
+STAT_PROFILES_MONTHLY_DIR=/gpfs/scratch/userexternal/gbolzon0/OPEN_BOUNDARY/TEST_05/wrkdir/POSTPROC/output/AVE_FREQ_2/STAT_PROFILES/
+# Figures 4.11 and 4.17 (previously named 4.19)
+mkdir -p sim_vs_clim_profiles/ Fig4.11 Fig4.17
+python simulation_vs_clim.py -i $STAT_PROFILES_MONTHLY_DIR -o sim_vs_clim_profiles/ -s 20170101 -e 20180101 -m $MASKFILE
 cp sim_vs_clim_profiles/Fig_4.11*png Fig4.11
-cp sim_vs_clim_profiles/Fig_4.19*png Fig4.19
+cp sim_vs_clim_profiles/Fig_4.17*png Fig4.17
 
 DIR=static_clim
 mkdir -p $DIR table4.6 table4.7 table4.10/ table4.11 table4.13/ table4.14
 # -------------------------------------------------------------------------
 
-python static_clim_validation.py -i $STAT_PROFILES_DIR -o $DIR -m $MASKFILE -s 20150101 -e 20170101
+#python static_clim_validation_STD.py -i $STAT_PROFILES_DIR -o $DIR -m $MASKFILE -s 20170101 -e 20180101
+#python static_clim_validation_STD_pH.py -i $STAT_PROFILES_DIR -o $DIR -m $MASKFILE -s 20170101 -e 20180101
+python static_clim_validation.py -i $STAT_PROFILES_DIR -o $DIR -m $MASKFILE -s 20170101 -e 20180101
 
 # -------------------------------------------------------------------------
 cp $DIR/N1p-LAYER-Y-CLASS4-CLIM.txt $DIR/N3n-LAYER-Y-CLASS4-CLIM.txt table4.6/
 cp $DIR/O2o-LAYER-Y-CLASS4-CLIM.txt                                  table4.10/
-cp $DIR/Ac-LAYER-Y-CLASS4-CLIM.txt $DIR/DIC-LAYER-Y-CLASS4-CLIM.txt  table4.13/
+
+# PH-PROF-Y-CLASS4-CLIM-[BIAS/RMSD/CORR]-BASIN
+# PCO-PROF-Y-CLASS4-CLIM-[BIAS/RMSD/CORR]-BASIN
+
+#cp $DIR/Ac-LAYER-Y-CLASS4-CLIM.txt $DIR/DIC-LAYER-Y-CLASS4-CLIM.txt  table4.13/
+cp $DIR/ALK-LAYER-Y-CLASS4-CLIM.txt $DIR/DIC-LAYER-Y-CLASS4-CLIM.txt  table4.13/
+cp $DIR/pH-LAYER-Y-CLASS4-CLIM.txt  $DIR/pCO2-LAYER-Y-CLASS4-CLIM.txt table4.13/
 
 # ALK-PROF-Y-CLASS4-CLIM-CORR-BASIN calculated on 14 layers --> in table4.14
 # DIC-PROF-Y-CLASS4-CLIM-CORR-BASIN calculated on 14 layers --> in table4.14
 
 cp $DIR/N1p-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt $DIR/N3n-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt table4.7
 cp $DIR/O2o-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt                                            table4.11
-cp $DIR/Ac-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt  $DIR/DIC-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt table4.14
+#cp $DIR/Ac-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt  $DIR/DIC-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt table4.14
+cp $DIR/ALK-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt  $DIR/DIC-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt table4.14
+cp $DIR/pH-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt   $DIR/pCO2-PROF-Y-CLASS4-CLIM-CORR-BASIN.txt table4.14
+
+### SECTION WITH NEW FIGURES FOR pCO2 COMPARISON/ANALYSIS ###
+# Compare pCO2 vs SOCAT dataset: 
+# Fig4.20  : PCO-SURF-M-CLASS4-CLIM-MEAN-BASIN
+# table4.15: PCO-SURF-M-CLASS4-CLIM-RMSD-BASIN.txt 
+mkdir -p table4.15 Fig4.20
+# First generate climatology tables:
+python monthly_clim_socat_pCO2.py
+#python monthly_2017.py
 
 mkdir -p monthly_2017_surf/
 python monthly_2017 -i $STAT_PROFILES_DIR -o monthly_2017_surf/
+
+python table_pCO2vsSOCAT.py -i monthly_2017_surf/
+mv pCO2-SURF-M-CLASS4-CLIM-RMSD-BASIN.txt table4.15/.
+mv TOT_RMSD_pCO2vsSOCAT.txt table4.15/.
+
+# generate new figure 4.20: comparison pCO2 BFM vs SOCAT dataset
+
+python plot_month_pCO2vsSOCAT.py
+mv pCO2_monthly_tseries_Fig4.20.png Fig4.20/.
+
+# generate new figure 4.21: comparison pCO2 and CO2ariflux vs REAN (CLIM)
+# Fig4.21: airseaCO2flux-SURF-M-CLASS4-CLIM-MEAN-BASIN
+
+REANDIR="/gpfs/scratch/userexternal/gbolzon0/REA/output/STAT_PROFILES/"
+#python plot_pCO2_CO2airflux_vs_REAN.py -r "/gpfs/scratch/userexternal/gbolzon0/REA/output/STAT_PROFILES/"
+python plot_pCO2_CO2airflux_vs_REAN.py -r $REANDIR -i $STAT_PROFILES_MONTHLY_DIR -o Fig4.21/
+

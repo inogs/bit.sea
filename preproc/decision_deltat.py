fa9c106178ac35939ca9170a13eda4a16cde76ab
GianFranco Marras <a07ogs00@node166.galileo.cineca.it>
2019-04-05 11:16:37 +0200

    Adding argparse to fix a bug

--- preproc/decision_deltat.py
+++ preproc/decision_deltat.py
@@ -1,7 +1,28 @@
+import argparse
+def argument():
+    parser = argparse.ArgumentParser(description = '''
+    Prints in standard output the name of the choosen namelist.init file:
+    One of these:
+    - namelist.init.300
+    - namelist.init.450
+    - namelist.init.600
+    ''',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(   '--inputdir', '-i',
+                                type = str,
+                                required = True,
+                                help = '''Directory where dailt DeltaT_*txt files are '''
+                                )
+    return parser.parse_args()
+
+
+args = argument()
 import numpy as np
 import os
+from commons.utils import addsep
 mydtype=[('time','S8'), ('deltaT',np.float32),('K',np.int),('J',np.int),('I',np.int)]
-INPUTDIR="DeltaT/"
+INPUTDIR=addsep(args.inputdir)
 cattedfile=INPUTDIR + "DeltaT.txt"
 command="cat  " + INPUTDIR + "DeltaT_* > " + cattedfile 
 os.system(command)
@@ -16,3 +37,4 @@ else:
         print "namelist.init.450"
     else:
         print "namelist.init.300"
+

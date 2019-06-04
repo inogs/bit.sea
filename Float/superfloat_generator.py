91df2b07cf7912bf66ebe17840d418ee5d67ce43
Giorgio Bolzon <gbolzon@inogs.it>
2019-06-04 16:29:32 +0200

    Working step of super float generation for CHLA superfloat_structure is the main caller which has the architecture: 1. looking for LOV profiles and detect if they are only_lov or commons 2. looking for CORIOLIS, avoiding existing files (of course of LOV)
    superfloat_generator has functions, like
    - quenching( which works still without LOV value at z=0)
    - synthesis_profile()
    - treating coriolis()

--- Float/superfloat_generator.py
+++ Float/superfloat_generator.py
@@ -9,6 +9,15 @@ TI = TimeInterval('2015','2017','%Y')
 R = Rectangle(-6,36,30,46)
 
 def quenching(profile_obj, PresChl, Chl, chl_lov_zero):
+    '''
+    Arguments:
+    * profile_obj  * a Coriolis profile object
+    * Pres_chl     * numpy 1D-array of coriolis pressure
+    * Chl          * numpy 1D-array of coriolis chl
+    * chl_lov_zero * value of LOV quench; an algorithm will try to guess the Mixed Layer Depth
+                     calculated by LOV, in order to use it here to calculate quench value
+                     if None, the Coriols Mixed Layer Depth will be used.
+    '''
     Quenched_Chl = Chl.copy()
     if profile_obj.time.month in range(1,13):
         PresT, Temp, _ = profile_obj.read('TEMP', read_adjusted=False)
@@ -17,21 +26,27 @@ def quenching(profile_obj, PresChl, Chl, chl_lov_zero):
 
         ii=PresChl<=mld
         base_mld_level=ii.sum()
-        nTries = 25
-        if  nTries > len(PresChl)-base_mld_level-1:
-            nTries = len(PresChl)-base_mld_level-1
+        if chl_lov_zero is not None:
+            nTries = 25
+            if  nTries > len(PresChl)-base_mld_level-1:
+                nTries = len(PresChl)-base_mld_level-1
 
-        quench_value = np.zeros((nTries,), np.float32)
-        diff_fromlov = np.zeros((nTries,), np.float32)
-        for down_level in range(nTries):
-            nLev = base_mld_level + down_level
-            if nLev==0: nLev=1
-            quench_value[down_level] = Chl[:nLev].max()
-            diff_fromlov[down_level] = np.abs(quench_value[down_level]-chl_lov_zero)
+            quench_value = np.zeros((nTries,), np.float32)
 
-        k = diff_fromlov.argmin()
-        nLev= base_mld_level + k
-        Quenched_Chl[:nLev] = quench_value[k]
+            diff_fromlov = np.zeros((nTries,), np.float32)
+            for down_level in range(nTries):
+                nLev = base_mld_level + down_level
+                if nLev==0: nLev=1
+                quench_value[down_level] = Chl[:nLev].max()
+                diff_fromlov[down_level] = np.abs(quench_value[down_level]-chl_lov_zero)
+
+            k = diff_fromlov.argmin()
+            nLev= base_mld_level + k
+            Quenched_Chl[:nLev] = quench_value[k]
+        else:
+            nLev = base_mld_level + 1
+            quench_value = Chl[:nLev].max()
+            Quenched_Chl[:nLev] = quench_value
     return Quenched_Chl
 
 def are_identical(chl1, chl2):
@@ -57,53 +72,107 @@ def has_drift(Pres,Profile):
     else:
         return False
 
+def synthesis_profile(pLov, pCor):
+    '''
+    Arguments:
+    * pLov, pCor * two profile objects
+    Returns
+    * Pres, Value, Qc * numpy ndarrays
+    '''
 
-#PROFILES_COR =   bio_float.FloatSelector('CHLA', TI, R)
-PROFILES_LOV =lovbio_float.FloatSelector('CHLA', TI, R)
+    PresL, ValueL, QcL = pLov.read('CHLA',read_adjusted=True)
+    if ('CHLA' in pCor.available_params) and  (pCor._my_float.status_var('CHLA')=='A'):
+        PresC, ValueC, QcC = pCor.read('CHLA',read_adjusted=True)
+    else:
+        return PresL, ValueL, QcL
+    if len(PresC)<5:
 
-for ip, pLov in enumerate(PROFILES_LOV[:]):
-    pCor=bio_float.from_lov_profile(pLov)
-    if pCor is not None:
-        PresL, ValueL, QcL = pLov.read('CHLA',read_adjusted=True)
-        if 'CHLA' in pCor.available_params:
-            if pCor._my_float.status_var('CHLA')=='A':
-                PresC, ValueC, QcC = pCor.read('CHLA',read_adjusted=True)
-            else:
-                print pCor._my_float.filename + " without CHLA_ADJUSTED"
-                continue
+        if len(PresL)>5:
+            print "few values in Coriolis: using LOV"
+            return PresL, ValueL, QcL
         else:
-            print pCor._my_float.filename + " without CHLA"
-            continue
+            print "few values in either dataset"
+            return None, None, None
+    if has_drift(PresC, ValueC):
+        print pCor._my_float.filename + " has drift"
+        return None, None, None
 
-        if len(ValueC) < 5: continue
-        if len(ValueL)<  1: continue
-        first_derivative=np.diff(ValueC)/np.diff(PresC)
-        max_jump = np.abs(np.diff(ValueC)).max()
-        z_max = PresC[max_jump.argmax()]
+    if len(ValueL)> 0:
+        chl_lov_zero=ValueL[0]
+    else:
+        chl_lov_zero = None
+    CHL = quenching(pCor, PresC, ValueC,chl_lov_zero)
 
-        if (max_jump > 0.2) & (z_max > 100) :
-            print "jump = ", max_jump, pCor._my_float.filename  + " will be replaced by LOV"
-            continue
-        if has_drift(PresC, ValueC):
-            print pCor._my_float.filename + " has drift"
-            continue
-        CHL = quenching(pCor, PresC, ValueC,ValueL[0])
+    ii=(PresC >= 400) & (PresC <= 600)
+    if ii.sum() > 0:
+        shift = CHL[ii].mean()
+        CHL = CHL - shift
+    return PresC, CHL, QcC
 
-        ii=(PresC >= 400) & (PresC <= 600)
+def treating_coriolis(pCor):
+    if pCor._my_float.status_var('CHLA')=='A':
+        Pres,Value, Qc=pCor.read('CHLA', read_adjusted=True)
+        if len(Pres)<5:
+            print "few values in Coriolis"
+            return None, None, None
+
+        CHL = superfloat_generator.quenching(pCor, Pres, Value, None)
+        ii=(Pres >= 400) & (Pres <= 600)
         if ii.sum() > 0:
             shift = CHL[ii].mean()
             CHL = CHL - shift
+        return Pres, CHL, Qc
+    else:
+        print "R", pCor._my_float.filename
+        return None, None, None
 
-        if not are_identical(CHL, ValueL):
-            if not are_shifted(CHL, ValueL):
-                print ip
-                fig,ax =pl.subplots()
-                ax.plot(ValueL,PresL,'r', label="LOV")
-                ax.plot(CHL  , PresC,'b.-', label="COR")
-                ax.invert_yaxis()
-                ax.grid()
-                ax.legend()
-                fig.show()
-                pl.savefig(pCor.ID()+ ".png")
-                #import sys
-                #sys.exit()
+if __name__=="__main__":
+
+    PROFILES_LOV =lovbio_float.FloatSelector('CHLA', TI, R)
+
+    for ip, pLov in enumerate(PROFILES_LOV[:]):
+        pCor=bio_float.from_lov_profile(pLov)
+        if pCor is not None:
+            PresL, ValueL, QcL = pLov.read('CHLA',read_adjusted=True)
+            if 'CHLA' in pCor.available_params:
+                if pCor._my_float.status_var('CHLA')=='A':
+                    PresC, ValueC, QcC = pCor.read('CHLA',read_adjusted=True)
+                else:
+                    print pCor._my_float.filename + " without CHLA_ADJUSTED"
+                    continue
+            else:
+                print pCor._my_float.filename + " without CHLA"
+                continue
+
+            if len(ValueC) < 5:
+                print "few values in Coriolis for " + pCor._my_float.filename
+                continue
+
+            if len(ValueL)<  1:
+                print "no LOV data"
+                import sys
+                sys.exit()
+
+            if has_drift(PresC, ValueC):
+                print pCor._my_float.filename + " has drift"
+                continue
+            CHL = quenching(pCor, PresC, ValueC,ValueL[0])
+
+            ii=(PresC >= 400) & (PresC <= 600)
+            if ii.sum() > 0:
+                shift = CHL[ii].mean()
+                CHL = CHL - shift
+
+            if not are_identical(CHL, ValueL):
+                if not are_shifted(CHL, ValueL):
+                    print ip
+                    fig,ax =pl.subplots()
+                    ax.plot(ValueL,PresL,'r', label="LOV")
+                    ax.plot(CHL  , PresC,'b.-', label="COR")
+                    ax.invert_yaxis()
+                    ax.grid()
+                    ax.legend()
+                    fig.show()
+                    pl.savefig(pCor.ID()+ ".png")
+                    #import sys
+                    #sys.exit()
---
+++ Float/superfloat_structure.py
@@ -0,0 +1,102 @@
+from instruments import bio_float
+from instruments import lovbio_float
+from commons.time_interval import TimeInterval
+from basins.region import Rectangle
+import superfloat_generator
+import os,sys
+import scipy.io.netcdf as NC
+import numpy as np
+TI = TimeInterval('2012','2020','%Y')
+R = Rectangle(-6,36,30,46)
+
+PROFILES_LOV =lovbio_float.FloatSelector('CHLA', TI, R)
+OUTDIR="/gpfs/scratch/userexternal/gbolzon0/SuperFloat/" #os.getenv("ONLINE_REPO")
+
+
+def get_info(p,outdir):
+    wmo=p._my_float.wmo
+    filename="%s%s/MR%s_%03d.nc" %(outdir,wmo, wmo,p._my_float.cycle)
+    return filename
+
+def dumpfile(outfile,p,Pres,chl_profile,Qc):
+    PresT, Temp, QcT = p.read('TEMP', read_adjusted=False)
+    PresT, Sali, QcS = p.read('PSAL', read_adjusted=False)
+
+    print "dumping " + outfile
+    ncOUT = NC.netcdf_file(outfile,"w")
+    ncOUT.createDimension("DATETIME",14)
+    ncOUT.createDimension("NPROF", 1)
+    ncOUT.createDimension('nTEMP', len(PresT))
+    ncOUT.createDimension('nPSAL', len(PresT))
+    ncOUT.createDimension('nCHLA', len(Pres ))    
+    
+    ncvar=ncOUT.createVariable("REFERENCE_DATE_TIME", 'c', ("DATETIME",))
+    ncvar[:]=p.time.strftime("%Y%m%d%H%M%S")
+    ncvar=ncOUT.createVariable("JULD", 'd', ("NPROF",))
+    ncvar[:]=0.0
+    ncvar=ncOUT.createVariable("LONGITUDE", "d", ("NPROF",))
+    ncvar[:] = p.lon.astype(np.float64)
+    ncvar=ncOUT.createVariable("LATITUDE", "d", ("NPROF",))
+    ncvar[:] = p.lat.astype(np.float64)
+ 
+    
+    ncvar=ncOUT.createVariable('TEMP','f',('nTEMP',))
+    ncvar[:]=Temp
+    ncvar=ncOUT.createVariable('PRES_TEMP','f',('nTEMP',))
+    ncvar[:]=PresT
+    ncvar=ncOUT.createVariable('TEMP_QC','f',('nTEMP',))
+    ncvar[:]=QcT
+    
+    ncvar=ncOUT.createVariable('PSAL','f',('nTEMP',))
+    ncvar[:]=Sali
+    ncvar=ncOUT.createVariable('PRES_PSAL','f',('nTEMP',))
+    ncvar[:]=PresT
+    ncvar=ncOUT.createVariable('PSAL_QC','f',('nTEMP',))
+    ncvar[:]=QcS
+    
+    ncvar=ncOUT.createVariable('CHLA','f',('nCHLA',))
+    ncvar[:]=Pres
+    ncvar=ncOUT.createVariable('PRES_CHLA','f',('nCHLA',))
+    ncvar[:]=chl_profile
+    ncvar=ncOUT.createVariable('CHLA_QC','f',('nCHLA',))
+    ncvar[:]=Qc
+    ncOUT.close()
+
+
+for ip, pLov in enumerate(PROFILES_LOV[:1]):
+    pCor = bio_float.from_lov_profile(pLov)
+    is_only_lov = pCor is None
+    if is_only_lov:
+        outfile = get_info(pLov,OUTDIR)
+    else:
+        outfile = get_info(pCor,OUTDIR)
+    if os.path.exists(outfile): continue
+    os.system('mkdir -p ' + os.path.dirname(outfile))
+    
+    if is_only_lov:
+        Pres, Profile, Qc = pLov.read('CHLA',read_adjusted=True)
+        profile_for_dump = pLov
+    else:
+        Pres, Profile,Qc = superfloat_generator.synthesis_profile(pLov, pCor)
+        profile_for_dump = pCor
+    
+    if Pres is None: continue # no data
+    
+    dumpfile(outfile, profile_for_dump, Pres, Profile, Qc)
+
+
+
+# Deve gestire i profili only_coriolis
+PROFILES_COR =bio_float.FloatSelector('CHLA', TI, R)
+
+for ip, pCor in enumerate(PROFILES_COR):
+    outfile = get_info(pCor, OUTDIR)
+    if os.path.exists(outfile): continue
+    os.system('mkdir -p ' + os.path.dirname(outfile))
+    Pres, CHL, Qc=treating_coriolis(pCor)
+    if Pres is None: continue # no data
+    dumpfile(outfile, pCor, Pres, CHL, Qc)
+    
+    
+    
+    

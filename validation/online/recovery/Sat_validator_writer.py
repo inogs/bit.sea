ad3e9e4e55453efe090d200454973cb7d07bc3c9
Giorgio Bolzon <gbolzon@inogs.it>
2019-09-12 15:55:55 +0200

    updating recovery section, files are saved in a single directory

--- validation/online/recovery/Sat_validator_writer.py
+++ validation/online/recovery/Sat_validator_writer.py
@@ -1,17 +1,14 @@
 from commons.timeseries import TimeSeries
 from commons.time_interval import TimeInterval
 from dateutil.relativedelta import relativedelta
-TI = TimeInterval("20190401","20190801","%Y%m%d")
+TI = TimeInterval("20190701","20190912","%Y%m%d")
 archive_dir="/gpfs/work/OGS_prod_0/OPA/V5C/prod/archive/"
 
 var="P_l"
-LOC="/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/AVE/"
+
+
 T_bio = TimeSeries(TI, archive_dir,postfix_dir='POSTPROC/AVE_FREQ_1/ARCHIVE/',glob_pattern="ave*" +var + ".nc.gz")
 
-
-
-#T_bio.extract_analysis(LOC  + "ANALYSISgz/", command = "ln -s $INFILE $OUTFILE", remove_ext=False)
-#T_bio.extract_forecast(LOC  + "FORECASTgz/", command = "ln -s $INFILE $OUTFILE", remove_ext=False)
 
 print "#! /bin/bash"
 print "ONLINE_REPO=/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/ONLINE_V5C/"
@@ -22,17 +19,19 @@ print "FORECAST_DIR=/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/AVE/FORECAST"
 print ""
 
 Tuplelist=T_bio.get_runs([2])
+RUNDATE_DIR="/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/SAT_VALIDATION_ARCHIVE"
+command = "mkdir -p " + RUNDATE_DIR
+print command
+
 for time, path in Tuplelist[1:]:
     RUNDATE=time.strftime("%Y%m%d")
     time_f0=time - relativedelta(days=7)
     time_f1=time - relativedelta(days=6)
     time_f2=time - relativedelta(days=5)
-    RUNDATE_DIR="/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/archive/" + RUNDATE
-    command = "mkdir -p " + RUNDATE_DIR
-    print command
+       
     f_0_weekly_name=RUNDATE_DIR + "/Validation_f0_" + time_f0.strftime("%Y%m%d") + "_on_weekly_Sat." + time_f0.strftime("%Y%m%d") + ".nc"
     f_0__daily_name=RUNDATE_DIR + "/Validation_f0_" + time_f0.strftime("%Y%m%d") + "_on_daily_Sat."  + time_f0.strftime("%Y%m%d") + ".nc"
-    f_1__daily_name=RUNDATE_DIR + "/Validation_f1_" + time_f0.strftime("%Y%m%d") + "_on_daily_Sat."  + time_f1.strftime("%Y%m%d") + ".nc"
+    f_1__daily_name=RUNDATE_DIR + "/Validation_f1_" + time_f0.strftime("%Y%m%d") + "_on_daily_Sat."  + time_f1.strftime("%Y%Pm%d") + ".nc"
     f_2__daily_name=RUNDATE_DIR + "/Validation_f2_" + time_f0.strftime("%Y%m%d") + "_on_daily_Sat."  + time_f2.strftime("%Y%m%d") + ".nc"
     
     basecommand_w="python SatValidation.py -d %s -f $FORECAST_DIR -s $SAT_WEEKLY_DIR -o %s -m $MASKFILE " 
--- validation/online/recovery/biofloat_ms_launcher_writer.py
+++ validation/online/recovery/biofloat_ms_launcher_writer.py
@@ -2,7 +2,7 @@ from commons.Timelist import TimeList, TimeInterval
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-TI = TimeInterval("20190401","20190901","%Y%m%d")
+TI = TimeInterval("20190701","20190915","%Y%m%d")
 DIR="/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/AVE/ANALYSIS"
 
 TL = TimeList.fromfilenames(TI, DIR, "*nc", filtervar="N3n")
@@ -12,10 +12,13 @@ maskfile="/gpfs/work/OGS_prod_0/OPA/V5C/prod/wrkdir/2/MODEL/meshmask.nc"
 PROFILATORE="/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/PROFILATORE"
 
 print "#! /bin/bash"
+
+dirname = "/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/FLOAT_WEEKLY_VALIDATION_ARCHIVE/"
+print "mkdir -p " + dirname
+
 for req in WEEKLY_REQS:
     d = datetime.strptime(req.string, "%Y%m%d")
-    next_week = d+relativedelta(days=7)
-    dirname = "%s/ANALYSIS_PROFILES_1week" %(next_week.strftime("%Y%m%d"))
-    print "mkdir -p " + dirname
+    #next_week = d+relativedelta(days=7)
+    
     NRT_biofloat_out_name = "%s/BioFloat_Weekly_validation_%s.nc"  %(dirname,req.string )
     print "python biofloats_ms.py -d %s -m %s -b %s -o %s" %(req.string, maskfile, PROFILATORE, NRT_biofloat_out_name)

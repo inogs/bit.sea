a94885fd4e1da0880b6282dd3cacc9acec0479fd
Giorgio Bolzon <gbolzon@inogs.it>
2019-11-14 14:55:35 +0100

    1. basin expansion in order to get more data from nearest neighbours sub basins - based on table of Valeria Di Biagio    get_climatology performs the usual calculation, then    if the new argument basin_expand is True it re-places the average  of a sub basin with the mean of the averages of selected sub basins.
    2. Selection on layers is done as : (Pres>=layer.top) & (Pres<layer.bottom)
       in order to avoid measurements taken in account twice.
    
    3. DIC_ric variable is now used in carbonatic dataset
    4. Setting the official Time Interval for climatology
    5. adding __main__ section for tests

--- static/climatology.py
+++ static/climatology.py
@@ -3,6 +3,8 @@ from Nutrients_reader import NutrientsReader
 from Carbon_reader import CarbonReader
 from instruments.var_conversions import NUTRVARS
 import numpy as np
+from basins import V2 as OGS
+from basins.basin import SimplePolygonalBasin, ComposedBasin
 
 N=NutrientsReader()
 C=CarbonReader()
@@ -30,7 +32,7 @@ def DatasetInfo(modelvarname):
         var = 'ALK'
         dataset = C
     if modelvarname in ['O3c', 'DIC'] :
-        var ='DIC'
+        var ='DICric'
         dataset = C
     if modelvarname in ['pH', 'PH'] :
         var='PHt_{T-Press-ins}'
@@ -43,10 +45,65 @@ def DatasetInfo(modelvarname):
     return var, dataset
 
 
+def basin_expansion(sub, var):
+    '''
+    In order to have more data for each subbasin, sometimes we need to
+    expand the research to nearest neighbours.
+    The amount of available data depends on subbasin and var.
+    This function follows the table provided by Valeria Di Biagio
+    and returns a suitable subbasin (with nearest neighbours) to guarantee a
+    minimum amount of data in research.
 
-TI = TimeInterval("1950","2050","%Y")
+    Arguments:
+    * sub * a basin object
+    * var * string, can be "N1p","N3n","N5s","O2o","O3c","O3h"
+    Returns:
+    * search_sub * a sub object
+    '''
+    assert var in ["N1p","N3n","N5s","O2o","O3c","O3h"]
 
-def get_climatology(modelvarname, subbasinlist, LayerList ):
+    if (sub.name == "swm1"):
+        if var == "O2o":
+            return OGS.swm1
+        else:
+            return ComposedBasin('swm3', [OGS.alb, OGS.swm2], 'Neighbors of swm1')
+
+    if (sub.name == "tyr1"):
+        if var in ["N5s","O2o","O3c","O3h"]: return OGS.tyr2
+        return OGS.tyr1
+
+    if (sub.name == "adr1"):
+        if var in ["N3n", "O2o"]: return OGS.adr2
+        return OGS.adr1
+
+    if (sub.name == "ion1"):
+        if var in ["N3n", "O3c", "O3h"]: return ComposedBasin('ion4', [OGS.ion2, OGS.tyr2], 'Neighbors of ion1')
+        return OGS.ion1
+
+    if (sub.name == "lev3"):
+        if var in ["O3c","O3h"] : return ComposedBasin('lev5', [OGS.lev1, OGS.lev4], 'Neighbors of lev3')
+        return OGS.lev3
+
+    return sub
+
+def get_sub_indexes(sub_search):
+    '''
+    Returns a list of indexes
+    '''
+    if isinstance(sub_search, SimplePolygonalBasin):
+        index = OGS.Pred.basin_list.index(sub_search)
+        return [index]
+    if isinstance(sub_search, ComposedBasin):
+        L=[]
+        for sub in sub_search:
+            L.append(OGS.Pred.basin_list.index(sub))
+        return L
+
+
+
+TI = TimeInterval("1997","2016","%Y")
+
+def get_climatology(modelvarname, subbasinlist, LayerList, basin_expand=False):
     '''
     Returns 
     * CLIM * [nsub, nLayers] numpy array, a basic annual climatology
@@ -66,8 +123,26 @@ def get_climatology(modelvarname, subbasinlist, LayerList ):
             Pres   = np.concatenate((Pres,pres))
             Values = np.concatenate((Values,profile))
         for ilayer, layer in enumerate(LayerList):
-            ii = (Pres>=layer.top) & (Pres<=layer.bottom)
+            ii = (Pres>=layer.top) & (Pres<layer.bottom)
             if (ii.sum()> 1 ) :
                 CLIM[isub, ilayer] = Values[ii].mean()
                 STD[isub, ilayer] = Values[ii].std()
+
+    if basin_expand:
+        for isub, sub in enumerate(subbasinlist):
+            sub_search = basin_expansion(sub, modelvarname)
+            print isub, sub , sub_search
+            INDEX_LIST=get_sub_indexes(sub_search)
+            print INDEX_LIST
+            CLIM[isub,:] = CLIM[INDEX_LIST,:].mean(axis=0)
+            STD [isub,:] =  STD[INDEX_LIST,:].mean(axis=0) # brutto ...
+
     return CLIM, STD
+
+
+if __name__ == "__main__":
+    from commons.layer import Layer
+    PresDOWN=np.array([0,25,50,75,100,125,150,200,400,600,800,1000,1500,2000,2500])
+    LayerList=[ Layer(PresDOWN[k], PresDOWN[k+1])  for k in range(len(PresDOWN)-1)]
+    SUBLIST = OGS.P.basin_list
+    N3n_clim, N3n_std = get_climatology('N3n', SUBLIST, LayerList, basin_expand=True)

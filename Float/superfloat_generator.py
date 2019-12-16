02cef3697b202f0dd7b7e253f31c49405cf9ec26
Giorgio Bolzon <gbolzon@inogs.it>
2019-12-16 15:31:02 +0100

    New rules for chlorophyll NPQ; tested on coriolis_chla_quench_stat.py, applied on superfloat_generator

--- Float/superfloat_generator.py
+++ Float/superfloat_generator.py
@@ -3,8 +3,48 @@ import numpy as np
 import scipy.io.netcdf as NC
 
 
+def general_quenching(profile_obj, Pres, Profile, Qc):
+    '''
+    Manages the quenching for Coriolis profiles
+    When mld > 80m (flag 1 ) it doesn't do anything
+    When Z_top > mld + 5m (flag 2 ) idem
+    In the remaining cases: it applies the quenching, from mld to top
+
+    In case of cutted profile (Z_top > 10.) the quench value is replied up to the sea surface
+    on a set of extrapolation points, every 5 meters.
+    '''
+    PresT, Temp, _ = profile_obj.read('TEMP', read_adjusted=False)
+    mld = calculated_depths.mld(Temp, PresT, zref=0, deltaTemp=0.1)
+    print mld
+    if mld < 80:
+        if PresC[0] <= mld+5:
+            ii = PresC <= mld + 5
+            descending_ordered = np.sort(ValueC[ii])[-1::-1]
+            n_chosen = min(3, len(descending_ordered))
+            if n_chosen==0: n_chosen = 1
+            quench_value = descending_ordered[:n_chosen].mean()
+            ii = PresC < mld
+            Profile[ii] = quench_value
+
+            if PresC[0] > 10. : # cutted profiles
+                creation_step = 5 #m
+                Top_Pressures= np.arange(0.5,PresC[0],creation_step)
+                Top_Values   = np.ones_like(Top_Pressures)* quench_value
+                Top_QC       = np.ones_like(Top_Pressures)* 1
+
+                Pres    = np.concatenate((Top_Pressures, Pres), axis=0)
+                Profile = np.concatenate((Top_Values, Profile), axis=0)
+                Qc      = np.concatenate((Top_QC,          Qc), axis=0)
+
+    return Pres, Profile, Qc
+
+
 def quenching(profile_obj, PresChl, Chl, chl_lov_zero):
     '''
+    Apply quenching by substitution of chl values
+    chl(Mixed Layer) = max(chl(Mixed Layer))
+
+
     Arguments:
     * profile_obj  * a Coriolis profile object
     * Pres_chl     * numpy 1D-array of coriolis pressure
@@ -12,6 +52,8 @@ def quenching(profile_obj, PresChl, Chl, chl_lov_zero):
     * chl_lov_zero * value of LOV quench; an algorithm will try to guess the Mixed Layer Depth
                      calculated by LOV, in order to use it here to calculate quench value
                      if None, the Coriols Mixed Layer Depth will be used.
+    Returns:
+    * CHL          * numpy 1D-array of corrected chlorophyll
     '''
     Quenched_Chl = Chl.copy()
     if profile_obj.time.month in range(1,13):
@@ -111,7 +153,7 @@ def treating_coriolis(pCor):
             print "few values in Coriolis for " + pCor._my_float.filename
             return None, None, None
 
-        CHL = quenching(pCor, Pres, Value, None)
+        Pres, CHL, Qc = general_quenching(pCor, Pres, Value, Qc)
         ii=(Pres >= 400) & (Pres <= 600)
         if ii.sum() > 0:
             shift = CHL[ii].mean()
@@ -145,57 +187,62 @@ if __name__=="__main__":
     TI = TimeInterval('2012','2020','%Y')
     R = Rectangle(-6,36,30,46)
 
-    PROFILES_LOV =lovbio_float.FloatSelector('BBP700', TI, R)
-    for ip, pLov in enumerate(PROFILES_LOV[:]):
-        pCor=bio_float.from_lov_profile(pLov)
-        if pCor is not None:
-            PresL, ValueL, QcL = pLov.read('BBP700',read_adjusted=False)
-            if 'BBP700' in pCor.available_params:
-                PresC, ValueC, QcC = pCor.read('BBP700',read_adjusted=False)
-                if len(ValueC)==len(ValueL):
-                    if not are_identical(ValueC, ValueL, threshold=1.e-3):
-                        print "different", ip
-                        fig,ax =pl.subplots()
-                        ax.plot(ValueL,PresL,'r', label="LOV")
-                        ax.plot(ValueC  , PresC,'b.-', label="COR")
-                        ax.invert_yaxis()
-                        ax.grid()
-                        ax.legend()
-                        fig.show()
-                        sys.exit()
-                        if are_shifted(ValueC, ValueL):
-                            pass
-                        sys.exit()
-                else:
-                    print "different lengths", ip, len(ValueC), len(ValueL)
-
-
-    sys.exit()
-
-
-    PROFILES_LOV =lovbio_float.FloatSelector('PAR', TI, R)
-    for ip, pLov in enumerate(PROFILES_LOV[:1000]):
-        pCor=bio_float.from_lov_profile(pLov)
-        if pCor is not None:
-            PresL, ValueL, QcL = pLov.read('PAR',read_adjusted=False)
-            if 'DOWNWELLING_PAR' in pCor.available_params:
-                PresC, ValueC, QcC = pCor.read('DOWNWELLING_PAR',read_adjusted=False)
-                if not are_identical(ValueC, ValueL, 10):
-                    print "different", ip
-                    fig,ax =pl.subplots()
-                    ax.plot(ValueL,PresL,'r', label="LOV")
-                    ax.plot(ValueC  , PresC,'b.-', label="COR")
-                    ax.invert_yaxis()
-                    ax.grid()
-                    ax.legend()
-                    fig.show()
-                    # sys.exit()
-
-    sys.exit()
-    PROFILES_LOV =lovbio_float.FloatSelector('CHLA', TI, R)
+#     PROFILES_LOV =lovbio_float.FloatSelector('BBP700', TI, R)
+#     for ip, pLov in enumerate(PROFILES_LOV[:]):
+#         pCor=bio_float.from_lov_profile(pLov)
+#         if pCor is not None:
+#             PresL, ValueL, QcL = pLov.read('BBP700',read_adjusted=False)
+#             if 'BBP700' in pCor.available_params:
+#                 PresC, ValueC, QcC = pCor.read('BBP700',read_adjusted=False)
+#                 if len(ValueC)==len(ValueL):
+#                     if not are_identical(ValueC, ValueL, threshold=1.e-3):
+#                         print "different", ip
+#                         fig,ax =pl.subplots()
+#                         ax.plot(ValueL,PresL,'r', label="LOV")
+#                         ax.plot(ValueC  , PresC,'b.-', label="COR")
+#                         ax.invert_yaxis()
+#                         ax.grid()
+#                         ax.legend()
+#                         fig.show()
+#                         sys.exit()
+#                         if are_shifted(ValueC, ValueL):
+#                             pass
+#                         sys.exit()
+#                 else:
+#                     print "different lengths", ip, len(ValueC), len(ValueL)
+#
+#
+#     sys.exit()
+#
+#
+#     PROFILES_LOV =lovbio_float.FloatSelector('PAR', TI, R)
+#     for ip, pLov in enumerate(PROFILES_LOV[:1000]):
+#         pCor=bio_float.from_lov_profile(pLov)
+#         if pCor is not None:
+#             PresL, ValueL, QcL = pLov.read('PAR',read_adjusted=False)
+#             if 'DOWNWELLING_PAR' in pCor.available_params:
+#                 PresC, ValueC, QcC = pCor.read('DOWNWELLING_PAR',read_adjusted=False)
+#                 if not are_identical(ValueC, ValueL, 10):
+#                     print "different", ip
+#                     fig,ax =pl.subplots()
+#                     ax.plot(ValueL,PresL,'r', label="LOV")
+#                     ax.plot(ValueC  , PresC,'b.-', label="COR")
+#                     ax.invert_yaxis()
+#                     ax.grid()
+#                     ax.legend()
+#                     fig.show()
+#                     # sys.exit()
+#
+#     sys.exit()
+    PROFILES_LOV =lovbio_float.FloatSelector('CHLA', TI, R)   
+#    PROFILES_LOV = lovbio_float.filter_by_wmo(PROFILES_LOV, "6901769")
+    counter=0
     BAD_LIST=[]
     for ip, pLov in enumerate(PROFILES_LOV[:]):
+        #if pLov._my_float.wmo == "6901765" : continue # save time
+        #if pLov._my_float.wmo != "6901772" : continue # save time
         pCor=bio_float.from_lov_profile(pLov)
+        
         if pCor is not None:
             PresL, ValueL, QcL = pLov.read('CHLA',read_adjusted=True)
             if 'CHLA' in pCor.available_params:
@@ -216,29 +263,33 @@ if __name__=="__main__":
                 print "no LOV data"
                 continue
 
-
             if has_drift(PresC, ValueC):
                 print pCor._my_float.filename + " has drift"
                 continue
-            CHL = quenching(pCor, PresC, ValueC,chl_lov_zero=None) #ValueL[0])
+
+            ValueCorig = ValueC.copy()
+            PresCorig  = PresC.copy()
+            PresC, CHL, Qc = general_quenching(pCor, PresC, ValueC, QcC)
+
 
             ii=(PresC >= 400) & (PresC <= 600)
             if ii.sum() > 0:
                 shift = CHL[ii].mean()
                 CHL = CHL - shift
 
-            if not are_identical(CHL, ValueL):
-                if not are_shifted(CHL, ValueL):
+
+            if True: #not are_identical(CHL, ValueL):
+                if True: # not are_shifted(CHL, ValueL):
                     BAD_LIST.append(ip)
-                    print ip
                     fig,ax =pl.subplots()
                     ax.plot(ValueL,PresL,'r', label="LOV")
+                    ax.plot(ValueCorig  , PresCorig,'m.-', label="CORorig")
                     ax.plot(CHL  , PresC,'b.-', label="COR")
                     ax.invert_yaxis()
                     ax.grid()
                     ax.legend()
-                    #fig.show()
-                    pl.savefig(pCor.ID()+ ".png")
-                    pl.close(fig)
-                    #import sys
-                    #sys.exit()
+                    fig.show()
+                    counter+=1
+                    #pl.savefig(pCor.ID()+ ".png")
+                    #pl.close(fig)
+                    if counter > 10: sys.exit()

f20e7bb8354b7acc6647842dca5ba1d4d6f26bea
Giorgio Bolzon <gbolzon@inogs.it>
2020-01-17 12:20:02 +0100

    Adding interfaces to gen_3dvar_satfloat_namelists.py

--- Float/launcher_datimes.sh
+++ Float/launcher_datimes.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
 export ONLINE_REPO=/gpfs/scratch/userexternal/gbolzon0/CHAIN_V5C/ONLINE_V5C/
-
+DA_TIMES_SAT=/gpfs/scratch/userexternal/ateruzzi/MULTIVARIATE_24/TEST_04/wrkdir/MODEL/daTimes_sat
 DAFREQ=1
 DA_HH=13
 
-DATIMES_DIR=./
-OUTFILE=${DATIMES_DIR}/daTimes
+DATIMES_WRKDIR=./
+OUTDIR=DA_NML
+OUTFILE=${DATIMES_WRKDIR}/daTimes
+
+
+mkdir -p $OUTDIR $DATIMES_WRKDIR
 
 for VARDA in N3n P_l; do
-python profile_dates_DAfreq.py -f $DAFREQ -t $DA_HH -s 20170102 -e 20170201 -v ${VARDA} -o ${DATIMES_DIR}/daTimes_float_${VARDA}
+python profile_dates_DAfreq.py -f $DAFREQ -t $DA_HH -s 20170102 -e 20170201 -v ${VARDA} -o ${DATIMES_WRKDIR}/daTimes_float_${VARDA}
 done
 
-# Modify varlist in merge_daTimes.py
-DA_TIMES_SAT=/gpfs/scratch/userexternal/ateruzzi/MULTIVARIATE_24/TEST_04/wrkdir/MODEL/daTimes_sat
-python merge_daTimes.py -s $DA_TIMES_SAT -d $DATIMES_DIR -o ${OUTFILE}
+# Edit varlist in merge_daTimes.py
+python merge_daTimes.py -s $DA_TIMES_SAT -d $DATIMES_WRKDIR -o ${OUTFILE}
+
+
+python gen_3dvar_satfloat_namelists.py -o $OUTDIR -t satfloat.template -r $OUTFILE -s $DA_TIMES_SAT -d $DATIMES_WRKDIR

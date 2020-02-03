ac378ad70dc3ce4256edf4eabb0147b2310a6ee7
Elena Terzic <eterzic0@r033c01s03.galileo.cineca.it>
2020-02-03 13:00:37 +0100

    Adding codes for postproc

--- config.sh
+++
@@ -1,3 +0,0 @@
-export MASKFILE=/pico/home/usera07ogs/a07ogs00/OPA/V2C/etc/static-data/MED1672_cut/MASK/meshmask.nc
-export SUBMASKFILE=/pico/home/usera07ogs/a07ogs00/OPA/V2C/etc/static-data/POSTPROC/submask.nc
-

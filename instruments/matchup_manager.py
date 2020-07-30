9518c71a42dcc3f39dc03a38de8d079505e3816f
Elena <eterzic@inogs.it>
2020-07-30 17:27:17 +0200

    Adding a couple of prints to check the matchup_manager.py

--- instruments/matchup_manager.py
+++ instruments/matchup_manager.py
@@ -266,6 +266,11 @@ class Matchup_Manager():
                 continue
 
             Modelfile = self.profilingDir + "PROFILES/" + Model_time.strftime("ave.%Y%m%d-%H:%M:%S.profiles.nc")
+
+            print('Modelfile = ', Modelfile)
+            print('Model_time = ', Model_time)
+            print('p_ID = ', p.ID())
+            
             ModelProfile = self.readModelProfile(Modelfile, model_varname, p.ID())
             seaPoints = ~np.isnan(ModelProfile)
 

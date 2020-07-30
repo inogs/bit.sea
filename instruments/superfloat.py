fcd08bff52eb361b10aa8847042fd10c0ba4b887
Elena <eterzic@inogs.it>
2020-07-30 18:18:36 +0200

    Adding read_fitted in superfloat, as in optbio_float_2019

--- instruments/superfloat.py
+++ instruments/superfloat.py
@@ -48,6 +48,17 @@ class BioFloatProfile(Profile):
 
         return self._my_float.read(var)
 
+
+    def read_fitted(self,var, func):
+        '''
+        This is used to calculate the radiometric profile values at the surface.
+        Use an exponential fit and extrapolate values to the surface.
+        Returns 3 numpy arrays: Pres, Profile, Qc with values extrapolated to 0 m
+        '''
+
+        return self._my_float.read_fitted(var, func, mean=self.mean)
+
+
     def name(self):
         '''returns a string, the wmo of the associated BioFloat.
         '''
@@ -100,6 +111,54 @@ class BioFloat(Instrument):
         ncIN.close()
 
         return Pres, Profile, Qc
+
+
+    def read_fitted(self, var, func, mean=None):
+        '''
+        This is used to calculate the radiometric profile values at the surface.
+        Use an exponential fit and extrapolate values to the surface.
+        Returns the irradiance values at the surface.
+
+        Reads profile data from file, applies a rarefaction and optionally a filter to the data
+
+        Returns the measurement's surface value
+
+        '''
+
+        raw_pres, raw_prof   = self.read_raw(var)
+        pres,index           = np.unique(raw_pres,return_index=True)
+        prof                 = raw_prof[index]
+
+        if pres[0] < 1.5 and pres[3] < 10.: 
+        
+            popt,_   = curve_fit(func, pres, prof)
+            pres_new = np.insert(pres, 0, 0.)  if not pres[0] == 0. else pres # Add z=0 m
+            prof_new = func(pres_new, *popt)
+
+        else:
+            pres_new = pres
+            prof_new = prof
+
+        qc       = np.ones_like(pres_new)*2
+
+        #import matplotlib.pyplot as plt
+        #plt.plot(prof, -pres, 'm', label='Profile')
+        #plt.plot(prof_new, -pres_new, 'b', label='Curve fit')
+        #plt.legend(loc='best')
+        #plt.show()
+        
+        if pres_new.size ==0:
+            return pres_new, prof_new, qc
+
+        if mean == None:
+            if BioFloat.default_mean != None:
+                return pres_new, BioFloat.default_mean.compute(prof_new, pres_new), qc
+            else:
+                return pres_new, prof_new, qc
+        else:
+            return pres_new, mean.compute(prof_new, pres_new), mean.compute(qc,pres_new)
+
+
     def origin(self,var):
         '''
         Arguments:

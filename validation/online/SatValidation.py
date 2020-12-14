db093f8c8bae8bb8f9a22e928913efd9235ce61c
Anna Teruzzi <ateruzzi@r033c01s04.galileo.cineca.it>
2020-12-14 12:01:09 +0100

    New SatValidation for monthly report to be sent to PQ dashboard

--- validation/online/SatValidation.py
+++ validation/online/SatValidation.py
@@ -11,7 +11,12 @@ def argument():
                                 type = str,
                                 required = True,
                                 help = 'input model file')
+
     parser.add_argument(   '--satfile','-s',
+                                type = str,
+                                required = True)
+    
+    parser.add_argument(   '--climafile','-c',
                                 type = str,
                                 required = True)
 
@@ -50,9 +55,17 @@ def weighted_mean(Conc, Weight):
     Weighted_Mean   = Mass/Weight_sum
     return Weighted_Mean
 
+def weighted_var(Conc, Weight):
+    ConcMean = weighted_mean(Conc,Weight)
+    Weight_sum      = Weight.sum()
+    Mass            = ((Conc-ConcMean)**2 * Weight).sum()
+    Weighted_Var    = Mass/Weight_sum
+    return Weighted_Var
+
 
 
 modfile =args.modelfile
+climafile = args.climafile
 satfile=args.satfile
 outfile = args.outfile
 
@@ -76,12 +89,16 @@ COASTNESS_LIST=['coast','open_sea','everywhere']
 nCOAST = len(COASTNESS_LIST)
 dtype = [(coast, np.bool) for coast in COASTNESS_LIST]
 COASTNESS = np.ones((jpj,jpi),dtype=dtype)
-COASTNESS['coast']     = ~mask200_2D;
-COASTNESS['open_sea']  =  mask200_2D;
+COASTNESS['coast']     = ~mask200_2D
+COASTNESS['open_sea']  =  mask200_2D
 #COASTNESS['everywhere'] = True;
 
-De = DataExtractor(TheMask,filename=modfile,varname='P_l')
-Model = De.values[0,:,:]
+De = DataExtractor(TheMask,filename=modfile,varname='P_l',dimvar=2)
+# Model = De.values[0,:,:]
+Model = De.values[:,:]
+
+Declim = DataExtractor(TheMask,filename=climafile,varname='P_l',dimvar=2)
+Clima = Declim.values[:,:]
 try:
 #    Sat24 = Sat.readfromfile(satfile,'lchlm') # weekly
     Sat24 = Sat.readfromfile(satfile,'CHL') # weekly
@@ -90,19 +107,33 @@ except:
 
 cloudsLand = np.isnan(Sat24)
 Sat24[cloudsLand] = -999.0
-cloudsLand = Sat24==-999.0; 
+cloudsLand = Sat24==-999.0
 modelLand  = Model > 1.0e+19
 nodata     = cloudsLand | modelLand
 
 
 
-BGC_CLASS4_CHL_RMS_SURF_BASIN      = np.zeros((nSUB,nCOAST),np.float32)
-BGC_CLASS4_CHL_BIAS_SURF_BASIN     = np.zeros((nSUB,nCOAST),np.float32)
 VALID_POINTS                       = np.zeros((nSUB,nCOAST),np.float32)
+
 MODEL_MEAN                         = np.zeros((nSUB,nCOAST),np.float32)
 SAT___MEAN                         = np.zeros((nSUB,nCOAST),np.float32)
+MODEL_VARIANCE                     = np.zeros((nSUB,nCOAST),np.float32)
+SAT___VARIANCE                     = np.zeros((nSUB,nCOAST),np.float32)
+BGC_CLASS4_CHL_BIAS_SURF_BASIN     = np.zeros((nSUB,nCOAST),np.float32)
+BGC_CLASS4_CHL_RMS_SURF_BASIN      = np.zeros((nSUB,nCOAST),np.float32)
+BGC_CLASS4_CHL_CORR_SURF_BASIN     = np.zeros((nSUB,nCOAST),np.float32)
+
+MODEL_MEAN_LOG                     = np.zeros((nSUB,nCOAST),np.float32)
+SAT___MEAN_LOG                     = np.zeros((nSUB,nCOAST),np.float32)
+MODEL_VARIANCE_LOG                 = np.zeros((nSUB,nCOAST),np.float32)
+SAT___VARIANCE_LOG                 = np.zeros((nSUB,nCOAST),np.float32)
 BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG  = np.zeros((nSUB,nCOAST),np.float32)
 BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG = np.zeros((nSUB,nCOAST),np.float32)
+BGC_CLASS4_CHL_CORR_SURF_BASIN_LOG = np.zeros((nSUB,nCOAST),np.float32)
+# varianceCheck = np.zeros((nSUB,nCOAST),np.float32)
+
+ANOMALY_CORR     = np.zeros((nSUB,nCOAST),np.float32)
+ANOMALY_CORR_LOG = np.zeros((nSUB,nCOAST),np.float32)
 
 for icoast, coast in enumerate(COASTNESS_LIST):
 
@@ -113,21 +144,51 @@ for icoast, coast in enumerate(COASTNESS_LIST):
         if M.number() > 0 :
             BGC_CLASS4_CHL_RMS_SURF_BASIN[isub,icoast]  = M.RMSE()
             BGC_CLASS4_CHL_BIAS_SURF_BASIN[isub,icoast] = M.bias()
+            BGC_CLASS4_CHL_CORR_SURF_BASIN[isub,icoast] = M.correlation()
                  
             weight = TheMask.area[selection]
             MODEL_MEAN[isub,icoast] = weighted_mean( M.Model,weight)
             SAT___MEAN[isub,icoast] = weighted_mean( M.Ref,  weight)
+            MODEL_VARIANCE[isub,icoast] = weighted_var( M.Model,weight)
+            SAT___VARIANCE[isub,icoast] = weighted_var( M.Ref,  weight)
+            # _,varianceCheck[isub,icoast] = M.variances()
         
             Mlog = matchup.matchup(np.log10(Model[selection]), np.log10(Sat24[selection])) #add matchup based on logarithm
             BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG[isub,icoast]  = Mlog.RMSE()
             BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG[isub,icoast] = Mlog.bias()
+            BGC_CLASS4_CHL_CORR_SURF_BASIN_LOG[isub,icoast] = Mlog.correlation()
+
+            MODEL_MEAN_LOG[isub,icoast] = weighted_mean( Mlog.Model,weight)
+            SAT___MEAN_LOG[isub,icoast] = weighted_mean( Mlog.Ref,  weight)
+            MODEL_VARIANCE_LOG[isub,icoast] = weighted_var( Mlog.Model,weight)
+            SAT___VARIANCE_LOG[isub,icoast] = weighted_var( Mlog.Ref,  weight)
+
+            Mclima = matchup.matchup(Model[selection]-Clima[selection],  \
+                                     Sat24[selection]-Clima[selection])
+            ANOMALY_CORR[isub,icoast] = Mclima.correlation()
+            
+            Mclima_log = matchup.matchup(np.log10(Model[selection])-np.log10(Clima[selection]), \
+                                         np.log10(Sat24[selection])-np.log10(Clima[selection]))
+            ANOMALY_CORR_LOG[isub,icoast] = Mclima_log.correlation()
         else:
             BGC_CLASS4_CHL_RMS_SURF_BASIN[isub,icoast] = np.nan
             BGC_CLASS4_CHL_BIAS_SURF_BASIN[isub,icoast]= np.nan
+            BGC_CLASS4_CHL_CORR_SURF_BASIN[isub,icoast]= np.nan
             MODEL_MEAN[isub,icoast] = np.nan
             SAT___MEAN[isub,icoast] = np.nan
+            MODEL_VARIANCE[isub,icoast] = np.nan
+            SAT___VARIANCE[isub,icoast] = np.nan
+            
             BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG[isub,icoast] = np.nan
             BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG[isub,icoast]= np.nan
+            BGC_CLASS4_CHL_CORR_SURF_BASIN_LOG[isub,icoast]= np.nan
+            MODEL_MEAN_LOG[isub,icoast] = np.nan
+            SAT___MEAN_LOG[isub,icoast] = np.nan
+            MODEL_VARIANCE_LOG[isub,icoast] = np.nan
+            SAT___VARIANCE_LOG[isub,icoast] = np.nan
+
+            ANOMALY_CORR[isub,icoast]     = np.nan
+            ANOMALY_CORR_LOG[isub,icoast] = np.nan
 
 
 
@@ -151,17 +212,47 @@ ncvar[:] = BGC_CLASS4_CHL_RMS_SURF_BASIN
 ncvar = ncOUT.createVariable('BGC_CLASS4_CHL_BIAS_SURF_BASIN', 'f', ('nsub','ncoast'))
 ncvar[:] = BGC_CLASS4_CHL_BIAS_SURF_BASIN
 
+ncvar = ncOUT.createVariable('BGC_CLASS4_CHL_CORR_SURF_BASIN', 'f', ('nsub','ncoast'))
+ncvar[:] = BGC_CLASS4_CHL_CORR_SURF_BASIN
+
 ncvar = ncOUT.createVariable('MODEL_MEAN', 'f', ('nsub','ncoast'))
 ncvar[:] = MODEL_MEAN
 
 ncvar=ncOUT.createVariable('SAT___MEAN', 'f', ('nsub','ncoast'))
 ncvar[:] = SAT___MEAN
 
+ncvar = ncOUT.createVariable('MODEL_VARIANCE', 'f', ('nsub','ncoast'))
+ncvar[:] = MODEL_VARIANCE
+
+ncvar=ncOUT.createVariable('SAT___VARIANCE', 'f', ('nsub','ncoast'))
+ncvar[:] = SAT___VARIANCE
+
 ncvar = ncOUT.createVariable('BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG', 'f', ('nsub','ncoast'))
 ncvar[:] = BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG
 
 ncvar= ncOUT.createVariable('BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG', 'f', ('nsub','ncoast'))
 ncvar[:] =BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG
+
+ncvar= ncOUT.createVariable('BGC_CLASS4_CHL_CORR_SURF_BASIN_LOG', 'f', ('nsub','ncoast'))
+ncvar[:] =BGC_CLASS4_CHL_CORR_SURF_BASIN_LOG
+
+ncvar = ncOUT.createVariable('MODEL_MEAN_LOG', 'f', ('nsub','ncoast'))
+ncvar[:] = MODEL_MEAN_LOG
+
+ncvar=ncOUT.createVariable('SAT___MEAN_LOG', 'f', ('nsub','ncoast'))
+ncvar[:] = SAT___MEAN_LOG
+
+ncvar = ncOUT.createVariable('MODEL_VARIANCE_LOG', 'f', ('nsub','ncoast'))
+ncvar[:] = MODEL_VARIANCE_LOG
+
+ncvar=ncOUT.createVariable('SAT___VARIANCE_LOG', 'f', ('nsub','ncoast'))
+ncvar[:] = SAT___VARIANCE_LOG
+
+ncvar=ncOUT.createVariable('ANOMALY_CORRELATION', 'f', ('nsub','ncoast'))
+ncvar[:] = ANOMALY_CORR
+
+ncvar=ncOUT.createVariable('ANOMALY_CORRELATION_LOG', 'f', ('nsub','ncoast'))
+ncvar[:] = ANOMALY_CORR_LOG
 
 ncOUT.close()
     

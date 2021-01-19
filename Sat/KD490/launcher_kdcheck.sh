b91c43b6f3256dc48a0de9daf8bb3bbe0af03471
Giorgio Bolzon <gbolzon@inogs.it>
2021-01-19 10:07:34 +0100

    Renaming sat_check_kd.py -> sat_check.py

--- Sat/KD490/launcher_kdcheck.sh
+++ Sat/KD490/launcher_kdcheck.sh
@@ -3,4 +3,4 @@ CHECKDIR= # output dir
 CLIMFILE=/gpfs/scratch/userexternal/gbolzon0/REA24/SAT/KD490/KD490_Climatology_1km.nc
 SATMESH= # The same used as "-m argument" for sat_check on chlorophyll
 
-echo python sat_check_kd.py -i $ORIGDIR -o $CHECKDIR -c $CLIMFILE -m $SATMESH
+echo python sat_check.py -i $ORIGDIR -o $CHECKDIR -c $CLIMFILE -m $SATMESH

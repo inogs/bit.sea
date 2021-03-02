21e3d56611b15e60adcf08a9247ba3a0b6181f2d
Giorgio Bolzon <gbolzon@inogs.it>
2021-03-02 09:53:35 +0100

    oxygen_saturation.py moved in Float/ section

---
+++ Float/oxygen_saturation.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+def oxy_sat(p):
+    '''
+    Calculate the oxygen at saturation with the formula from
+    Garcia and Gordon, 1992 L&O
+    using Temperature and Salinity "at surface" misurated by
+    argo float. Instead of the "real surface", for T and S we adopt
+    the mean value of the first 5m.
+    As input, it requires the profile object.
+    '''
+    
+
+    PresT, Temp, QcT = p.read('TEMP', read_adjusted=False)#p._my_float.adjusted('TEMP'))
+    PresS, Sali, QcS = p.read('PSAL', read_adjusted=False)#p._my_float.adjusted('PSAL'))
+
+    ii = PresT<=5
+
+    temp=np.mean(Temp[ii]) 
+    sal =np.mean(Sali[ii])
+
+    ts = np.log( ( 298.15 - temp) / ( temp + 273.15 ) )
+    ts2 = ts * ts 
+
+    lnk = 2.00856 + 3.22400 *ts + 3.99063 *ts2 + 4.80299 *ts *ts2 + 9.78188 * 10**(-01) * ts2 *ts2 + 1.71069*ts2 *ts2 *ts +  sal  * (-6.24097 * 10**(-03) -6.93498 * 10**(-03) * ts -6.90358 * 10**(-03) * ts2 -4.29155 * 10**(-03) *ts *ts2) -3.11680 * 10**(-7)  *sal *sal
+    o2sat_ml= np.exp(lnk)  # oxygen saturation in ml / l
+
+    o2sat_mmol=  o2sat_ml * 1000 / 22.391  # Oxygen saturation in (mmol/m3)  % ICES conversion volume of 1 mole O2 at STP
+
+    return o2sat_mmol
+
+
+
+def oxy_check(Pres,Prof,p):
+    '''
+    Check float oxygen quality comparing surface value with oxygen saturation
+    Argument:
+    * Pres * numpy 1d array, pressure profile
+    * Prof * Oxygen values profile
+    * p * float profile object (needed for T and S inn the Oxy Sat calculation)
+
+    Return:
+    * CR * Check Result wich is 0 ==> check passed
+                                1 ==> check failed
+    '''
+
+
+    Osat = oxy_sat(p)
+
+    bad=(np.isnan(Prof))
+    Prof_good = Prof[~bad]
+    Pres_good = Pres[~bad]
+ 
+# Define O2o surface as the mean values in the first 5m from the sea surface:
+    Osurf=np.mean(Prof_good[Pres_good<=5])
+
+    mydiff = np.abs(Osurf-Osat)
+    
+    if (mydiff < 10):
+       return True
+    else:
+       return False
+
+
+if __name__ == "__main__":
+
+    from commons.time_interval import TimeInterval
+    from instruments import bio_float
+    from basins import V2 as OGS
+
+
+    DATESTART = "20190101"
+    DATE__END = "20191231"
+
+    T_INT = TimeInterval(DATESTART,DATE__END, '%Y%m%d')
+    var="DOXY"
+
+    Profilelist=bio_float.FloatSelector(var,T_INT,OGS.med)
+    p=Profilelist[100]
+
+    Pres, Prof, Qc= p.read(var,read_adjusted=True)
+    CR=oxy_check(Pres,Prof,p)
+
+    print "DOXY check is " + str(CR)
+
+    if not CR:
+               Prof=np.nan*np.ones_like(Prof)
+               Qc=4*np.ones_like(Qc)
+               print "Exclude WMO " + np.str(p._my_float.wmo) + " - " + p._my_float.time.strftime("%Y%m%d")
+

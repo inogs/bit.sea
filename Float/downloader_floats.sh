1bade88cf21cb2585501cc309e502166a0bd4884
Giorgio Bolzon <gbolzon@inogs.it>
2021-03-08 17:00:22 +0100

    Working version of downloader_floats: download in a tmp dir, then moves files in CORIOLIS dataset

--- Float/downloader_floats.sh
+++ Float/downloader_floats.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-DIR=/galileo/home/userexternal/gcoidess/test_float_download/
-HOST='ftp.ifremer.fr'
-USER='anonymous'
+DIFF_FILE=/galileo/home/userexternal/gcoidess/test_float_download/DIFF_floats.txt
+LOCALDIR=/gpfs/scratch/userexternal/gbolzon0/V7C/TMP_CORIOLIS
+HOST=ftp.ifremer.fr
+USER=anonymous
 REMOTEDIR=/ifremer/argo/dac/
-LOCALDIR=$DIR/download/
-mkdir -p $LOCALDIR
+export ONLINE_REPO=/gpfs/scratch/userexternal/gbolzon0/V7C/ONLINE
 
-for I in `awk --field-separator=, '{print $1}' $DIR/DIFF_floats.txt`
-do ncftpget -u $USER $HOST $LOCALDIR $REMOTEDIR/$I
+mkdir -p $LOCALDIR
+for I in `awk --field-separator=, '{print $1}'  $DIFF_FILE ` ; do
+  ncftpget -u $USER $HOST $LOCALDIR $REMOTEDIR/$I
 done
 
+
+echo "Moving NetCDF files from TMP dir to ONLINE CORIOLIS file"
+cd $LOCALDIR
+for I in `ls *nc ` ; do
+   WMO=${I:2:7}
+   CORIOLIS_DIR=$ONLINE_REPO/CORIOLIS/${WMO}/
+   mkdir -p $CORIOLIS_DIR
+   mv $I $CORIOLIS_DIR
+done

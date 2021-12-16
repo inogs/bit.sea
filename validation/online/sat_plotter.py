Bug fix in sat_plotter, about wrong plot of red line (EAN_RMSD). Avoid use of times1, times2: just times, read from dates in filenames rotation of time labels, it was not readable

validation/online/sat_plotter.py
@@ -49,8 +49,8 @@ def avoid_diagonals_in_jumps(x,y):
     newX=[]
     for i in range(n-1):
         if y[i]==y[i+1]:
-           newY.append(y[i])
-           newX.append(x[i])
+            newY.append(y[i])
+            newX.append(x[i])
         else:
             newX.append(x[i])
             newX.append(x[i])
@@ -95,24 +95,22 @@ for isub, sub in enumerate(OGS.P):
     if sub.name == 'atl': continue
     print(sub.name)
     outfile=OUTFIG_DIR + "NRTvalidation_chlsup_" + sub.name + "_" + coast + ".png"
-    #matplotlib.rc('xtick', labelsize=12)
     fig, (ax1, ax2, ax3) = pl.subplots(3, sharex=True, figsize=(15,10)) 
     # BIAS
 
     times, f0= F0.plotdata(F0.bias, sub.name,coast) ; ax1.plot(times,f0,'-k' , label='1-day lead time fc') # label='1$^{st}$ day of forecast')
-    times1=times
+
     times, f1= F1.plotdata(F1.bias, sub.name,coast);  ax1.plot(times,f1,'--b', label='2-day lead time fc') # label='2$^{nd}$ day of forecast')  
     times, f2= F2.plotdata(F2.bias, sub.name,coast);  ax1.plot(times,f2,':g' , label='3-day lead time fc') # label='3$^{rd}$ day of forecast')
-    times2=times
-    times1.extend(times2[5:])
 
-    EAN_BIAS=np.zeros((len(times1),nSub), dtype = np.float32)*np.nan
-    for it, t in enumerate(times1):
+
+    EAN_BIAS=np.zeros((len(times),nSub), dtype = np.float32)*np.nan
+    for it, t in enumerate(times):
         if (( t.month <= 5 ) | ( t.month >= 10 )):
-           EAN_BIAS[it,isub] = np.nan
+            EAN_BIAS[it,isub] = np.nan
         else:
-           EAN_BIAS[it,isub] = EAN_BIAS_s[isub]
-    ax1.plot(times1,EAN_BIAS[:,isub],marker='_',color='red',linewidth=2.0, label="EAN")
+            EAN_BIAS[it,isub] = EAN_BIAS_s[isub]
+    ax1.plot(times,EAN_BIAS[:,isub],marker='_',color='red',linewidth=2.0, label="EAN")
 
     
     ax1.set_title(sub.extended_name + " (" + coast + ")",fontsize=14)
@@ -130,13 +128,13 @@ for isub, sub in enumerate(OGS.P):
     times, f1= F1.plotdata(F1.rmse, sub.name,coast) ; ax2.plot(times,f1,'--b') 
     times, f2= F2.plotdata(F2.rmse, sub.name,coast) ; ax2.plot(times,f2,':g')
 
-    EAN_RMSD=np.ndarray((len(times1),nSub), dtype = np.float32)
-    for it, t in enumerate(times1):
+    EAN_RMSD=np.zeros((len(times),nSub), dtype = np.float32)
+    for it, t in enumerate(times):
         if (( t.month <= 5 ) | ( t.month >= 10 )):
-           EAN_RMSD[it,isub] = EAN_RMSD_w[isub]
+            EAN_RMSD[it,isub] = EAN_RMSD_w[isub]
         else:
-           EAN_RMSD[it,isub] = EAN_RMSD_s[isub]
-    new_times, new_y = avoid_diagonals_in_jumps(times1, EAN_RMSD[:,isub])
+            EAN_RMSD[it,isub] = EAN_RMSD_s[isub]
+    new_times, new_y = avoid_diagonals_in_jumps(times, EAN_RMSD[:,isub])
     ax2.plot(new_times, new_y,marker='_',color='red',linewidth=2.0)
     
     ax2.set_ylabel('RMS mg/m$^3$',fontsize=14)
@@ -151,6 +149,8 @@ for isub, sub in enumerate(OGS.P):
     ax3.set_ylabel('# of points',fontsize=14)
     ax3.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=0, interval=2))
     ax3.xaxis.set_major_formatter(mdates.DateFormatter("%d-%b-%y"))
+    xlabels = ax3.get_xticklabels()
+    pl.setp(xlabels,rotation=30)
 
     fig.subplots_adjust(hspace=0.1)
     pl.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
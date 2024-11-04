1802b06e600a59980333b983eccae498e86b99ea
Giorgio Bolzon <gbolzon@ogs.it>
2024-11-04 18:13:55 +0100

    Functional call of sat_check_QI.py

--- src/bitsea/Sat/sat_check_QI.py
+++ src/bitsea/Sat/sat_check_QI.py
@@ -1,5 +1,8 @@
 import argparse
 from bitsea.utilities.argparse_types import some_among
+from bitsea.utilities.argparse_types import existing_dir_path
+
+
 def argument():
     parser = argparse.ArgumentParser(description = '''
     Apply check based on QI provided by OCTAC
@@ -10,13 +13,13 @@ def argument():
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(   '--origdir', '-i',
-                                type = str,
+                                type = existing_dir_path,
                                 required = True,
                                 help = ''' ORIG sat directory'''
                                 )
 
     parser.add_argument(   '--checkdir', '-o',
-                                type = str,
+                                type = existing_dir_path,
                                 required = True,
                                 help = ''' Base for CHECKED sat directory'''
                                 )
@@ -56,62 +59,86 @@ def argument():
 
 args = argument()
 
+from pathlib import Path
+from typing import List
 from bitsea.commons.Timelist import TimeList
-from bitsea.commons.utils import addsep
 from bitsea.commons.time_interval import TimeInterval
 from bitsea.postproc import masks
 import numpy as np
-import os
 from bitsea.Sat import SatManager as Sat
 from bitsea.utilities.mpi_serial_interface import get_mpi_communicator
 
-if not args.serial:
-    import mpi4py
+
+def sat_check(*,
+inputdir : Path,
+outputdir: Path,
+mesh: str,
+varnames : List[str],
+QI : float,
+Kd_min: float = float(0.021),
+force : bool = False,
+serial : bool = False,
+):
+    if not serial:
+        import mpi4py
 
 
-comm = get_mpi_communicator()
-rank  = comm.Get_rank()
-nranks =comm.size
+    comm = get_mpi_communicator()
+    rank  = comm.Get_rank()
+    nranks =comm.size
 
 
-ORIGDIR = addsep(args.origdir)
-CHECKDIR = addsep(args.checkdir)
-THRESHOLD = float(args.QI)
-Kd_min = float(args.Kd_min)
+    ORIGDIR = inputdir
+    CHECKDIR = outputdir
+    THRESHOLD = QI
 
-maskSat = getattr(masks,args.mesh)
+    maskSat = getattr(masks,mesh)
 
-Timestart="19501231"
-Time__end="20500101"
-TI = TimeInterval(Timestart,Time__end,"%Y%m%d")
-TL_orig = TimeList.fromfilenames(TI, ORIGDIR ,"*.nc",prefix='',dateformat='%Y%m%d')
+    Timestart="19501231"
+    Time__end="20500101"
+    TI = TimeInterval(Timestart,Time__end,"%Y%m%d")
+    TL_orig = TimeList.fromfilenames(TI, ORIGDIR ,"*.nc",prefix='',dateformat='%Y%m%d')
 
-for filename in TL_orig.filelist[rank::nranks]:
+    for filename in TL_orig.filelist[rank::nranks]:
 
-    outfile = CHECKDIR + os.path.basename(filename)
-    for varname in args.varnames:
+        outfile = CHECKDIR / filename.name
+        for varname in varnames:
 
-        writing_mode = Sat.writing_mode(outfile)
+            writing_mode = Sat.writing_mode(outfile)
 
-        condition_to_write = not Sat.exist_valid_variable(varname,outfile)
-        if args.force: condition_to_write=True
-        if not condition_to_write: continue
-
-
-        if varname in ["DIATO","NANO","PICO","DINO"]:
-            QI = Sat.readfromfile(filename, "QI_CHL")
-        else:
-            QI = Sat.readfromfile(filename, "QI_" + varname)
-        VALUES = Sat.readfromfile(filename,varname)
-
-        if varname == 'KD490':
-            # VALUES[(VALUES<Kd_min) & (VALUES>0)] = Kd_min
-            VALUES[(VALUES<Kd_min) & (VALUES>0)] = Sat.fillValue   # Filter out values below Kd490_min threshold
-
-        bad = np.abs(QI) > THRESHOLD # 2.0
-        VALUES[bad] = Sat.fillValue
-        print(outfile, varname, flush=True)
-        Sat.dumpGenericfile(outfile, VALUES, varname, mesh=maskSat,mode=writing_mode)
+            condition_to_write = not Sat.exist_valid_variable(varname,outfile)
+            if args.force: condition_to_write=True
+            if not condition_to_write: continue
 
 
+            if varname in ["DIATO","NANO","PICO","DINO"]:
+                QI = Sat.readfromfile(filename, "QI_CHL")
+            else:
+                QI = Sat.readfromfile(filename, "QI_" + varname)
+            VALUES = Sat.readfromfile(filename,varname)
+
+            if varname == 'KD490':
+                # VALUES[(VALUES<Kd_min) & (VALUES>0)] = Kd_min
+                VALUES[(VALUES<Kd_min) & (VALUES>0)] = Sat.fillValue   # Filter out values below Kd490_min threshold
+
+            bad = np.abs(QI) > THRESHOLD # 2.0
+            VALUES[bad] = Sat.fillValue
+            print(outfile, varname, flush=True)
+            Sat.dumpGenericfile(outfile, VALUES, varname, mesh=maskSat,mode=writing_mode)
+    return 0
+
+if __name__ == "__main__":
+    args = argument()
+    exit(
+        sat_check(
+            inputdir=args.origdir,
+            outputdir=args.checkdir,
+            mesh=args.mesh,
+            varnames=args.varnames,
+            QI = float(args.QI),
+            Kd_min = float(args.Kd_min),
+            force = args.force,
+            serial=args.serial,
+        )
+    )
 

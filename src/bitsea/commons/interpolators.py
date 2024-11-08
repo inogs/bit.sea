ba9c2d647492a63a83fc4b46d68980ccf3b00e1d
Stefano Piani <spiani@ogs.it>
2024-11-08 12:40:44 +0100

    Run precommit over interpolators.py

--- src/bitsea/commons/interpolators.py
+++ src/bitsea/commons/interpolators.py
@@ -1,76 +1,91 @@
-from scipy.interpolate import griddata
 import numpy as np
 from bitsea.commons.utils import data_for_linear_interp
 from scipy import interpolate
+from scipy.interpolate import griddata
 from scipy.interpolate import RegularGridInterpolator
 
-def shift(M2d,pos, verso):
-    out = np.ones_like(M2d)*np.nan
 
-    if (verso=='u'): out[:-pos,:] = M2d [pos:,:]
-    if (verso=='d'): out[ pos:,:] = M2d[:-pos,:]
-    if (verso=='l'): out[:,:-pos] = M2d [:,pos:]
-    if (verso=='r'): out[:,pos:]  = M2d[:,:-pos]
+def shift(M2d, pos, verso):
+    out = np.ones_like(M2d) * np.nan
 
-    if (verso=="ul") : out[:-pos,:-pos] =  M2d [pos:,pos:]
-    if (verso=="ur") : out[:-pos,pos: ] =  M2d [pos:,:-pos]
-    if (verso=="dl") : out[ pos:,pos:]  =  M2d [:-pos,pos:]
-    if (verso=="dr") : out[pos:,pos: ]  = M2d [:-pos,:-pos]
+    if verso == "u":
+        out[:-pos, :] = M2d[pos:, :]
+    if verso == "d":
+        out[pos:, :] = M2d[:-pos, :]
+    if verso == "l":
+        out[:, :-pos] = M2d[:, pos:]
+    if verso == "r":
+        out[:, pos:] = M2d[:, :-pos]
+
+    if verso == "ul":
+        out[:-pos, :-pos] = M2d[pos:, pos:]
+    if verso == "ur":
+        out[:-pos, pos:] = M2d[pos:, :-pos]
+    if verso == "dl":
+        out[pos:, pos:] = M2d[:-pos, pos:]
+    if verso == "dr":
+        out[pos:, pos:] = M2d[:-pos, :-pos]
     return out
 
 
-
-def SeaOverLand(M2d,n):
-    jpj,jpi = M2d.shape
-    a = np.zeros((8*n, jpj,jpi),np.float32)
+def SeaOverLand(M2d, n):
+    jpj, jpi = M2d.shape
+    a = np.zeros((8 * n, jpj, jpi), np.float32)
     for i in range(n):
-        a[i,:,:] = shift(M2d,i+1,'u')
+        a[i, :, :] = shift(M2d, i + 1, "u")
     for i in range(n):
-        a[i+n,:,:] = shift(M2d,i+1,'d')
+        a[i + n, :, :] = shift(M2d, i + 1, "d")
     for i in range(n):
-        a[i+2*n,:,:] = shift(M2d,i+1,'r')
+        a[i + 2 * n, :, :] = shift(M2d, i + 1, "r")
     for i in range(n):
-        a[i+3*n,:,:] = shift(M2d,i+1,'l')
+        a[i + 3 * n, :, :] = shift(M2d, i + 1, "l")
     for i in range(n):
-        a[i+4*n,:,:] = shift(M2d,i+1,'ur')
+        a[i + 4 * n, :, :] = shift(M2d, i + 1, "ur")
     for i in range(n):
-        a[i+5*n,:,:] = shift(M2d,i+1,'dl')
+        a[i + 5 * n, :, :] = shift(M2d, i + 1, "dl")
     for i in range(n):
-        a[i+6*n,:,:] = shift(M2d,i+1,'ul')
+        a[i + 6 * n, :, :] = shift(M2d, i + 1, "ul")
     for i in range(n):
-        a[i+7*n,:,:] = shift(M2d,i+1,'dr')
-    result = np.nanmean (a,axis=0)
-    ii=~np.isnan(M2d)
+        a[i + 7 * n, :, :] = shift(M2d, i + 1, "dr")
+    result = np.nanmean(a, axis=0)
+    ii = ~np.isnan(M2d)
     result[ii] = M2d[ii]
     return result
 
+
 def surf_interp_2d(Mask1, Mask2, Map2d):
-    '''
+    """
     Interpolates a 2d numpy array over a new regular mesh
     Arguments:
     * Mask1, Mask2 * mask objects, defined
-    * Map2d        * 2d numpy array defined on Mask1 
-    
+    * Map2d        * 2d numpy array defined on Mask1
+
     Returns:
-    * NearestMap * 2d numpy array defined on Mask2 
+    * NearestMap * 2d numpy array defined on Mask2
     Works correctly only in surface because it uses griddata
-    
-    '''
+
+    """
     tmask1 = Mask1.mask_at_level(0)
     tmask2 = Mask2.mask_at_level(0)
     nP = tmask1.sum()
-    points = np.zeros((nP,2), np.float32)
-     
-    points[:,0] = Mask1.xlevels[tmask1]
-    points[:,1] = Mask1.ylevels[tmask1]
+    points = np.zeros((nP, 2), np.float32)
+
+    points[:, 0] = Mask1.xlevels[tmask1]
+    points[:, 1] = Mask1.ylevels[tmask1]
     values = Map2d[tmask1]
-    MAP2d_nearest =griddata( points, values, (Mask2.xlevels, Mask2.ylevels), 'nearest', fill_value=np.nan)
-    MAP2d_nearest[~tmask2]=np.nan
+    MAP2d_nearest = griddata(
+        points,
+        values,
+        (Mask2.xlevels, Mask2.ylevels),
+        "nearest",
+        fill_value=np.nan,
+    )
+    MAP2d_nearest[~tmask2] = np.nan
     return MAP2d_nearest
 
 
-def interp_same_resolution(Mask1, Mask2, M3d):
-    '''
+def interp_same_resolution(input_data_mask, output_mask, M3d):
+    """
     Performs nearest interpolation for masks very similar,
     differing only for sea/lands points.
     Arguments:
@@ -78,30 +93,34 @@ def interp_same_resolution(Mask1, Mask2, M3d):
     * M3d          * 3d numpy array, consistent with Mask1
 
     Returns:
-     * OUT * 3d numpy array, consistent with Mask2
-    '''
-    ii=Mask2.mask & (~Mask1.mask)
-    K,J,I = np.nonzero(ii)
-    OUT = M3d
-    for k in np.unique(K):
-        M2d=M3d[k,:,:]
-        goods  = Mask1.mask[k,:,:]
+     np.ndarray: 3d numpy array, consistent with Mask2
+    """
+    only_output_mask = output_mask.mask & (~input_data_mask.mask)
+
+    # We check on which levels we must perform interpolation
+    interpolation_depth_levels = np.unique(np.nonzero(only_output_mask)[0])
+
+    output = M3d.copy()
+    for k in interpolation_depth_levels:
+        M2d = M3d[k, :, :]
+        goods = input_data_mask.mask[k, :, :]
         Jgoods, Igoods = np.nonzero(goods)
         nP = len(Jgoods)
-        points = np.zeros((nP,2),dtype=np.float32)
-        points[:,0] = Jgoods
-        points[:,1] = Igoods
+        points = np.zeros((nP, 2), dtype=np.float32)
+        points[:, 0] = Jgoods
+        points[:, 1] = Igoods
         values = M2d[goods]
 
-        bool_mask2lands_on_k = ii[k,:,:]
-        J,I = np.nonzero(bool_mask2lands_on_k)
-        nP =bool_mask2lands_on_k.sum()
-        xi = np.zeros((nP,2),dtype=np.float32)
-        xi[:,0] = J
-        xi[:,1] = I
+        bool_mask2lands_on_k = only_output_mask[k, :, :]
+        jj, ii = np.nonzero(bool_mask2lands_on_k)
+        nP = bool_mask2lands_on_k.sum()
+        xi = np.zeros((nP, 2), dtype=np.float32)
+        xi[:, 0] = jj
+        xi[:, 1] = ii
         V = griddata(points, values, xi, "nearest")
-        OUT[k,bool_mask2lands_on_k] = V
-    return OUT
+        output[k, bool_mask2lands_on_k] = V
+    return output
+
 
 def space_interpolator_griddata(mask2, mask1, M3d):
     """Interpolates a 3d matrix using horizontal slices"""
@@ -147,6 +166,7 @@ def space_interpolator_griddata(mask2, mask1, M3d):
 
     return M3d_out
 
+
 def vertical_plane_interpolator(mask2, mask1, M2d, side):
     """Interpolates a 2d vertical matrix by using horizontal profiles
     M2d size is [jpk, LonSize, or LatSize]
@@ -190,56 +210,61 @@ def vertical_plane_interpolator(mask2, mask1, M2d, side):
 
         # Non lo calcola e si tiene il precedente, che ha sicuramente gia' calcolato
     return M
-def regular(Mask1,Mask2,VAR, method="linear",rescale=False):
-    '''
+
+
+def regular(Mask1, Mask2, VAR, method="linear", rescale=False):
+    """
     rescale works on a box cube
     With rescale = False and method = 'nearest'
     horizontal distances (deg) are much lesser than vertical ones (meters)
     So the nearest points are at the same level.
-    '''
+    """
     if rescale:
-        jpk,jpj,jpi = Mask1.shape
+        jpk, jpj, jpi = Mask1.shape
         x1 = np.array(range(jpi))
         y1 = np.array(range(jpj))
         z1 = np.array(range(jpk))
-        x2 = np.interp(Mask2.lon,Mask1.lon,x1)
-        y2 = np.interp(Mask2.lat,Mask1.lat,y1)
-        z2 = np.interp(Mask2.zlevels,Mask1.zlevels,z1)
+        x2 = np.interp(Mask2.lon, Mask1.lon, x1)
+        y2 = np.interp(Mask2.lat, Mask1.lat, y1)
+        z2 = np.interp(Mask2.zlevels, Mask1.zlevels, z1)
     else:
-        x1,y1,z1 = Mask1.lon,Mask1.lat,Mask1.zlevels
-        x2,y2,z2 = Mask2.lon,Mask2.lat,Mask2.zlevels
+        x1, y1, z1 = Mask1.lon, Mask1.lat, Mask1.zlevels
+        x2, y2, z2 = Mask2.lon, Mask2.lat, Mask2.zlevels
 
-    def bound_check(arr1,arr2):
-        ''' Extent arr1 to arr2 limits
+    def bound_check(arr1, arr2):
+        """Extent arr1 to arr2 limits
         to be compliant with RegularGridInterpolator
-        '''
-        if arr1[0]> arr2[0]  : arr1[0] = arr2[0]
-        if arr1[-1]<arr2[-1] : arr1[-1] = arr2[-1]
+        """
+        if arr1[0] > arr2[0]:
+            arr1[0] = arr2[0]
+        if arr1[-1] < arr2[-1]:
+            arr1[-1] = arr2[-1]
+
     bound_check(z1, z2)
     bound_check(y1, y2)
     bound_check(x1, x2)
 
-
-    Values=VAR.copy()
+    Values = VAR.copy()
     Values[~Mask1.mask] = np.nan
-    f = RegularGridInterpolator((z1,y1,x1),Values, method=method)
-    X2,Y2,Z2=np.meshgrid(x2,y2,z2, indexing='ij')
+    f = RegularGridInterpolator((z1, y1, x1), Values, method=method)
+    X2, Y2, Z2 = np.meshgrid(x2, y2, z2, indexing="ij")
     nPoints = X2.size
-    points = np.zeros((nPoints,3),float)
-    points[:,2] = X2.ravel()
-    points[:,1] = Y2.ravel()
-    points[:,0] = Z2.ravel()
+    points = np.zeros((nPoints, 3), float)
+    points[:, 2] = X2.ravel()
+    points[:, 1] = Y2.ravel()
+    points[:, 0] = Z2.ravel()
     F2 = f(points)
-    jpk,jpj,jpi = Mask2.shape
-    M2 = F2.reshape((jpi,jpj,jpk)).T
+    jpk, jpj, jpi = Mask2.shape
+    M2 = F2.reshape((jpi, jpj, jpk)).T
     return M2
 
-def compose_methods(Mask1,Mask2,VAR):
-    L = regular(Mask1, Mask2, VAR, method='linear')
-    N = regular(Mask1, Mask2, VAR, method='nearest')
+
+def compose_methods(Mask1, Mask2, VAR):
+    L = regular(Mask1, Mask2, VAR, method="linear")
+    N = regular(Mask1, Mask2, VAR, method="nearest")
     G = space_interpolator_griddata(Mask2, Mask1, VAR)
     OUT = L.copy()
-    toFill=np.isnan(L) & (~np.isnan(N))
+    toFill = np.isnan(L) & (~np.isnan(N))
     OUT[toFill] = N[toFill]
     toFill = np.isnan(OUT) & Mask2.mask
     OUT[toFill] = G[toFill]
@@ -247,25 +272,23 @@ def compose_methods(Mask1,Mask2,VAR):
 
 
 if __name__ == "__main__":
-
     from bitsea.commons import netcdf4
     from bitsea.commons.mask import Mask
     from bitsea.commons.dataextractor import DataExtractor
-    
 
-    Mask1=Mask("/Users/gbolzon/Downloads/test_interp/mask_006_014_reduced.nc")
-    Mask2=Mask('/Users/gbolzon/Downloads/test_interp/mask.nc')
+    Mask1 = Mask("/Users/gbolzon/Downloads/test_interp/mask_006_014_reduced.nc")
+    Mask2 = Mask("/Users/gbolzon/Downloads/test_interp/mask.nc")
 
-    filename="/Users/gbolzon/Downloads/test_interp/ave.20241027-12:00:00.N1p.nc"
-    VAR = DataExtractor(Mask1,filename,"N1p").values
-    
+    filename = (
+        "/Users/gbolzon/Downloads/test_interp/ave.20241027-12:00:00.N1p.nc"
+    )
+    VAR = DataExtractor(Mask1, filename, "N1p").values
+
     A = space_interpolator_griddata(Mask2, Mask1, VAR)
-    B = regular(Mask1, Mask2, VAR, method='nearest')
+    B = regular(Mask1, Mask2, VAR, method="nearest")
     C = compose_methods(Mask1, Mask2, VAR)
-    A[~Mask2.mask] = 1.e20
+    A[~Mask2.mask] = 1.0e20
 
-
-    netcdf4.write_3d_file(A, 'N1p', 'griddata.nc', Mask2, fillValue=1e+20)
-    netcdf4.write_3d_file(B, 'N1p', 'regular.nc' , Mask2, fillValue=1e+20)
-    netcdf4.write_3d_file(C, 'N1p', 'composed.nc', Mask2, fillValue=1e+20)
-
+    netcdf4.write_3d_file(A, "N1p", "griddata.nc", Mask2, fillValue=1e20)
+    netcdf4.write_3d_file(B, "N1p", "regular.nc", Mask2, fillValue=1e20)
+    netcdf4.write_3d_file(C, "N1p", "composed.nc", Mask2, fillValue=1e20)

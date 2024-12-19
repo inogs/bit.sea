2c2f78f64fc92740c60190dfda29cb859c7fe7b4
Giorgio Bolzon <gbolzon@ogs.it>
2024-12-19 17:54:42 +0100

    plot_timeseries_RMS_CORR.py reads NetCDF validation files

--- src/bitsea/validation/deliverables/plot_timeseries_RMS_CORR.py
+++ src/bitsea/validation/deliverables/plot_timeseries_RMS_CORR.py
@@ -4,6 +4,8 @@
 # Figure IV.3 and table IV.1
 
 import argparse
+from bitsea.utilities.argparse_types import date_from_str
+from bitsea.utilities.argparse_types import existing_dir_path
 
 def argument():
     parser = argparse.ArgumentParser(description = '''
@@ -15,22 +17,35 @@ def argument():
     )
 
     parser.add_argument(   '--outdir', '-o',
-                            type = str,
+                            type = existing_dir_path,
                             required =True,
                             default = "./",
                             help = ''' Output image dir'''
                             )
 
-    parser.add_argument(   '--inputfile', '-i',
-                            type = str,
+    parser.add_argument(   '--inputdir', '-i',
+                            type = existing_dir_path,
                             required = True,
-                            default = 'export_data_ScMYValidation_plan.pkl',
-                            help = 'Input pickle file')
+                            help = 'Dir containing NetCDF validation files')
+    parser.add_argument(   '--datestart', '-s',
+                                type = date_from_str,
+                                required =True,
+                                help = ''' Date start for time interval to consider for validation, format %Y%m%d'''
+                                )
+    parser.add_argument(   '--dateend', '-e',
+                                type = date_from_str,
+                                required =True,
+                                help = ''' Date end for time interval to consider for validation,format %Y%m%d'''
+                                )
     parser.add_argument(   '--var', '-v',
                                 type = str,
                                 required = True,
                                 choices = ['P_l','kd490','P1l','P2l','P3l','P4l','RRS412','RRS443','RRS490','RRS510','RRS555','RRS670' ],
                                 help = ''' model var name'''
+                                )
+    parser.add_argument(   '--coastness', '-c',
+                                type = str,
+                                choices=["coast","open_sea","everywhere"]
                                 )
     parser.add_argument(   '--zone', '-z',
                                 type = str,
@@ -45,26 +60,23 @@ def argument():
 
 args = argument()
 
-import pickle
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as pl
 import matplotlib.dates as mdates
-import sys
 import numpy as np
-from bitsea.commons.utils import addsep
 from bitsea.instruments.var_conversions import SAT_VARS
+from bitsea.commons.Timelist import TimeList, TimeInterval
+from bitsea.validation.deliverables import netcdf_validation_file
 
-OUTDIR=addsep(args.outdir)
-fid = open(args.inputfile,'rb')
-LIST = pickle.load(fid)
-fid.close()
+
 
 if (args.zone == "Med"):
-   from bitsea.basins import V2 as OGS
+    from bitsea.basins import V2 as OGS
 if (args.zone == "rivers"):
-   print ("rivers")
-   from bitsea.basins import RiverBoxes as OGS
+    print ("rivers")
+    from bitsea.basins import RiverBoxes as OGS
 
 model_label=' MODEL'
 
@@ -81,33 +93,52 @@ else:
     vmin=-0.3
     vmax=0.3
     if (args.zone == "rivers"):
-       vmin=-1.0
-       vmax=1.0
+        vmin=-1.0
+        vmax=1.0
     
 var_label = SAT_VARS[args.var] + " " + units
 
+TI = TimeInterval.fromdatetimes(args.datestart, args.dateend)
+TL = TimeList.fromfilenames(TI, args.inputdir, "valid.*", filtervar=args.var, prefix="valid.", dateformat="%Y%m%d")
 
-TIMES                          = LIST[0]
-BGC_CLASS4_CHL_RMS_SURF_BASIN  = LIST[1]
-BGC_CLASS4_CHL_BIAS_SURF_BASIN = LIST[2]
-BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG = LIST[5]
-BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG= LIST[6]
+TIMES=TL.Timelist
+nFrames = TL.nTimes
+First = netcdf_validation_file.read(TL.filelist[0])
+nSub, nCoast=First.VALID_POINTS.shape
+iCoast=First.coastlist.rsplit(",").index(args.coastness)
+MODEL_MEAN = np.zeros((nFrames,nSub), np.float32)
+SAT___MEAN = np.zeros((nFrames,nSub), np.float32)
+MODEL_STD  = np.zeros((nFrames,nSub), np.float32)
+SAT___STD  = np.zeros((nFrames,nSub), np.float32)
+BGC_CLASS4_CHL_RMS_SURF_BASIN   = np.zeros((nFrames,nSub), np.float32)
+BGC_CLASS4_CHL_BIAS_SURF_BASIN  = np.zeros((nFrames,nSub), np.float32)
+BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG = np.zeros((nFrames,nSub), np.float32)
+BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG= np.zeros((nFrames,nSub), np.float32)
+BGC_CLASS4_CHL_CORR_SURF_BASIN = np.zeros((nFrames,nSub), np.float32)
 
-MODEL_MEAN=LIST[3]
-SAT___MEAN=LIST[4]
-MODEL_STD = LIST[7]
-SAT___STD = LIST[8]
-BGC_CLASS4_CHL_CORR_SURF_BASIN= LIST[9]
-NUMB = LIST[10]
+for iFrame, filename in enumerate(TL.filelist):
+    H=netcdf_validation_file.read(filename)
+    MODEL_MEAN[iFrame,:] = H.MODEL_MEAN[:,iCoast]
+    SAT___MEAN[iFrame,:] = H.SAT___MEAN[:,iCoast]
+    MODEL_STD[iFrame,:] = H.MODEL__STD[:,iCoast]
+    SAT___STD[iFrame,:] = H.SAT____STD[:,iCoast]
+    BGC_CLASS4_CHL_RMS_SURF_BASIN[iFrame,:] = H.BGC_CLASS4_CHL_RMS_SURF_BASIN[:,iCoast]
+    BGC_CLASS4_CHL_BIAS_SURF_BASIN[iFrame,:] = H.BGC_CLASS4_CHL_BIAS_SURF_BASIN[:,iCoast]
+    BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG[iFrame,:] = H.BGC_CLASS4_CHL_RMS_SURF_BASIN_LOG[:,iCoast]
+    BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG[iFrame,:] = H.BGC_CLASS4_CHL_BIAS_SURF_BASIN_LOG[:,iCoast]
+    BGC_CLASS4_CHL_CORR_SURF_BASIN[iFrame,:]  = H.BGC_CLASS4_CHL_CORR_SURF_BASIN[:,iCoast]
+
+
 
 nSUB = len(OGS.P.basin_list)
 
 for isub,sub in enumerate(OGS.P):
     if (sub.name == 'atl') : continue
-    print (sub.name)
+    outfilename=args.outdir / f"{args.var}-RMS-BIAS_{sub.name}.png"
+    print (outfilename)
     if ((args.var =="P_l") & (sub.name=="Po")):
-           vmin=-4.0
-           vmax=4.0
+        vmin=-4.0
+        vmax=4.0
 
     fig, ax = pl.subplots()
     fig.set_size_inches(12,4)
@@ -128,9 +159,6 @@ for isub,sub in enumerate(OGS.P):
     pl.setp(xlabels, rotation=30,fontsize=9)
     ylabels = ax.get_yticklabels()
     pl.setp(ylabels, fontsize=10)
-#    #ax.tick_params(direction='left', pad=2)
-    #fig.show()
-    outfilename="%s%s-RMS-BIAS_%s.png"  %(OUTDIR, args.var, sub.name) #  args.outdir+"/"+'chl-RMS-BIAS_' + sub.name + ".png"
     pl.tight_layout()
     pl.tight_layout()
     pl.savefig(outfilename)
@@ -141,7 +169,6 @@ from bitsea.commons.season import season
 S=season()
 S.setseasons(["0101", "0501", "0601", "1001"], ["winter","spring","summer","fall"])
 from bitsea.commons import timerequestors
-from bitsea.commons.Timelist import TimeInterval, TimeList
 TL=TimeList(TIMES)
 from bitsea.commons.utils import writetable
 
@@ -175,8 +202,6 @@ STD_MOD_sum = np.nanmean(MODEL_STD[ii,:],axis=0)
 STD_REF_sum = np.nanmean(SAT___STD[ii,:],axis=0)
 CORR_sum    = np.nanmean(BGC_CLASS4_CHL_CORR_SURF_BASIN[ii,:],axis=0)
 
-#mat = np.zeros((nSUB,8),np.float32)
-#mat = np.zeros((nSUB,14),np.float32)
 mat = np.zeros((nSUB,18),np.float32)
 
 mat[:,0] = RMS__win
@@ -199,11 +224,9 @@ mat[:,15] = MEAN_REF_win
 mat[:,16] = MEAN_MOD_sum
 mat[:,17] = MEAN_REF_sum
 
-outfiletable = OUTDIR + "table4.1_" + args.var + ".txt"
+outfiletable = args.outdir / ("table4.1_" + args.var + ".txt")
 print (outfiletable)
 rows_names=[sub.name for sub in OGS.P.basin_list]
-#column_names=['RMSwin','RMSsum','BIASwin','BIASsum', 'RMSLwin','RMSLsum','BIASLwin','BIASLsum']
-#column_names=['RMSwin','RMSsum','BIASwin','BIASsum', 'RMSLwin','RMSLsum','BIASLwin','BIASLsum','STD_MODwin','STD_SATwin','STD_MODsum','STD_SATsum','CORRwin','CORRsum']
 column_names=['RMSwin','RMSsum','BIASwin','BIASsum', 'RMSLwin','RMSLsum','BIASLwin','BIASLsum','STD_MODwin','STD_SATwin','STD_MODsum','STD_SATsum','CORRwin','CORRsum','MEAN_MODwin','MEAN_SATwin','MEAN_MODsum','MEAN_SATsum']
 writetable(outfiletable, mat, rows_names, column_names, fmt='%5.3f\t')
 

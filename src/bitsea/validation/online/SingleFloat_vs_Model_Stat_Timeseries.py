ea18342117331498d9f116e219f92c7ecdc21ec4
Giorgio Bolzon <gbolzon@ogs.it>
2025-01-29 10:16:23 +0100

    Improving argparse and Path stuff

--- src/bitsea/validation/online/SingleFloat_vs_Model_Stat_Timeseries.py
+++ src/bitsea/validation/online/SingleFloat_vs_Model_Stat_Timeseries.py
@@ -1,4 +1,6 @@
 import argparse
+from bitsea.utilities.argparse_types import existing_dir_path, existing_file_path
+from bitsea.utilities.argparse_types import date_from_str
 def argument():
     parser = argparse.ArgumentParser(description = '''
     Needs a profiler.py, already executed.
@@ -10,24 +12,20 @@ def argument():
 
 
     parser.add_argument(   '--maskfile', '-m',
-                                type = str,
-                                default = "/marconi_scratch/userexternal/lfeudale/Maskfiles/meshmask.nc",
-                                required = False,
-                                help = ''' Path of maskfile''')
+                                type = existing_file_path,
+                                required = True)
     
     parser.add_argument(   '--basedir', '-b',
-                                type = str,
-                                default = "/marconi_scratch/userexternal/lfeudale/Maskfiles/meshmask.nc",
+                                type = existing_dir_path,
                                 required = True,
-                                help = ''' Path of maskfile''')    
+                                help = """ PROFILATORE dir, already generated""")    
 
     parser.add_argument(   '--outdir', '-o',
-                                type = str,
-                                default = None,
+                                type = existing_dir_path,
                                 required = True,
                                 help = "")
     parser.add_argument(   '--date','-d',
-                                type = str,
+                                type = date_from_str,
                                 required = True,
                                 help = 'start date in yyyymmdd format')
 
@@ -41,11 +39,13 @@ from bitsea.commons.Timelist import TimeList, TimeInterval
 from bitsea.instruments import superfloat as bio_float
 from bitsea.instruments.matchup_manager import Matchup_Manager
 from bitsea.instruments.var_conversions import FLOATVARS
-from bitsea.commons.utils import addsep
 from bitsea.commons.layer import Layer
 from bitsea.basins.region import Rectangle
-from metrics import *
-from SingleFloat_vs_Model_Stat_Timeseries_IOnc import dumpfile
+from bitsea.validation.deliverables.metrics2 import find_DCM, find_WBL,find_NITRICL
+from bitsea.validation.deliverables.metrics2 import find_OMZ, find_maxO2
+from bitsea.validation.deliverables.metrics2 import find_NITRICL_dz_max
+from bitsea.validation.online.metrics import find_WLB
+from bitsea.validation.online.SingleFloat_vs_Model_Stat_Timeseries_IOnc import dumpfile
 from bitsea.basins import V2 as OGS
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -54,20 +54,20 @@ from bitsea.Float.oxygen_saturation import oxy_sat
 
 
 
-BASEDIR = addsep(args.basedir)
-OUTDIR = addsep(args.outdir)
-Check_obj_nitrate = check.check(OUTDIR + "/nitrate_check/")
-Check_obj_chl     = check.check(OUTDIR + "chla_check/")
+BASEDIR = args.basedir
+OUTDIR = args.outdir
+Check_obj_nitrate = check.check(OUTDIR / "nitrate_check/")
+Check_obj_chl     = check.check(OUTDIR / "chla_check/")
 
 
 TheMask=Mask(args.maskfile, loadtmask=False)
 
 Graphic_DeltaT = relativedelta(months=18)
-datestart = datetime.strptime(args.date,'%Y%m%d') -Graphic_DeltaT
+datestart = args.date -Graphic_DeltaT
 timestart = datestart.strftime("%Y%m%d")
 
-TL = TimeList.fromfilenames(None, BASEDIR + "PROFILES/","ave*.nc")
-TI = TimeInterval(timestart, args.date,'%Y%m%d')
+TL = TimeList.fromfilenames(None, BASEDIR / "PROFILES/","ave*.nc")
+TI = TimeInterval.fromdatetimes(datestart,args.date)
 ALL_PROFILES = bio_float.FloatSelector(None, TI, Rectangle(-6,36,30,46))
 
 
@@ -99,8 +99,8 @@ for ivar, var_mod in enumerate(VARLIST):
     Profilelist = bio_float.FloatSelector(var, TI, Rectangle(-6,36,30,46))
     wmo_list=bio_float.get_wmo_list(Profilelist)
     for iwmo, wmo in enumerate(wmo_list):
-        OUTFILE = "%s%s_%s.nc" %(OUTDIR, var_mod, wmo )
-        print (OUTFILE)
+        OUTFILE = OUTDIR / f"{var_mod}_{wmo}.nc"
+        print (OUTFILE, flush=True)
         list_float_track=bio_float.filter_by_wmo(Profilelist,wmo)
         nTime = len(list_float_track)
         A_float = np.zeros(( nTime, nStat), np.float32 ) * np.nan

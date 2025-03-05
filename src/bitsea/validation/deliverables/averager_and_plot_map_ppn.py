fffed5cb5c2cee7692a0e682caff2f89b30ec5c7
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2025-03-05 15:11:33 +0100

    Adding Cafe and Octac as references; adding RMSD calculation on 16 subbasins; hardcoding colorscale

--- src/bitsea/validation/deliverables/averager_and_plot_map_ppn.py
+++ src/bitsea/validation/deliverables/averager_and_plot_map_ppn.py
@@ -3,6 +3,7 @@ from numpy import dtype
 def argument():
     parser = argparse.ArgumentParser(description = '''
     Generates png maps, based on matplotlib.imshow(), of time averaged fields
+    and bias and RMSD table with respect the datasets: CAFE and OCTAC
     
     Brief description of the algorithm:
     INPUTS                                               OPERATOR              OUTPUT
@@ -36,7 +37,7 @@ def argument():
                                 type = str,
                                 required = True,
                                 default = '',
-                                choices = ['P_l','P_i','N1p', 'N3n', 'O2o', 'pCO2','PH','pH','ppn','P_c','Ac','DIC','Z_c'] )
+                                choices = ['P_l','P_i','N1p', 'N3n', 'O2o', 'pCO2','PH','pH','ppn','P_c','Ac','DIC'] )
     parser.add_argument(   '--plotlistfile', '-l',
                                 type = str,
                                 required = True,
@@ -55,7 +56,7 @@ def argument():
     parser.add_argument(   '--starttime','-s',
                                 type = str,
                                 required = True,
-                                help = 'start date in yyyymmdd format')
+                                help = 'start date in yyyymmdd format; just year 2021, 2022 or 2023 can be selected)')
     parser.add_argument(   '--endtime','-e',
                                 type = str,
                                 required = True,
@@ -65,13 +66,18 @@ def argument():
 args = argument()
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pl
 
 from bitsea.commons.time_interval import TimeInterval
 from bitsea.commons.Timelist import TimeList
 from bitsea.commons.mask import Mask
 from bitsea.commons.layer import Layer
 from bitsea.layer_integral.mapbuilder import MapBuilder, Plot
-from bitsea.layer_integral.mapplot import mapplot,pl
+#from bitsea.layer_integral.mapplot import mapplot,pl
+# X ppn:
+# from mapplot_ppn import mapplot,pl
 from bitsea.commons.dataextractor import DataExtractor
 from bitsea.commons.time_averagers import TimeAverager3D
 from bitsea.layer_integral import coastline
@@ -109,7 +115,6 @@ TheMask=Mask(args.maskfile)
 
 CONVERSION_DICT={
          'ppn' : 365./1000,
-         'Z_c' : 1./1000,
          'O2o' : 1,
          'N1p' : 1,
          'N3n' : 1,
@@ -117,9 +122,9 @@ CONVERSION_DICT={
          'pH'  : 1, 
          'pCO2': 1,
          'P_l' : 1,
-         'P_c' : 1., # 1./1000, #divided by 1000 in the integral
+         'P_c' : 1,
          'P_i' : 1,
-	     'Ac'  : 1,
+         'Ac'  : 1,
          'DIC' : 1
          }
 
@@ -132,6 +137,8 @@ if TL.inputFrequency is None:
     TL.inputFrequency='monthly'
     print ("inputFrequency forced to monthly because of selection of single time")
 
+#Set the year of interest:
+yy = TI.start_time.year
 req = requestors.Generic_req(TI)
 indexes,weights = TL.select(req)
 
@@ -145,16 +152,13 @@ for k in indexes:
     filelist.append(filename)
 # ----------------------------------------------------------
 print ("time averaging ...")
-if (var == "CO2airflux"):
-   M3d     = TimeAverager2D(filelist, weights, var, TheMask)
-else:
-   M3d     = TimeAverager3D(filelist, weights, var, TheMask)
+M3d     = TimeAverager3D(filelist, weights, var, TheMask)
 print ("... done.")
 for il, layer in enumerate(PLOT.layerlist):
     z_mask = PLOT.depthfilters[il]
     z_mask_string = "-%04gm" %z_mask
     if  args.optype=='integral':
-        outfile    = OUTPUTDIR + "Map_" + var + "_" + req_label + "_Int" + layer.longname() + z_mask_string  + ".png"
+        outfile    = OUTPUTDIR + "Map_" + var + "_" + req_label + "_Int" + layer.longname() + z_mask_string  + "_refScale.png"
     else:
         outfile    = OUTPUTDIR + "Map_" + var + "_" + req_label + "_Ave" + layer.longname() + z_mask_string  + ".png"
 
@@ -168,89 +172,88 @@ for il, layer in enumerate(PLOT.layerlist):
 
 
     mask=TheMask.mask_at_level(z_mask)
-#        clim = [M3d[TheMask.mask].min(), M3d[TheMask.mask].max()]
     clim=PLOT.climlist[il]
     integrated_masked=integrated*mask # taglio il costiero
     integrated_masked[integrated_masked==0]=np.nan # sostituisco gli 0 con i NAN
 
-    fig,ax     = mapplot({'clim':clim, 'data':integrated_masked, },fig=None,ax=None,mask=TheMask,coastline_lon=clon,coastline_lat=clat)
+# X ppn:
+    fig,ax = pl.subplots()
+    fig.set_size_inches(10.0, 10.0*16/42)
+    ax.set_position([0.08, 0.13, 0.78, 0.78])
+    levels = [0, 25, 50, 75, 100, 125, 150, 175, 200, 300, 400, 500]
+    # colors = ['navy','blue','royalblue','deepskyblue','aqua','lawngreen','greenyellow','gold','orange','red','maroon']
+    colors = ['midnightblue','indigo','blue','royalblue','deepskyblue','aqua','greenyellow','gold','orange','red','maroon']
+    # New corlors more similar to Lazzari et al. (2012)
+
+    CS=ax.contourf(TheMask.xlevels, TheMask.ylevels,integrated_masked,levels,colors=colors)
+    cbar=fig.colorbar(CS,ticks=levels)
     ax.set_xlim([-5,36])
+
     ax.set_ylim([30,46])
     ax.set_xlabel('Lon').set_fontsize(11)
     ax.set_ylabel('Lat').set_fontsize(12)
-#    ax.ticklabel_format(fontsize=10)
     ax.tick_params(axis='x', labelsize=10)
-#    ax.text(-4,44.5,PLOT.name() + ' [' + PLOT.units() + ']',horizontalalignment='left',verticalalignment='center',fontsize=14, color='black')
-    if (var=="Z_c"):
-       ax.text(-4,44.5, 'ZOOPL [' + PLOT.units() + ']',horizontalalignment='left',verticalalignment='center',fontsize=14, color='black')
+# CHANGE ACRONYM for NET PRIMARY PRODUCTION from "ppn" to "npp":
+    if (var=="ppn"):
+        ax.text(-4,44.5,'npp [' + PLOT.units() + ']',horizontalalignment='left',verticalalignment='center',fontsize=14, color='black')
+        title = "%s %s %s" % ('annual', 'npp', layer.__repr__())
     else:
-       ax.text(-4,44.5,var + ' [' + PLOT.units() + ']',horizontalalignment='left',verticalalignment='center',fontsize=14, color='black')
+        ax.text(-4,44.5,var + ' [' + PLOT.units() + ']',horizontalalignment='left',verticalalignment='center',fontsize=14, color='black')
+        title = "%s %s %s" % ('annual', var, layer.__repr__())
 
     ax.xaxis.set_ticks(np.arange(-2,36,6))
     ax.yaxis.set_ticks(np.arange(30,46,4))
-    #ax.text(-4,30.5,req_label,horizontalalignment='left',verticalalignment='center',fontsize=13, color='black')
+##########
+    #Draw coastline
+    ax.plot(clon,clat, color='#000000',linewidth=0.5)
+    ax.set_xlim([-6, 36])
+    ax.set_ylim([30, 46])
+#########
     ax.grid()
-    if (var=="Z_c"):
-       title = "%s %s %s" % ('annual', 'ZOOPL', layer.__repr__())
-    else:
-        title = "%s %s %s" % ('annual', var, layer.__repr__())
-#    if (var == "PH"): title = "%s %s %s" % ('annual', "pH$\mathrm{_T}$", layer.__repr__())
-#    title = "%s %s %s" % (MONTH_STRING[TI.start_time.month - 1], var, layer.__repr__())
     if (var == "pH"): title = "%s %s %s" % (MONTH_STRING[TI.start_time.month - 1], "pH$\mathrm{_T}$", layer.__repr__())
     if (var == "pCO2"): title = "%s %s %s" % (MONTH_STRING[TI.start_time.month - 1], "pCO2", layer.__repr__())
-    if (var=="P_c"): title = "%s %s %s" % ('annual', "phytoplankton biomass", layer.__repr__())
-    fig.suptitle(title)
-    fig.savefig(outfile)
+    pl.suptitle(title)
+    pl.savefig(outfile)
     pl.close(fig)
     if (var == "ppn"): 
-        ncfile = OUTPUTDIR + "Map_" + var + "_" + req_label + "_Int" + layer.longname() + z_mask_string  + ".nc"
-#    netcdf3.write_2d_file(integrated_masked,"ppn",ncfile,mask)
+        # READ OBS DATASETS:
+#        DIR_OBS="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/PPN/"
+        DIR_OBS="/g100_scratch/userexternal/lfeudale/NPP_OBS/datasets_cafe_octac/"
+        filename_CAFE="ppn_CAFE_mean_16basins_" + str(yy) + "_annual.txt"
+        filename_OCTAC="ppn_OCTAC_mean_16basins_" + str(yy) + "_annual.txt"
+#       RMSE = np.loadtxt(INDIR + '/' + var + '.rmse.txt', skiprows=1,usecols=[ii for ii in range(1,8)])
+
+        CAFE = np.loadtxt(DIR_OBS + filename_CAFE, skiprows=1,usecols=1)
+        OCTAC = np.loadtxt(DIR_OBS + filename_OCTAC , skiprows=1,usecols=1)
+
+# 
+        ncfile = OUTPUTDIR + "Map_" + var + "_" + req_label + "_Int" + layer.longname() + z_mask_string  + "_refScale.nc"
         netcdf3.write_2d_file(integrated_masked,"ppn",ncfile,TheMask)
-        
-    if (var == "ppn"or var == "Z_c" or var == "P_c"):
+
         from bitsea.basins import V2 as OGS
         from bitsea.commons.submask import SubMask
-        from bitsea.basins.basin import ComposedBasin
         from bitsea.commons.utils import writetable
-        tablefile = OUTPUTDIR + '/' + var + '_mean_basin.txt'
-        OGSred = ComposedBasin('OGSred',[OGS.alb, \
-             #       OGS.swm1, OGS.swm2, OGS.nwm, OGS.tyr, \
-                    OGS.swm, OGS.nwm, OGS.tyr, \
-                    OGS.adr, OGS.aeg, OGS.ion, OGS.lev , OGS.med], \
-                   # 'Gruped Subbasin for ppn analysis')
-                    'Gruped Subbasin for integral variable analysis')
-        SUBlist = OGSred.basin_list
-        nSub   = len(OGSred.basin_list)
+        tablefile = OUTPUTDIR + '/' + var + '_mean_basin_' + str(yy) + '.txt'
+        tablefile_PPN_EAN =  OUTPUTDIR + '/' + var + '_ean_' + str(yy) + '.txt'
+
+        SUBlist = OGS.P.basin_list
+        nSub   = len(SUBlist)
         rows_names=[sub.name for sub in SUBlist]
-        ppn_submean = np.zeros((nSub,2),np.float32)*np.nan # FIRST COL: mean; SECOND COL: std
-        for isub, sub in enumerate(OGSred):
+        ppn_submean = np.zeros((nSub,3),np.float32)*np.nan
+        ppn_ean = np.zeros((2,2),np.float32)*np.nan
+        for isub, sub in enumerate(OGS.P):
             S = SubMask(sub, maskobject=TheMask)
             mask2d=S.mask[0,:,:]
             ppn_submean[isub,0] = integrated_masked[mask2d].mean()
-            ppn_submean[isub,1] = integrated_masked[mask2d].std()
-
-#        writetable(tablefile,ppn_submean,rows_names,['mean'])
-        writetable(tablefile,ppn_submean,rows_names,['MEAN' ,'STD'],fmt=("%8.2f\t %8.2f"))
-
+            ppn_submean[isub,1] = CAFE[isub]
+            ppn_submean[isub,2] = OCTAC[isub]
         
+        ppn_ean[0,0] = np.nanmean(ppn_submean[:16,0]-ppn_submean[:16,1]) # BIAS CAFE
+        ppn_ean[0,1] = np.sqrt(np.nanmean((ppn_submean[:16,0]-ppn_submean[:16,1])**2)) #RMSD CAFE
+        ppn_ean[1,0] = np.nanmean(ppn_submean[:16,0]-ppn_submean[:16,2]) # BIAS OCTAC
+        ppn_ean[1,1] = np.sqrt(np.nanmean((ppn_submean[:16,0]-ppn_submean[:16,2])**2)) # RMSD OCTAC
+  
+        writetable(tablefile,ppn_submean,rows_names,['mean Mod', 'CAFE mean','OCTAC mean'])
+        writetable(tablefile_PPN_EAN,ppn_ean,["Mod_vs_CAFE","Mod_vs_OCTAC"],["BIAS","RMSD"])
 
-#        ncfile = OUTPUTDIR + "Map_" + var + "_" + req_label + "_Int" + layer.longname() + z_mask_string  + ".nc"
-#        netcdf3.write_2d_file(integrated_masked,"Z_c",ncfile,TheMask)
-#
-#        from bitsea.basins import V2 as OGS
-#        from bitsea.commons.submask import SubMask
-#        from bitsea.basins.basin import ComposedBasin
-##        from bitsea.commons.utils import writetable
-#        OGSred = ComposedBasin('OGSred',[OGS.alb, OGS.swm1, OGS.swm2, OGS.nwm, OGS.tyr, OGS.adr, OGS.ion, OGS.lev , OGS.med],'Gruped Subbasin for ppn analysis')
-#        SUBlist = OGSred.basin_list
-#        nSub   = len(OGSred.basin_list)
-#        rows_names=[sub.name for sub in SUBlist]
-#column_names=['bias','rmse','corr']
-#        ppn_submean = np.zeros((nSub,1),np.float32)*np.nan
-#        for isub, sub in enumerate(OGSred):
-#            S = SubMask(sub, maskobject=TheMask)
-#            mask2d=S.mask[0,:,:]
-#            ppn_submean[isub,0] = integrated_masked[mask2d].mean()
-#
-#        writetable(OUTPUTDIR + var + "MEAN-BASIN.txt", ppn_submean, rows_names, ['mean'])
-#  
+

d396925abd1eed096c024320741a4ce9bb4c6f76
Stefano Piani <spiani@ogs.it>
2025-04-12 00:26:07 +0200

    Adding methods to find the boundaries of a connected component

--- src/bitsea/components/component_mask.py
+++ src/bitsea/components/component_mask.py
@@ -1,3 +1,4 @@
+from itertools import product as cart_prod
 from typing import Union
 
 import numpy as np
@@ -15,6 +16,99 @@ class ComponentMask:
     easy to retrieve individual components and analyze their properties.
     Components are assigned unique integer IDs in the output array.
     """
+
+    @staticmethod
+    def _fill_holes(component: np.ndarray) -> np.ndarray:
+        """
+        Given a boolean mask, fills the holes that are within the component,
+        returning a boolean mask that is True on all the points that are in
+        the component or in all the points that are surrounded by the
+        component.
+
+        By definition, a "hole" is a region of `False` values that is not
+        connected to the boundary of the mask.
+        """
+        # We allocate the space for the final output
+        final_output = np.empty_like(component)
+        final_output[:] = component
+
+        # We build an object to check which ones of the components of `False`
+        # are holes
+        inverse_cm = ComponentMask(~component)
+        for i in range(inverse_cm.n_components):
+            current_component = inverse_cm.get_component(i)
+
+            # Check if this component reaches the boundary; we need to build
+            # all the slices of the form [:, :, ..., :, 0, :, ..., :] or
+            # [:, :, ..., :, -1, :, ..., :] to analyze all the boundaries
+            on_boundary = False
+            for axis in range(component.ndim):
+                for side in (0, -1):
+                    boundary_slice = tuple(
+                        side if k == axis else slice(None)
+                        for k in range(component.ndim)
+                    )
+                    if np.count_nonzero(current_component[boundary_slice]) > 0:
+                        on_boundary = True
+                        break
+                if on_boundary:
+                    break
+
+            # if we are not on boundary, this is a hole and must be added to
+            # the output
+            if not on_boundary:
+                np.logical_or(final_output, current_component, out=final_output)
+
+        return final_output
+
+    @staticmethod
+    def _get_boundary_cells(
+        mask: np.ndarray, closed_boundary: bool = False
+    ) -> np.ndarray:
+        """Given a boolean mask, returns the cells that are inside the mask
+        (i.e. that are `True`) and that are on the boundary of the mask.
+
+        In other words, it returns the cells whose value is `True` but that
+        have a neighbour that is `False`. With the word "neighbour", in this
+        case, we also include cells that can be reached by going one step
+        in every direction, including the diagonals.
+
+        Args:
+            mask: A boolean array representing the input mask.
+            closed_boundary: If True, the cells inside the mask that are on the
+                boundary of the domain will be also included in the output.
+        """
+        enlarged_data = np.pad(
+            ~mask, 1, mode="constant", constant_values=closed_boundary
+        )
+
+        # To determine if a cell has a neighbour that is outside the mask, is
+        # enough to shift the indices of tha mask by 1 on every possible
+        # direction. If we apply the following slices to the enlarged_data we
+        # may get the original values (slice associated to 0), the original
+        # values moved towards the right (slice associated to 1) or ti the left
+        # (slice associated to -1). By applying all the possible choice of the
+        # slices for each axis, we get all the possible neighbours.
+        slice_shifts = {0: slice(1, -1), 1: slice(2, None), -1: slice(None, -2)}
+
+        # After the loop, this array will contain `True` if the point at that
+        # position has a neighbour that is outside the mask
+        neighbour_outside = np.zeros_like(mask)
+
+        # For every axis, we decide where we need to shift
+        for shift in cart_prod((-1, 0, 1), repeat=mask.ndim):
+            # If all the shifts are 0 (do not move this axis), we get the
+            # original values, and we can skip this check
+            if tuple(set(shift)) == (0,):
+                continue
+            current_slice = tuple(slice_shifts[i] for i in shift)
+            np.logical_or(
+                neighbour_outside,
+                enlarged_data[current_slice],
+                out=neighbour_outside,
+            )
+
+        return np.logical_and(mask, neighbour_outside)
 
     def __init__(self, mask: Union[Mask, np.ndarray]):
         """
@@ -114,3 +208,48 @@ class ComponentMask:
             int: The ID of the largest connected component.
         """
         return int(np.argmax(self._n_cells_per_component))
+
+    def get_component_boundary(
+        self, component_id: int, closed_boundary=False
+    ) -> np.ndarray:
+        """
+        Returns the cells of the component with the specified ID that are on
+        the boundary of the component, i.e. that have at least one neighbour
+        that is not in the component.
+
+        We consider as "neighbours" also points that can be reached by moving
+        along the diagonals. This ensures that the corner of a concave mask
+        is also included in the boundary.
+
+        Args:
+            component_id: The ID of the connected component to retrieve.
+            closed_boundary: If True, the cells inside the mask that are on the
+                boundary of the domain will be also included in the output.
+        """
+        component_mask = self.get_component(component_id)
+        return self._get_boundary_cells(
+            component_mask, closed_boundary=closed_boundary
+        )
+
+    def get_component_external_boundary(self, component_id: int) -> np.ndarray:
+        """
+        Returns the cells of the component with the specified ID that are on
+        the external boundary of the component.
+
+        This function is very close to `get_component_boundary`, but it does
+        not return the cells that are on the boundary of a hole of the
+        component.
+
+        The algorithm is as follows: the holes of the component are filled and
+        then the boundary cells are computed.
+
+        Be aware that some points that are part of the external boundary may not
+        be part of the original mask. This may happen if there is a hole that
+        includes the vertex of a concave angle of the external boundary. In this
+        case, when the hole is filled by the algorithm, the vertex becomes part
+        of the boundary. If you don't want this behaviour, intersect the output
+        of this function with the original mask.
+        """
+        component_mask = self.get_component(component_id)
+        component_without_holes = self._fill_holes(component_mask)
+        return self._get_boundary_cells(component_without_holes)
--- tests/components/test_components.py
+++ tests/components/test_components.py
@@ -76,3 +76,84 @@ def test_component_mask_3d():
     component_mask = ComponentMask(data_map)
     assert component_mask.n_components == 1
     assert np.all(component_mask.get_component(0) == data_map)
+
+
+def test_component_mask_compute_boundary():
+    data_map = np.zeros((10, 10, 10), dtype=bool)
+    data_map[7, 1:9, 1:9] = True
+    data_map[1:4, 1:4, 1:4] = True
+
+    component_mask = ComponentMask(data_map)
+    assert component_mask.n_components == 2
+
+    k1 = component_mask.get_biggest_component()
+    k2 = 1 if k1 == 0 else 0
+
+    b2 = np.zeros_like(data_map)
+    b2[1, 1:4, 1:4] = True
+    b2[3, 1:4, 1:4] = True
+    b2[1:4, 1, 1:4] = True
+    b2[1:4, 3, 1:4] = True
+    b2[1:4, 1:4, 1] = True
+    b2[1:4, 1:4, 3] = True
+
+    assert np.all(
+        component_mask.get_component_boundary(k1)
+        == component_mask.get_component(k1)
+    )
+    assert np.all(component_mask.get_component_boundary(k2) == b2)
+
+
+def test_component_mask_compute_boundary_on_boundary():
+    data_map = np.zeros((6, 11), dtype=bool)
+    data_map[2:5, 2:9] = True
+    data_map[2:5, 2:9] = True
+    data_map[:3, 4:7] = True
+
+    expected_boundary = np.zeros_like(data_map)
+    expected_boundary[2:5, 2] = True
+    expected_boundary[2:5, 8] = True
+    expected_boundary[4, 2:9] = True
+    expected_boundary[2, 2:5] = True
+    expected_boundary[2, 6:9] = True
+    expected_boundary[:2, 4] = True
+    expected_boundary[:2, 6] = True
+
+    expected_boundary_on_boundary = expected_boundary.copy()
+    expected_boundary_on_boundary[0, 5] = True
+
+    component_mask = ComponentMask(data_map)
+
+    assert np.all(component_mask.get_component_boundary(0) == expected_boundary)
+    assert np.all(
+        component_mask.get_component_boundary(0, closed_boundary=True)
+        == expected_boundary_on_boundary
+    )
+
+
+def test_component_mask_compute_external_boundary():
+    data_map = np.zeros((11, 11), dtype=bool)
+    data_map[2:9, 2:9] = True
+    data_map[3:5, 3:5] = False
+    data_map[6:8, 6:8] = False
+    data_map[2, 2] = False
+    data_map[8, 6] = False
+
+    expected_boundary_with_holes = data_map.copy()
+    expected_boundary_with_holes[3:5, 6:8] = False
+    expected_boundary_with_holes[6:8, 3:5] = False
+
+    expected_external_boundary = data_map.copy()
+    expected_external_boundary[3, 3] = True
+    expected_external_boundary[5:8, 3:5] = False
+    expected_external_boundary[3:5, 5:8] = False
+
+    component_mask = ComponentMask(data_map)
+
+    assert np.all(
+        component_mask.get_component_boundary(0) == expected_boundary_with_holes
+    )
+    assert np.all(
+        component_mask.get_component_external_boundary(0)
+        == expected_external_boundary
+    )

78a7ab9e41b237622df794b86ed53e23411a28ae
Stefano Piani <spiani@ogs.it>
2025-05-22 11:18:33 +0200

    Now geographiclib is an optional dependency

--- src/bitsea/utilities/optional_dependencies/__init__.py
+++ src/bitsea/utilities/optional_dependencies/__init__.py
@@ -1,2 +1,7 @@
 class OptionalDependencyMissing(UserWarning):
     pass
+
+
+# In principle, OptionalDependencyMissing could also have been an
+# "ImportWarning", but since they are ignored by default, we use the
+# UserWarning

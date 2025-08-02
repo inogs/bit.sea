6cd445c91593bf34e96d0b3cf0986eafcab70296
Anna Teruzzi <ateruzzi@login03.g100.cineca.it>
2025-08-02 07:31:19 +0200

    Added informative comments in launcher for varsat adjustment

--- src/bitsea/PreprocDA/launcher_changevarsat.sh
+++ src/bitsea/PreprocDA/launcher_changevarsat.sh
@@ -1,12 +1,12 @@
 
 
 MESHMASK=/g100_scratch/userexternal/ateruzzi/PerCarolina/ADJUST_VARSAT/meshmask_v12c.nc
-INDIR=/g100_scratch/userexternal/ateruzzi/PerCarolina/ADJUST_VARSAT/VARSAT_V9C
-# LIMERRCOAST=0.1
-LIMERRCOAST=nan
-FIRSMONTH=6
-LASTMONT=8
-ADDERRSUMMER=0.005
+INDIR=/g100_scratch/userexternal/ateruzzi/PerCarolina/ADJUST_VARSAT/VARSAT_V9C/ # Copiati da /g100_work/OGS_devC/V9C/RUNS_SETUP/PREPROC/DA/DA_static_data (sono quelli usati nella QUID_NECCTON)
+# LIMERRCOAST=0.1 #Valore limite dell'errore usato in costa
+LIMERRCOAST=nan # Mettendo nan NON corregge in costa (testato)
+FIRSMONTH=6 # primo mese d'estate
+LASTMONT=8 # ultimo mese d'estate
+ADDERRSUMMER=0.005 # Mettendo nana NON dovrebbe correggere in costa (NON TESTATO)
 SATMAP=Mesh24
 
 OUTDIR=/g100_scratch/userexternal/ateruzzi/PerCarolina/ADJUST_VARSAT/VARSATadj_COAST${LIMERRCOAST}_SUMMER${ADDERRSUMMER}
